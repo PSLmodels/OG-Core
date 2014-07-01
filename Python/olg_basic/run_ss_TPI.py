381d9c990e1c265a1e9a40c19367bf41b45bd4dc
Evan Magnusson <evanmag42@gmail.com>
2014-07-01 16:43:16 -0600

    renamed files

--- Python/olg_basic/run_ss_TPI.py
+++ Python/olg_basic/run_ss_TPI.py
@@ -2,9 +2,9 @@
 This will run the steady state solver as well as time path iteration.
 '''
 
-import OLG_fastversion
+import SS
 
-import TPI_fastversion
+import TPI
 
 '''
 See the pickles for results.
