22c88542c79a5691cddf5df1ce11c45a96a923b4
Evan Magnusson <evanmag42@gmail.com>
2014-07-01 17:31:30 -0600

    fixed income for samuel

--- Python/olg_basic/income.py
+++ Python/olg_basic/income.py
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------------
 Last updated 7/1/2014
 
-Functions for created the matrix of ability levels, e, and the 
+Functions for created the matrix of ability levels, e, and the
 probabilities, f, to be used in OLG_fastversion.py
 
 This py-file calls the following other file(s):
@@ -43,15 +43,14 @@ del markov_dta['ER33504'], markov_dta['ER33537O'], markov_dta[
     'ER33604'], markov_dta['ER33628O'], markov_dta['ER30001'], markov_dta[
     'ER30002'], markov_dta['ER33501'], markov_dta['ER33502'], markov_dta[
     'ER33503'], markov_dta['ER33601'], markov_dta['ER33602'], markov_dta[
-    'ER33603'], 
-markov_dta = markov_dta.query("19 < age_99 < 80")
-markov_dta = markov_dta.query("19 < age_01 < 80")
-markov_dta = markov_dta.query("wage_99 != -99")
-markov_dta = markov_dta.query("wage_01 != -99")
-markov_dta = markov_dta.query("wage_99 != 0")
-markov_dta = markov_dta.query("wage_01 != 0")
-markov_dta = markov_dta.query("wage_99 != 999")
-markov_dta = markov_dta.query("wage_01 != 999")
+    'ER33603']
+markov_dta = markov_dta[(19 < markov_dta.age_99) & (
+    markov_dta.age_99 < 80) & (19 < markov_dta.age_01) & (
+    markov_dta.age_01 < 80)]
+markov_dta = markov_dta[(markov_dta.wage_99 != -99) & (
+    markov_dta.wage_99 != 0) & (markov_dta.wage_99 != 999)]
+markov_dta = markov_dta[(markov_dta.wage_01 != -99) & (
+    markov_dta.wage_01 != 0) & (markov_dta.wage_01 != 999)]
 markov_dta = markov_dta.reset_index()
 del markov_dta['index']
 
@@ -61,14 +60,15 @@ def get_e(S, J):
     Parameters: S - Number of age cohorts
                 J - Number of ability levels by age
 
-    Returns:    e - S x J matrix of J working ability levels for each 
-                    age cohort measured by hourly wage, normalized so 
+    Returns:    e - S x J matrix of J working ability levels for each
+                    age cohort measured by hourly wage, normalized so
                     the mean is one
     '''
     age_groups = np.linspace(20, 80, S+1)
     e = np.zeros((S, J))
     for i in xrange(S):
-        incomes = data.query('age_groups[i]<=age<age_groups[i+1]')
+        incomes = data[(age_groups[i] <= data.age) & (
+            data.age < age_groups[i+1])]
         inc = np.array(incomes['wage'])
         inc.sort()
         for j in xrange(J):
@@ -115,30 +115,34 @@ def get_f(S, J):
     for ind99 in xrange(J):
         # Count the number of individuals in each percentile group in 1999
         if ind99 == 0:
-            num_in_99df = markov_dta.query('wage_99 <= percentiles99[ind99]')
+            num_in_99df = markov_dta[
+                markov_dta.wage_99 <= percentiles99[ind99]]
             num_in_99 = np.array(num_in_99df.count()[0])
         elif ind99 < J-1:
-            num_in_99df = markov_dta.query(
-                'percentiles99[ind99-1] < wage_99 <= percentiles99[ind99]')
+            num_in_99df = markov_dta[
+                (percentiles99[ind99-1] < markov_dta.wage_99) & (
+                    markov_dta.wage_99 <= percentiles99[ind99])]
             num_in_99 = np.array(num_in_99df.count()[0])
         else:
-            num_in_99df = markov_dta.query('percentiles99[ind99-1] < wage_99')
+            num_in_99df = markov_dta[percentiles99[
+                ind99-1] < markov_dta.wage_99]
             num_in_99 = np.array(num_in_99df.count()[0])
         for ind01 in xrange(J):
             # Count the number of individuals that were in a certain
                 # percentile group in 1999 who remain in that group
             if ind01 == 0:
-                num_in_both = num_in_99df.query(
-                    'wage_01 <= percentiles01[ind01]').count()
-                num_in_both = np.array(num_in_both[0])
+                num_in_both = num_in_99df[
+                    num_in_99df.wage_01 <= percentiles01[ind01]]
+                num_in_both = np.array(num_in_both.count()[0])
             elif ind01 < J-1:
-                num_in_both = num_in_99df.query(
-                    'percentiles01[ind01-1] < wage_01 <= percentiles01[ind01]').count()
-                num_in_both = np.array(num_in_both[0])
+                num_in_both = num_in_99df[(
+                    percentiles01[ind01-1] < num_in_99df.wage_01) & (
+                    num_in_99df.wage_01 <= percentiles01[ind01])]
+                num_in_both = np.array(num_in_both.count()[0])
             else:
-                num_in_both = num_in_99df.query(
-                    'percentiles01[ind01-1] < wage_01').count()
-                num_in_both = np.array(num_in_both[0])
+                num_in_both = num_in_99df[
+                    percentiles01[ind01-1] < num_in_99df.wage_01]
+                num_in_both = np.array(num_in_both.count()[0])
             # Each entry of the Markov matrix is the number of individuals who
                 # were in a percentile group and stayed in that percentile
                 # group, divided by the number of individuals originally in
--- Python/olg_basic/samuel_test.py
+++
@@ -1,5 +0,0 @@
-import income
-
-e = income.get_e(5, 5)
-f1 = income.get_f_simple(5, 5)
-f2 = income.get_f(5, 5)
