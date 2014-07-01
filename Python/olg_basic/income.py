e361852104168908ae289a888af1a37ac7168ec7
Evan Magnusson <evanmag42@gmail.com>
2014-07-01 12:07:47 -0600

    markov chain

--- Python/olg_basic/income.py
+++ Python/olg_basic/income.py
@@ -34,6 +34,27 @@ data['age'], data['wage'] = data['PRTAGE'], data['PTERNHLY']
 del data['HRHHID'], data['OCCURNUM'], data['YYYYMM'], data[
     'HRHHID2'], data['PRTAGE'], data['PTERNHLY']
 
+markov_dta = pd.read_csv("data/PSIDdata.csv", sep=',', header=0)
+markov_dta['age_99'], markov_dta['wage_99'] = markov_dta[
+    'ER33504'], markov_dta['ER33537O']
+markov_dta['age_01'], markov_dta['wage_01'] = markov_dta[
+    'ER33604'], markov_dta['ER33628O']
+del markov_dta['ER33504'], markov_dta['ER33537O'], markov_dta[
+    'ER33604'], markov_dta['ER33628O'], markov_dta['ER30001'], markov_dta[
+    'ER30002'], markov_dta['ER33501'], markov_dta['ER33502'], markov_dta[
+    'ER33503'], markov_dta['ER33601'], markov_dta['ER33602'], markov_dta[
+    'ER33603'], 
+markov_dta = markov_dta.query("19 < age_99 < 80")
+markov_dta = markov_dta.query("19 < age_01 < 80")
+markov_dta = markov_dta.query("wage_99 != -99")
+markov_dta = markov_dta.query("wage_01 != -99")
+markov_dta = markov_dta.query("wage_99 != 0")
+markov_dta = markov_dta.query("wage_01 != 0")
+markov_dta = markov_dta.query("wage_99 != 999")
+markov_dta = markov_dta.query("wage_01 != 999")
+markov_dta = markov_dta.reset_index()
+# del markov_dta['index']
+
 
 def get_e(S, J):
     '''
@@ -56,7 +77,7 @@ def get_e(S, J):
     return e
 
 
-def get_f(S, J):
+def get_f_simple(S, J):
     '''
     Parameters: S - Number of age cohorts
                 J - Number of ability levels by age
@@ -65,4 +86,41 @@ def get_f(S, J):
                     by age group
     '''
     f = np.ones((S, J))*(1.0/J)
+    return f
+
+
+def get_f(S, J):
+    f = np.zeros((J, J))
+    sort99 = np.array(markov_dta['wage_99'])
+    sort01 = np.array(markov_dta['wage_01'])
+    sort99.sort()
+    sort01.sort()
+    percentiles99 = np.zeros(J-1)
+    percentiles01 = np.zeros(J-1)
+    for j in xrange(J-1):
+        percentiles99[j] = sort99[len(sort99)*(j+1)/J]
+        percentiles01[j] = sort01[len(sort01)*(j+1)/J]
+    for ind99 in xrange(J):
+        if ind99 == 0:
+            num_in_99df = markov_dta.query('wage_99 <= percentiles99[ind99]')
+            num_in_99 = np.array(num_in_99df.count()[0])
+        elif ind99 < J-1:
+            num_in_99df = markov_dta.query('percentiles99[ind99-1] < wage_99 <= percentiles99[ind99]')
+            num_in_99 = np.array(num_in_99df.count()[0])
+        else:
+            num_in_99df = markov_dta.query('percentiles99[ind99-1] < wage_99')
+            num_in_99 = np.array(num_in_99df.count()[0])
+        for ind01 in xrange(J):
+            if ind01 == 0:
+                num_in_both = num_in_99df.query('wage_01 <= percentiles01[ind01]').count()
+                num_in_both = np.array(num_in_both[0])
+            elif ind01 < J-1:
+                num_in_both = num_in_99df.query('percentiles01[ind01-1] < wage_01 <= percentiles01[ind01]').count()
+                num_in_both = np.array(num_in_both[0])
+            else:
+                num_in_both = num_in_99df.query('percentiles01[ind01-1] < wage_01').count()
+                num_in_both = np.array(num_in_both[0])
+            f[ind99, ind01] = float(num_in_both) / num_in_99
+    f = np.linalg.matrix_power(f, 60/S)
+    f = np.tile(f.reshape(1, J, J), (S, J, J))
     return f
