e1f5130f2582b3e865d41218db3dbc35cc07b63c
unknown <isaacdswift@gmail.com>
2014-07-31 18:00:29 -0600

    OLG

--- Python/demographics/demographics.py
+++ Python/demographics/demographics.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import scipy.optimize as opt
+from scipy.special import beta, gamma
+import matplotlib.pyplot as plt
 
 data = pd.read_table('data\demographic_data.csv', sep=',')
 data = data[(15 < data.Age) & (data.Age < 76)]
@@ -29,6 +32,45 @@ def get_survival(S, J):
     surv_array = np.tile(surv_rate_condensed.reshape(S, 1), (1, J))
     return surv_array
 
+# x = np.arange(35) + 15
+# y = np.array([43.7]*5 + [104.0]*5 + [114.5]*5 + [93.5]*5 + [42.8]*5 + [8.5]*5 + [0.5]*5)
+x = np.array([17.5, 22.5, 27.5, 32.5, 37.5, 42.5, 27.5])
+y = np.array([43.7, 104.0, 114.5, 93.5, 42.8, 8.5, 0.5]) / 2000.0
+b = y.var()/y.mean()
+a = y.mean()/b
+params = [a, b]
+# params = [3.0, 1.0]
+
+def GB2(x, params):
+    a, b, p, q = params[0], params[1], params[2], params[3]
+    return a*x**(a*p-1) / ( b**(a*p) * bt(p,q) * (1+(x/b)**a)**(p+q) )
+
+def GA(x, params):
+    a, b = params.flatten()
+    part1 = 1.0/((b**a) * gamma(a))
+    part2 = (x**(a-1)) * (np.exp(-x/b))
+    return part1 * part2
+
+def GMM(params, x, y):
+    size = len(y)
+    e = (GA(x, params) - y)/y
+    W = np.identity(size)
+    return np.linalg.norm(e)**2
+    # return (e.reshape(1,size)).dot(W.dot(e.reshape(size,1)))
+
+def get_fertility(S, J):
+    ages = np.linspace(16+(S/120.0), 76-(S/120.0), S)
+    answer = opt.minimize(GMM, params, args=(x,y), method='Nelder-Mead', bounds=((0,None),(0,None)))
+    opt_params = answer.x
+    print answer.success
+    print opt_params
+    plt.plot(np.arange(50), GA(np.arange(50), opt_params))
+    plt.plot(x, y)
+    plt.show()
+    fert_array = GA(ages, opt_params)
+    return ages, fert_array
+
+# get_fertility(60,7)
 
 def get_omega(S, J, T):
     age_groups = np.linspace(16, 76, S+1)
