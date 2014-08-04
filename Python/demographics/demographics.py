71b52eb5c89feb3c05545ffa7b4a916079ad71cd
Evan Magnusson <evanmag42@gmail.com>
2014-08-04 17:44:03 -0600

    polynomial fitting

--- Python/demographics/demographics.py
+++ Python/demographics/demographics.py
@@ -21,11 +21,12 @@ mort_data['mort_rate'] = (mort_data.prob_live_next_male + mort_data.prob_live_ne
 del mort_data['prob_live_next_female'], mort_data['prob_live_next_male']
 mort_data['surv_rate'] = 1 - mort_data.mort_rate
 children_rate = np.array(mort_data[(mort_data.age < 15)].surv_rate)
-mort_data = mort_data[(mort_data.age < 76) & (mort_data.age >= 15)]
+# mort_data = mort_data[(mort_data.age < 76) & (mort_data.age >= 15)]
 
 
 def get_survival(S, J):
-    survival_rate = np.array(mort_data.surv_rate)
+    poly_surv = np.poly1d(np.polyfit(mort_data.age, mort_data.surv_rate, 10))
+    survival_rate = poly_surv(np.linspace(15, 75, S))
     surv_rate_condensed = np.zeros(S)
     for s in xrange(S):
         surv_rate_condensed[s] = np.product(survival_rate[s*(60/S):(s+1)*(60/S)])
@@ -52,32 +53,50 @@ def get_survival(S, J):
 
 
 # 15-19, ...,45-49
-fert_data = np.array([43.7, 104.0, 114.5, 93.5, 92.8, 8.5, .5]) / 1000
+# fert_data = np.array([43.7, 104.0, 114.5, 93.5, 92.8, 8.5, .5]) / 1000
+
+
+# from http://www.cdc.gov/nchs/data/nvsr/nvsr63/nvsr63_02.pdf
+fert_data = np.array([.3, 26.6, 12.3, 47.3, 81.2, 106.2, 98.7, 49.6, 10.5, .8])
+age_midpoint = np.array([12, 17, 16, 18.5, 22, 27, 32, 37, 42, 49.5])
 # 12.5 - 17.5
 ln1 = np.linspace(0, fert_data[0], 11)
-# 17.5 - 22.5
+# # 17.5 - 22.5
 ln2 = np.linspace(fert_data[0], fert_data[1], 11)
-# 22.5 - 27.5
+# # 22.5 - 27.5
 ln3 = np.linspace(fert_data[1], fert_data[2], 11)
-# 27.5 - 32.5
+# # 27.5 - 32.5
 ln4 = np.linspace(fert_data[2], fert_data[3], 11)
-# 32.5 - 37.5
+# # 32.5 - 37.5
 ln5 = np.linspace(fert_data[3], fert_data[4], 11)
-# 37.5 - 42.5
+# # 37.5 - 42.5
 ln6 = np.linspace(fert_data[4], fert_data[5], 11)
-# 42.5 - 47.5
+# # 42.5 - 47.5
 ln7 = np.linspace(fert_data[5], fert_data[6], 11)
-# 47.5 - 52.5
+# # 47.5 - 52.5
 ln8 = np.linspace(fert_data[6], 0, 11)
 
 def get_fert(S, J):
-    everything = np.array(list(ln1) + list(ln2[1:]) + list(ln3[1:]) + list(ln4[1:]) + list(ln5[1:]) + list(ln6[1:]) + list(ln7[1:]) + list(ln8[1:]))
-    sixteen_to_52 = everything[7::2]
-    fert_rate = np.array(list(sixteen_to_52) + list(np.zeros(23)))
+    # everything = np.array(list(ln1) + list(ln2[1:]) + list(ln3[1:]) + list(ln4[1:]) + list(ln5[1:]) + list(ln6[1:]) + list(ln7[1:]) + list(ln8[1:]))
+    # sixteen_to_52 = everything[7::2]
+    # fert_rate = np.array(list(sixteen_to_52) + list(np.zeros(23)))
+    # polynomial fit of fertility rates
+    poly_fert = np.poly1d(np.polyfit(age_midpoint, fert_data, 4))
+    fert_rate = poly_fert(np.linspace(15, 75, S))
+    for i in xrange(S):
+        if np.linspace(15,75, S)[i] > 52 or np.linspace(15,75, S)[i] < 10:
+            fert_rate[i] = 0
+        if fert_rate[i] < 0:
+            fert_rate[i] = 0
     fert_rate = fert_rate[ 60%S : : 60/S ]
     fert_rate = np.tile(fert_rate.reshape(S,1), (1,J))
     children = np.zeros((15, J))
     return fert_rate/2.0, children
+
+ran1, els = get_fert(60, 1)
+plt.plot(np.linspace(15, 75, 60), ran1)
+plt.scatter(age_midpoint, fert_data)
+plt.savefig('zzzsomething')
 
 
 # # x = np.arange(35) + 15
