1b17b3b4b402e33329f3fa500e794d77b6a29410
Evan Magnusson <evanmag42@gmail.com>
2015-06-23 13:18:25 -0600

    fixed TPI spike in period 1

--- Python/Development-fsolve/TPI.py
+++ Python/Development-fsolve/TPI.py
@@ -133,7 +133,7 @@ else:
 K0 = house.get_K(initial_b, omega_stationary[0])
 b_sinit = np.array(list(np.zeros(J).reshape(1, J)) + list(initial_b[:-1]))
 b_splus1init = initial_b
-L0 = firm.get_L(e, initial_n, omega_stationary[1])
+L0 = firm.get_L(e, initial_n, omega_stationary[0])
 Y0 = firm.get_Y(K0, L0, parameters)
 w0 = firm.get_w(Y0, L0, parameters)
 r0 = firm.get_r(Y0, K0, parameters)
@@ -155,18 +155,18 @@ def SS_TPI_firstdoughnutring(guesses, winit, rinit, BQinit, T_H_init):
     n1 = float(guesses[1])
     b1 = float(initial_b[-2, j])
     # Euler 1 equations
-    tax11 = tax.total_taxes_eul3_TPI(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], factor_ss, T_H_init, j)
-    cons11 = house.get_cons(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], b2, g_y, tax11)
+    tax11 = tax.total_taxes(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], factor_ss, T_H_init, j, 'TPI', False, parameters, theta, tau_bq)
+    cons11 = house.get_cons(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], b2, parameters, tax11)
     bequest_ut = rho * np.exp(-sigma * g_y) * chi_b[-1, j] * b2 ** (-sigma)
-    error1 = house.marg_ut_cons(cons11) - bequest_ut
+    error1 = house.marg_ut_cons(cons11, parameters) - bequest_ut
     # Euler 2 equations
-    tax2 = tax.total_taxes_eul3_TPI(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], factor_ss, T_H_init, j)
-    cons2 = house.get_cons(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], b2, g_y, tax2)
+    tax2 = tax.total_taxes(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], factor_ss, T_H_init, j, 'TPI', True, parameters, theta, tau_bq)
+    cons2 = house.get_cons(rinit, b1, winit, e[-1, j], n1, BQinit, lambdas[j], b2, parameters, tax2)
     income2 = (rinit * b1 + winit * e[-1, j] * n1) * factor_ss
     deriv2 = 1 - tau_payroll - tax.tau_income(rinit, b1, winit, e[
-        -1, j], n1, factor_ss) - tax.tau_income_deriv(
-        rinit, b1, winit, e[-1, j], n1, factor_ss) * income2
-    error2 = house.marg_ut_cons(cons2) * winit * e[-1, j] * deriv2 - house.marg_ut_labor(n1, chi_n[-1])
+        -1, j], n1, factor_ss, parameters) - tax.tau_income_deriv(
+        rinit, b1, winit, e[-1, j], n1, factor_ss, parameters) * income2
+    error2 = house.marg_ut_cons(cons2, parameters) * winit * e[-1, j] * deriv2 - house.marg_ut_labor(n1, chi_n[-1], parameters)
     if n1 <= 0:
         error2 += 1e12
     return [error1] + [error2]
@@ -193,7 +193,7 @@ def Steady_state_TPI_solver(guesses, winit, rinit, BQinit, T_H_init, factor, j, 
     if length == S:
         b_s = np.array([0] + list(b_guess[:-1]))
     else:
-        b_s = np.array([(initial_b[-(s+2), j])] + list(b_guess[:-1]))
+        b_s = np.array([(initial_b[-(s+3), j])] + list(b_guess[:-1]))
     b_splus1 = b_guess
     b_splus2 = np.array(list(b_guess[1:]) + [0])
     w_s = winit[t:t+length]
@@ -235,8 +235,7 @@ def Steady_state_TPI_solver(guesses, winit, rinit, BQinit, T_H_init, factor, j, 
     error2[mask3] += 1e12
     mask4 = b_guess <= 0
     error2[mask4] += 1e12
-    return list(error1.flatten()) + list(
-        error2.flatten())
+    return list(error1.flatten()) + list(error2.flatten())
 
 # Initialize Time paths
 domain = np.linspace(0, T, T)
@@ -284,6 +283,8 @@ while (TPIiter < maxiter) and (TPIdist >= mindist_TPI):
         T+10), Kpath_TPI[:T+10], 'b', linewidth=2, label=r"TPI time path $\hat{K}_t$")
     plt.savefig("OUTPUT/TPI_K")
     for j in xrange(J):
+        # b_mat[1, -1, j], n_mat[0, -1, j] = np.array(opt.fsolve(SS_TPI_firstdoughnutring, [guesses_b[1, -2, j], guesses_n[0, -2, j]],
+        #     args=(winit[1], rinit[1], BQinit[1, j], T_H_init[1])))
         for s in xrange(S-2):  # Upper triangle
             b_guesses_to_use = np.diag(guesses_b[1:S+1, :, j], S-(s+2))
             n_guesses_to_use = np.diag(guesses_n[:S, :, j], S-(s+2))
@@ -308,23 +309,34 @@ while (TPIiter < maxiter) and (TPIdist >= mindist_TPI):
             inputs = list(solutions)
             euler_errors[t, :, j] = np.abs(Steady_state_TPI_solver(
                 inputs, winit, rinit, BQinit[:, j], T_H_init, factor_ss, j, None, t, parameters, theta, tau_bq, rho, lambdas, e, None, chi_b, chi_n))
-        # b_mat[1, -1, j], n_mat[0, -1, j] = np.array(opt.fsolve(SS_TPI_firstdoughnutring, [b_mat[1, -2, j], n_mat[0, -2, j]],
-        #     args=(winit[1], rinit[1], BQinit[1, j], T_H_init[1])))
     
     b_mat[0, :, :] = initial_b
     b_mat[1, -1, :]= b_mat[1, -2, :]
     n_mat[0, -1, :] = n_mat[0, -2, :]
-    Knew = (omega_stationary[:T, :, :] * b_mat[:T, :, :]).sum(2).sum(1)
-    Lnew = (omega_stationary[1:T+1, :, :] * e.reshape(
+    Kinit = (omega_stationary[:T, :, :] * b_mat[:T, :, :]).sum(2).sum(1)
+    Linit = (omega_stationary[:T, :, :] * e.reshape(
         1, S, J) * n_mat[:T, :, :]).sum(2).sum(1)
-    BQnew = (1+rinit[:T].reshape(T, 1))*(b_mat[:T, :, :] * omega_stationary[:T, :, :] * rho.reshape(1, S, 1)).sum(1)
-    Kinit = misc_funcs.convex_combo(Knew, Kinit[:T], parameters)
-    Linit = misc_funcs.convex_combo(Lnew, Linit[:T], parameters)
+
+    Ynew = firm.get_Y(Kinit, Linit, parameters)
+    wnew = firm.get_w(Ynew, Linit, parameters)
+    rnew = firm.get_r(Ynew, Kinit, parameters)
+
+    BQnew = (1+rnew.reshape(T, 1))*(b_mat[:T] * omega_stationary[:T] * rho.reshape(1, S, 1)).sum(1)
+    bmat_s = np.zeros((T, S, J))
+    bmat_s[:, 1:, :] = b_mat[:T, :-1, :]
+    T_H_new = np.array(list(tax.get_lump_sum(rnew.reshape(T, 1, 1), bmat_s, wnew.reshape(
+        T, 1, 1), e.reshape(1, S, J), n_mat[:T], BQnew.reshape(T, 1, J), lambdas.reshape(
+        1, 1, J), factor_ss, omega_stationary[:T], 'TPI', parameters, theta, tau_bq)) + [T_Hss]*S)
+
+    winit[:T] = misc_funcs.convex_combo(wnew, winit[:T], parameters)
+    rinit[:T] = misc_funcs.convex_combo(rnew, rinit[:T], parameters)
     BQinit[:T] = misc_funcs.convex_combo(BQnew, BQinit[:T], parameters)
+    T_H_init[:T] = misc_funcs.convex_combo(T_H_new[:T], T_H_init[:T], parameters)
     guesses_b = misc_funcs.convex_combo(b_mat, guesses_b, parameters)
     guesses_n = misc_funcs.convex_combo(n_mat, guesses_n, parameters)
-    TPIdist = np.array(list(misc_funcs.perc_dif_func(Knew, Kinit))+list(misc_funcs.perc_dif_func(BQnew, BQinit[:T]).flatten())+list(misc_funcs.perc_dif_func(Lnew, Linit))).max()
-    TPIdist = np.array(list(np.abs(Knew - Kinit)) + list(np.abs(BQnew - BQinit[:T]).flatten()) + list(np.abs(Lnew - Linit))).max()
+
+    TPIdist = np.array(list(misc_funcs.perc_dif_func(rnew, rinit[:T]))+list(misc_funcs.perc_dif_func(BQnew, BQinit[:T]).flatten())+list(
+        misc_funcs.perc_dif_func(wnew, winit[:T]))+list(misc_funcs.perc_dif_func(T_H_new, T_H_init))).max()
     TPIdist_vec[TPIiter] = TPIdist
     # After T=10, if cycling occurs, drop the value of nu
     # wait til after T=10 or so, because sometimes there is a jump up
@@ -336,26 +348,47 @@ while (TPIiter < maxiter) and (TPIdist >= mindist_TPI):
     TPIiter += 1
     print '\tIteration:', TPIiter
     print '\t\tDistance:', TPIdist
-    if (TPIiter < maxiter) and (TPIdist >= mindist_TPI):
-        bmat_plus1 = np.zeros((T, S, J))
-        bmat_plus1[:, 1:, :] = b_mat[:T, :-1, :]
-        T_H_init = np.array(list(tax.get_lump_sum(rinit[:T].reshape(T, 1, 1), bmat_plus1, winit[:T].reshape(
-            T, 1, 1), e.reshape(1, S, J), n_mat[:T], BQinit[:T].reshape(T, 1, J), lambdas.reshape(
-            1, 1, J), factor_ss, omega_stationary[:T], 'TPI', parameters, theta, tau_bq)) + [T_Hss]*S)
-        Yinit = firm.get_Y(Kinit, Linit, parameters)
-        winit = np.array(list(firm.get_w(Yinit, Linit, parameters)) + list(np.ones(S)*wss))
-        rinit = np.array(list(firm.get_r(Yinit, Kinit, parameters)) + list(np.ones(S)*rss))
-    if TPIdist < mindist_TPI:
-        BQinit[:T] = BQnew
-        Kinit[:T] = Knew
-        Linit[:T] = Lnew
-    
+
+print 'Computing final solutions'
+
+b_mat = np.zeros((T+S, S, J))
+n_mat = np.zeros((T+S, S, J))
+
+for j in xrange(J):
+    for s in xrange(S-2):  # Upper triangle
+        b_guesses_to_use = np.diag(guesses_b[1:S+1, :, j], S-(s+2))
+        n_guesses_to_use = np.diag(guesses_n[:S, :, j], S-(s+2))
+        solutions = opt.fsolve(Steady_state_TPI_solver, list(
+            b_guesses_to_use) + list(n_guesses_to_use), args=(
+            winit, rinit, BQinit[:, j], T_H_init, factor_ss, j, s, 0, parameters, theta, tau_bq, rho, lambdas, e, initial_b, chi_b, chi_n), xtol=1e-13)
+        b_vec = solutions[:len(solutions)/2]
+        b_mat[1:S+1, :, j] += np.diag(b_vec, S-(s+2))
+        n_vec = solutions[len(solutions)/2:]
+        n_mat[:S, :, j] += np.diag(n_vec, S-(s+2))
+
+    for t in xrange(0, T):
+        b_guesses_to_use = np.diag(guesses_b[t+1:t+S+1, :, j])
+        n_guesses_to_use = np.diag(guesses_n[t:t+S, :, j])
+        solutions = opt.fsolve(Steady_state_TPI_solver, list(
+            b_guesses_to_use) + list(n_guesses_to_use), args=(
+            winit, rinit, BQinit[:, j], T_H_init, factor_ss, j, None, t, parameters, theta, tau_bq, rho, lambdas, e, None, chi_b, chi_n), xtol=1e-13)
+        b_vec = solutions[:S]
+        b_mat[t+1:t+S+1, :, j] += np.diag(b_vec)
+        n_vec = solutions[S:]
+        n_mat[t:t+S, :, j] += np.diag(n_vec)
+        inputs = list(solutions)
+        euler_errors[t, :, j] = np.abs(Steady_state_TPI_solver(
+            inputs, winit, rinit, BQinit[:, j], T_H_init, factor_ss, j, None, t, parameters, theta, tau_bq, rho, lambdas, e, None, chi_b, chi_n))
+    # b_mat[1, -1, j], n_mat[0, -1, j] = np.array(opt.fsolve(SS_TPI_firstdoughnutring, [b_mat[1, -2, j], n_mat[0, -2, j]],
+    #     args=(winit[1], rinit[1], BQinit[1, j], T_H_init[1])))
+
+b_mat[0, :, :] = initial_b
+b_mat[1, -1, :]= b_mat[1, -2, :]
+n_mat[0, -1, :] = n_mat[0, -2, :]
 
 Kpath_TPI = list(Kinit) + list(np.ones(10)*Kss)
 Lpath_TPI = list(Linit) + list(np.ones(10)*Lss)
 BQpath_TPI = np.array(list(BQinit) + list(np.ones((10, J))*BQss))
-
-
 
 
 b_s = np.zeros((T, S, J))
