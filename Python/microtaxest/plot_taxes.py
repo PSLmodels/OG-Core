20f14028508274907c3376b069f323d5bb043227
Richard Evans <rickecon@gmail.com>
2015-10-24 18:29:18 -0600

    updated Python/microtaxest/plot_taxes.py function to go through each year, and plot the truncated data.

--- Python/microtaxest/plot_taxes.py
+++ Python/microtaxest/plot_taxes.py
@@ -1,12 +1,12 @@
 '''
 ------------------------------------------------------------------------
-This script reads in data generated from the OSPC Tax Calcultor and 
+This script reads in data generated from the OSPC Tax Calcultor and
 the 2009 IRS PUF.  It then plots these data to help in visualizing
 the relationshiop between average effective tax rates and marginal
 effective rates rates and income and age.
 
 This Python script calls the following functions:
-    
+
 
 This Python script outputs the following:
 
@@ -32,32 +32,45 @@ import matplotlib.pyplot as plt
 Load OSPC Tax Calculator Data into a Dataframe
 ------------------------------------------------------------------------
 '''
-A = pd.read_csv('/Users/jasondebacker/repos/microsimint/Data/2015_tau_n.csv')
-A['Wage + Self-Employed Income'] = A['Wage and Salaries'] + A['Self-Employed Income']
+data_path = '/Users/rwe2/Documents/OSPC/Data/micro-dynamic/2015_tau_n.csv'
+# data_path = '/Users/jasondebacker/repos/microsimint/Data/2015_tau_n.csv'
+A = pd.read_csv(data_path)
+A['Total Labor Income'] = A['Wage and Salaries'] + A['Self-Employed Income']
 A["Effective Tax Rate"] = A['Total Tax Liability']/A["Adjusted Total income"]
-A["Total Capital Income"] = A['Adjusted Total income'] - A['Wage + Self-Employed Income']
+A["Total Capital Income"] = A['Adjusted Total income'] - A['Total Labor Income']
+B = A[['Age', 'Total Labor Income', 'Total Capital Income',
+             'Adjusted Total income', 'Effective Tax Rate', 'Weights']]
 
-# print some descriptive stats
-print 'Descriptive statistics: ', A['Effective Tax Rate'].describe()
-
-
-'''
-------------------------------------------------------------------------
-Plot tax rates over income 
-------------------------------------------------------------------------
-'''
-
-# drop all obs with AETR > 1
-A = A.drop(A[A['Effective Tax Rate'] >1].index)
-# drop all obs with AETR < 0
-A = A.drop(A[A['Effective Tax Rate'] <0].index)
+# Clean up the data
+# drop all obs with AETR > 0.5
+B = B.drop(B[B['Effective Tax Rate'] >0.5].index)
+# drop all obs with AETR < -0.15
+B = B.drop(B[B['Effective Tax Rate'] < -0.15].index)
 # drop all obs with ATI < $5
-A = A.drop(A[A['Adjusted Total income'] <5].index)
-#  Keep just those aged 45-55
-A = A.drop(A[A['Age'] <45].index)
-A = A.drop(A[A['Age'] >55].index)
-A.plot(kind='scatter', x='Adjusted Total income', y='Effective Tax Rate', color='DarkGreen')
-plt.show()
+B = B.drop(B[B['Adjusted Total income'] <5].index)
 
+# Create an array of the different ages in the data
+min_age = int(np.maximum(B['Age'].min(),21))
+max_age = int(B['Age'].max())
+age_groups = np.arange(min_age, max_age+1)
 
+for i in age_groups:
+    df = B[B['Age'] == i]
+    # print some desciptive stats
+    print 'Descriptive Statistics for age == ', i
+    print df.describe()
 
+    # # plot effective tax rate as a function of adjusted total income
+    # # without truncating income
+    # B.plot(kind='scatter', x='Adjusted Total income',
+    #        y='Effective Tax Rate', color='DarkGreen')
+    # plt.title('Effective tax rate and ATI, age ' + str(i) + ', non-truncated income')
+    # plt.show()
+
+    # plot effective tax rate as a function of adjusted total income
+    # with truncated income
+    C = B.drop(B[B['Adjusted Total income'] >500000].index)
+    C.plot(kind='scatter', x='Adjusted Total income',
+           y='Effective Tax Rate', color='DarkGreen')
+    # plt.title('Effective tax rate and ATI, age ' + str(i) + ', truncated income')
+    plt.show()
