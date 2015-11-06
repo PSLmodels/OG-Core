8f15f6453c48823f4d600e8e8365f2a41137b0c9
talumbau <tj.alumbaugh@continuum.io>
2015-11-05 21:08:27 -0600

    Add setup.py, versioneer and MANIFEST.in
    - adds several files to facilitate making an installable package out of
      ogusa

--- setup.py
+++ setup.py
@@ -3,18 +3,25 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import versioneer
+
 
 config = {
     'description': 'dynamic scoring model using Overlapping Generations model for the USA',
     'url': 'https://github.com/open-source-economics/OG-USA/',
     'download_url': 'https://github.com/open-source-economics/OG-USA/',
-    'description':'ogusa',
+    'description': 'ogusa',
+    'package_dir': {'': 'Python'},
     'install_requires': ['scipy', 'numpy'],
     'version': '0.1',
     'license': 'MIT',
     'packages': ['ogusa'],
+    'package_dir': {'ogusa': 'Python/ogusa'},
+    'package_data': {'ogusa': ['data/ability/*', 'data/demographic/*', 'data/labor/*', 'data/wealth/*']},
     'include_package_data': True,
     'name': 'ogusa',
+    'version': versioneer.get_version(),
+    'cmdclass': versioneer.get_cmdclass(),
     'classifiers': [
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
