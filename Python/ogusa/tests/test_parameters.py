174775fe42f175af01b77402f685d7c937ae06fc
talumbau <tj.alumbaugh@continuum.io>
2015-12-29 13:13:49 -0600

    Add flags to get_parameters
    -'user_modifiable' - retrieve only user_modifiable parameters
    -'metadata' return metadata associated with parameter values

---
+++ Python/ogusa/tests/test_parameters.py
@@ -0,0 +1,34 @@
+import os
+import sys
+CUR_PATH = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(CUR_PATH, "../../"))
+import pytest
+import tempfile
+import pickle
+import numpy as np
+from ogusa.utils import comp_array
+from ogusa.utils import comp_scalar
+from ogusa.utils import dict_compare
+from ogusa.parameters import (get_reduced_parameters, USER_MODIFIABLE_PARAMS,
+                              get_full_parameters)
+
+
+def test_parameters_user_modifiable():
+    dd = get_reduced_parameters(False, user_modifiable=True, metadata=False)
+    assert set(dd.keys()) == set(USER_MODIFIABLE_PARAMS)
+    dd = get_full_parameters(False, user_modifiable=True, metadata=False)
+    assert set(dd.keys()) == set(USER_MODIFIABLE_PARAMS)
+
+
+def test_parameters_metadata():
+    dd_standard = get_reduced_parameters(False, user_modifiable=True, metadata=False)
+    dd_meta = get_reduced_parameters(False, user_modifiable=True, metadata=True)
+    for k, v in dd_meta.iteritems():
+        assert dd_standard[k] == dd_meta[k]['value']
+    assert set(dd_meta.keys()) == set(USER_MODIFIABLE_PARAMS)
+
+    dd_standard = get_full_parameters(False, user_modifiable=True, metadata=False)
+    dd_meta = get_full_parameters(False, user_modifiable=True, metadata=True)
+    for k, v in dd_meta.iteritems():
+        assert dd_standard[k] == dd_meta[k]['value']
+    assert set(dd_meta.keys()) == set(USER_MODIFIABLE_PARAMS)
