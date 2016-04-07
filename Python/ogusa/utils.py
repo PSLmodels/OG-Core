3b43058d9900b846ab4cbb2c0ac58d5772a07fc9
Jason DeBacker <jason.debacker@gmail.com>
2016-04-07 16:23:00 -0500

    updates to util.py

--- Python/ogusa/utils.py
+++ Python/ogusa/utils.py
@@ -1,8 +1,8 @@
 '''
 ------------------------------------------------------------------------
-Last updated 7/16/2015
+Last updated 4/7/2015
 
-Miscellaneous functions for SS and TPI.
+Miscellaneous functions for SS.py and TPI.py.
 
 This python files calls:
     OUTPUT/Saved_moments/wealth_data_moments.pkl
@@ -22,6 +22,19 @@ PATH_EXISTS_ERRNO = 17
 
 
 def mkdirs(path):
+    '''
+    Makes directories to save output.
+
+    Inputs:
+        path = string, path name for new directory
+
+    Functions called: None
+
+    Objects in function:
+
+    Returns: N/A
+    '''
+
     try:
         os.makedirs(path)
     except OSError as oe:
@@ -29,16 +42,24 @@ def mkdirs(path):
             pass
 
 
-def perc_dif_func(simul, data):
+def pct_dif_func(simul, data):
     '''
-    Used to calculate the absolute percent difference between the data
-    moments and model moments
+    Used to calculate the absolute percent difference between data
+    moments and model moments.
+
     Inputs:
-        simul = model moments (any shape)
-        data  = data moments (same shape as simul)
-    Output:
-        output = absolute percent difference between data and model moments (same shape as simul)
+        simul = any shape, model moments
+        data  = same shape as simul, data moments
+    
+    Functions called: None
+
+    Objects in function:
+        frac   = same shape as simul, percent difference between data and model moments
+        output = same shape as simul, absolute percent difference between data and model moments
+
+    Returns: output
     '''
+
     frac = (simul - data) / data
     output = np.abs(frac)
     return output
@@ -46,32 +67,53 @@ def perc_dif_func(simul, data):
 
 def convex_combo(var1, var2, nu):
     '''
-    Takes the convex combination of two variables, where nu is the value
-    between 0 and 1 in params.
+    Takes the convex combination of two variables, where nu is in [0,1].
+
     Inputs:
-        var1 = (any shape)
-        var2 = (same shape as var1)
-        params = parameters list from model (list) (only nu is needed...perhaps it should just take that as an input)
-    Outputs:
-        combo = convex combination of var1 and var2 (same shape as var1)
+        var1 = any shape, variable 1
+        var2 = same shape as var1, variable 2
+        nu   = scalar, weight on var1 in convex combination
+    
+    Functions called: None
+
+    Objects in function:
+        combo = same shape as var1, convex combination of var1 and var2
+
+    Returns: combo
     '''
+
     combo = nu * var1 + (1 - nu) * var2
     return combo
 
 
-def check_wealth_calibration(wealth_model, factor_model, params, wealth_dir):
+def check_wealth_calibration(wealth_model, factor_model, params):
     '''
     Creates a vector of the percent differences between the
     model and data wealth moments for the two age groups for
     each J group.
+
     Inputs:
-        wealth_model = model wealth levels (SxJ array)
-        factor_model = factor to convert wealth levels to dollars (scalar)
-        params = parameters list from model (list)
-        wealth_dir = path to the wealth data momenets
-    Outputs:
-        wealth_fits = Fits for how well the model wealth levels match the data wealth levels ((2*J)x1 array)
+        wealth_model = [S,J] array, model wealth levels
+        factor_model = scalar, factor to convert wealth levels to dollars
+        params       = length 2 tuple, (wealth_dir, J)
+        wealth_dir   = string, directory containing wealth data moments
+        J            = integer, number of lifetime income groups
+
+    Functions called: 
+        pct_dif_func
+
+    Objects in function:
+        wealth path          = string, path of pickle file with wealth data moments 
+        wealth_dict          = dictionary, contains wealth data moments
+        wealth_model         = [S,J] array, wealth holdings of model households
+        wealth_model_dollars = [S,J] array, wealth holdings of model households in dollars
+        wealth_fits          = [2*J,] vector, fits for how well the model wealth levels match the data wealth levels
+
+    Returns: wealth_fits
     '''
+
+    wealth_dir, J = params
+
     # Import the wealth data moments
     wealth_path = os.path.join(
         wealth_dir, "Saved_moments/wealth_data_moments.pkl")
@@ -80,10 +122,6 @@ def check_wealth_calibration(wealth_model, factor_model, params, wealth_dir):
     # for the calibration
     wealth_dict['wealth_data_array'][2:26, 0] = 500.0
 
-    J, S, T, BW, beta, sigma, alpha, Z, delta, ltilde, nu, g_y,\
-                  g_n_ss, tau_payroll, retire, mean_income_data,\
-                  h_wealth, p_wealth, m_wealth, b_ellipse, upsilon = params
-
     # Convert wealth levels from model to dollar terms
     wealth_model_dollars = wealth_model * factor_model
     wealth_fits = np.zeros(2 * J)
@@ -91,9 +129,9 @@ def check_wealth_calibration(wealth_model, factor_model, params, wealth_dir):
     #   The wealth_data_array moment indices are shifted because they start at age 18
     # The :: indices is so that we look at the 2 moments for the lowest group,
     # the 2 moments for the second lowest group, etc in order
-    wealth_fits[0::2] = perc_dif_func(np.mean(wealth_model_dollars[
+    wealth_fits[0::2] = pct_dif_func(np.mean(wealth_model_dollars[
                                       :24], axis=0), np.mean(wealth_dict['wealth_data_array'][2:26], axis=0))
-    wealth_fits[1::2] = perc_dif_func(np.mean(wealth_model_dollars[
+    wealth_fits[1::2] = pct_dif_func(np.mean(wealth_model_dollars[
                                       24:45], axis=0), np.mean(wealth_dict['wealth_data_array'][26:47], axis=0))
     return wealth_fits
 
@@ -101,8 +139,22 @@ def check_wealth_calibration(wealth_model, factor_model, params, wealth_dir):
 def read_file(path, fname):
     '''
     Read the contents of 'path'. If it does not exist, assume the file
-    is installed in a .egg file, and adjust accordingly
+    is installed in a .egg file, and adjust accordingly.
+
+    Inputs:
+        path  = string, path name for new directory
+        fname = string, filename
+
+    Functions called: None
+
+    Objects in function:
+        path_in_egg
+        buf
+        _bytes
+
+    Returns: file contents
     '''
+
     if not os.path.exists(os.path.join(path, fname)):
         path_in_egg = os.path.join("ogusa", fname)
         buf = resource_stream(Requirement.parse("ogusa"), path_in_egg)
@@ -117,6 +169,22 @@ def pickle_file_compare(fname1, fname2, tol=1e-3, exceptions={}, relative=False)
     Read two pickle files and unpickle each. We assume that each resulting
     object is a dictionary. The values of each dict are either numpy arrays
     or else types that are comparable with the == operator.
+
+    Inputs:
+        fname1  = string, file name of file 1
+        fname2  = string, file name of file 2
+        tol     = scalar, tolerance
+        exceptions = dictionary, exceptions 
+        relative = boolean, 
+
+    Functions called: 
+        dict_compare
+
+    Objects in function:
+        pkl1 =  dictionary, from first pickle file
+        pkl2 = dictionary, from second pickle file
+
+    Returns: difference between dictionaries
     '''
 
     pkl1 = pickle.load(open(fname1, 'rb'))
@@ -128,10 +196,23 @@ def pickle_file_compare(fname1, fname2, tol=1e-3, exceptions={}, relative=False)
 
 def comp_array(name, a, b, tol, unequal, exceptions={}, relative=False):
     '''
-        Compare two arrays in the L inifinity norm
-        Return True if | a - b | < tol, False otherwise
-        If not equal, add items to the unequal list
-        name: the name of the value being compared
+    Compare two arrays in the L inifinity norm
+    Return True if | a - b | < tol, False otherwise
+    If not equal, add items to the unequal list
+    name: the name of the value being compared
+    
+    Inputs:
+        
+
+    Functions called: 
+        
+
+    Objects in function:
+        
+
+    Returns: Boolean
+
+
     '''
 
     if name in exceptions:
@@ -165,9 +246,9 @@ def comp_array(name, a, b, tol, unequal, exceptions={}, relative=False):
 
 def comp_scalar(name, a, b, tol, unequal, exceptions={}, relative=False):
     '''
-        Compare two scalars in the L inifinity norm
-        Return True if abs(a - b) < tol, False otherwise
-        If not equal, add items to the unequal list
+    Compare two scalars in the L inifinity norm
+    Return True if abs(a - b) < tol, False otherwise
+    If not equal, add items to the unequal list
     '''
 
     if name in exceptions:
