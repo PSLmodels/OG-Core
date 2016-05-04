a0069cc6679059e01647b3f88e93eeb9cd511b27
Richard Evans <rickecon@gmail.com>
2016-05-04 17:20:58 -0600

    updated parameters.py to use demographics.py in TravisCI run instead of demog.py. Also deleted a bunch of extraneous pickles.

--- Python/ogusa/parameters.py
+++ Python/ogusa/parameters.py
@@ -45,11 +45,11 @@ def read_parameter_metadata():
     --------------------------------------------------------------------
     This function reads in parameter metadata
     --------------------------------------------------------------------
-    
+
     INPUTS: None
 
-    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: 
-    /PARAMS_FILE_METADATA_PATH/ = json file with metadata 
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
+    /PARAMS_FILE_METADATA_PATH/ = json file with metadata
 
     OBJECTS CREATED WITHIN FUNCTION:
     params_dict = dictionary of metadata
@@ -76,12 +76,12 @@ def read_tax_func_estimate(pickle_path, pickle_file):
     --------------------------------------------------------------------
     This function reads in tax function parameters
     --------------------------------------------------------------------
-    
-    INPUTS: 
+
+    INPUTS:
     pickle_path = string, path to pickle with tax function parameter estimates
     pickle_file = string, name of pickle file with tax function parmaeter estimates
 
-    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: 
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
     /picklepath/ = pickle file with dictionary of tax function estimated parameters
 
     OBJECTS CREATED WITHIN FUNCTION:
@@ -109,10 +109,10 @@ def get_parameters_from_file():
     --------------------------------------------------------------------
     This function loads the json file with model parameters
     --------------------------------------------------------------------
-    
+
     INPUTS: None
 
-    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: 
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
     /PARAMS_FILE/ = json file with model parameters
 
     OBJECTS CREATED WITHIN FUNCTION:
@@ -133,15 +133,15 @@ def get_parameters(baseline=False, guid='', user_modifiable=False, metadata=Fals
     --------------------------------------------------------------------
     This function returns the model parameters.
     --------------------------------------------------------------------
-    
-    INPUTS: 
+
+    INPUTS:
     baseline        = boolean, =True if run is of baseline policy
     guid            = string, id for model run
     user_modifiable = boolean, =True if allow user modifiable parameters
-    metadata        = boolean, =True if use metadata file for parameter 
+    metadata        = boolean, =True if use metadata file for parameter
                        values (rather than what is entered in parameters below)
 
-    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: 
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
     get_full_parameters()
     get_reduced_parameters()
 
@@ -171,7 +171,7 @@ Model Parameters:
 ------------------------------------------------------------------------
 S            = integer, number of economically active periods an individual lives
 J            = integer, number of different ability groups
-T            = integer, number of time periods until steady state is reached 
+T            = integer, number of time periods until steady state is reached
 BW           = integer, number of time periods in the budget window
 lambdas      = [J,] vector, percentiles for ability groups
 starting_age = integer, age agents enter population
@@ -180,7 +180,7 @@ E            = integer, age agents become economically active
 beta_annual  = scalar, discount factor as an annual rate
 beta         = scalar, discount factor for model period
 sigma        = scalar, coefficient of relative risk aversion
-alpha        = scalar, capital share of income 
+alpha        = scalar, capital share of income
 Z            = scalar, total factor productivity parameter in firms' production
                function
 delta_annual = scalar, depreciation rate as an annual rate
@@ -190,7 +190,7 @@ ltilde       = scalar, measure of time each individual is endowed with each
 g_y_annual   = scalar, annual growth rate of technology
 g_y          = scalar, growth rate of technology for a model period
 frisch       = scalar, Frisch elasticity that is used to fit ellipitcal utility
-               to constant Frisch elasticity function 
+               to constant Frisch elasticity function
 b_ellipse    = scalar, value of b for elliptical fit of utility function
 k_ellipse    = scalar, value of k for elliptical fit of utility function
 upsilon      = scalar, value of omega for elliptical fit of utility function
@@ -199,14 +199,14 @@ Tax Parameters:
 ------------------------------------------------------------------------
 mean_income_data = scalar, mean income from IRS data file used to calibrate income tax
 etr_params       = [S,BW,#tax params] array, parameters for effective tax rate function
-mtrx_params      = [S,BW,#tax params] array, parameters for marginal tax rate on 
+mtrx_params      = [S,BW,#tax params] array, parameters for marginal tax rate on
                     labor income function
-mtry_params      = [S,BW,#tax params] array, parameters for marginal tax rate on 
+mtry_params      = [S,BW,#tax params] array, parameters for marginal tax rate on
                     capital income function
 h_wealth         = scalar, wealth tax parameter h (scalar)
 m_wealth         = scalar, wealth tax parameter m (scalar)
 p_wealth         = scalar, wealth tax parameter p (scalar)
-tau_bq           = [J,] vector, bequest tax 
+tau_bq           = [J,] vector, bequest tax
 tau_payroll      = scalar, payroll tax rate
 retire           = integer, age at which individuals eligible for retirement benefits
 ------------------------------------------------------------------------
@@ -223,9 +223,9 @@ nu           = scalar, contraction parameter in SS and TPI iteration process
                representing the weight on the new distribution
 flag_graphs  = boolean, =True if produce graphs in demographic, income,
                wealth, and labor files (True=graph)
-chi_b_guess  = [J,] vector, initial guess of \chi^{b}_{j} parameters 
+chi_b_guess  = [J,] vector, initial guess of \chi^{b}_{j} parameters
                (if no calibration occurs, these are the values that will be used for \chi^{b}_{j})
-chi_n_guess  = [S,] vector, initial guess of \chi^{n}_{s} parameters 
+chi_n_guess  = [S,] vector, initial guess of \chi^{n}_{s} parameters
                (if no calibration occurs, these are the values that will be used for \chi^{n}_{s})
 ------------------------------------------------------------------------
 Demographics and Ability variables:
@@ -247,15 +247,15 @@ def get_reduced_parameters(baseline, guid, user_modifiable, metadata):
     This function sets the parameters for the reduced model, which is
     simplified to run more quickly for testing.
     --------------------------------------------------------------------
-    
+
     INPUTS:
     baseline        = boolean, =True if baseline tax policy, =False if reform
     guid            = string, id for reform run
     user_modifiable = boolean, =True if allow user modifiable parameters
-    metadata        = boolean, =True if use metadata file for parameter 
+    metadata        = boolean, =True if use metadata file for parameter
                        values (rather than what is entered in parameters below)
 
-    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: 
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
     read_tax_func_estimate()
     elliptical_u_est.estimation()
     read_parameter_metadata()
@@ -265,7 +265,7 @@ def get_reduced_parameters(baseline, guid, user_modifiable, metadata):
     allvars = dictionary, dictionary with all parameters defined in this function
 
     RETURNS: allvars
-    
+
     OUTPUT: None
     --------------------------------------------------------------------
     '''
@@ -275,7 +275,7 @@ def get_reduced_parameters(baseline, guid, user_modifiable, metadata):
     S = int(ending_age-starting_age)
     J = int(2)
     T = int(2 * S)
-    BW = int(10) 
+    BW = int(10)
     lambdas = np.array([.50, .50])
     E = int(starting_age * (S / float(ending_age - starting_age)))
     beta_annual = .96 # Carroll (JME, 2009)
@@ -350,10 +350,10 @@ def get_reduced_parameters(baseline, guid, user_modifiable, metadata):
     chi_n_guess = np.array([5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 
     # Generate Income and Demographic parameters
-    #omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector = get_omega(
-    #    S, T, starting_age, ending_age, E, flag_graphs)
-    omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector = get_pop_objs(
-        E, S, T, 0, 100, 2015, flag_graphs)
+    omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector = get_omega(
+       S, T, starting_age, ending_age, E, flag_graphs)
+    # omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector = get_pop_objs(
+    #     E, S, T, 0, 100, 2015, flag_graphs)
     e = np.array([[0.25, 1.25]] * 10)
     allvars = dict(locals())
 
@@ -374,15 +374,15 @@ def get_full_parameters(baseline, guid, user_modifiable, metadata):
     --------------------------------------------------------------------
     This function sets the parameters for the full model.
     --------------------------------------------------------------------
-    
+
     INPUTS:
     baseline        = boolean, =True if baseline tax policy, =False if reform
     guid            = string, id for reform run
     user_modifiable = boolean, =True if allow user modifiable parameters
-    metadata        = boolean, =True if use metadata file for parameter 
+    metadata        = boolean, =True if use metadata file for parameter
                        values (rather than what is entered in parameters below)
 
-    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: 
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
     read_tax_func_estimate()
     elliptical_u_est.estimation()
     read_parameter_metadata()
@@ -392,7 +392,7 @@ def get_full_parameters(baseline, guid, user_modifiable, metadata):
     allvars = dictionary, dictionary with all parameters defined in this function
 
     RETURNS: allvars
-    
+
     OUTPUT: None
     --------------------------------------------------------------------
     '''
@@ -400,7 +400,7 @@ def get_full_parameters(baseline, guid, user_modifiable, metadata):
     S = int(80)
     J = int(7)
     T = int(2 * S)
-    BW = int(10) 
+    BW = int(10)
     lambdas = np.array([.25, .25, .2, .1, .1, .09, .01])
     starting_age = 20
     ending_age = 100
@@ -486,7 +486,7 @@ def get_full_parameters(baseline, guid, user_modifiable, metadata):
     PLOT_TPI = False
     maxiter = 250
     mindist_SS = 1e-9
-    mindist_TPI = 2e-5 
+    mindist_TPI = 2e-5
     nu = .4
     flag_graphs = False
     #   Calibration parameters
@@ -496,21 +496,21 @@ def get_full_parameters(baseline, guid, user_modifiable, metadata):
     #chi_b_guess = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 4.0, 10.0])
     #chi_b_guess = np.array([5, 10, 90, 250, 250, 250, 250])
     #chi_b_guess = np.array([2, 10, 90, 350, 1700, 22000, 120000])
-    chi_n_guess = np.array([38.12000874, 33.22762421, 25.34842241, 26.67954008, 24.41097278, 
-                            23.15059004, 22.46771332, 21.85495452, 21.46242013, 22.00364263, 
-                            21.57322063, 21.53371545, 21.29828515, 21.10144524, 20.8617942, 
-                            20.57282, 20.47473172, 20.31111347, 19.04137299, 18.92616951, 
-                            20.58517969, 20.48761429, 20.21744847, 19.9577682, 19.66931057, 
-                            19.6878927, 19.63107201, 19.63390543, 19.5901486, 19.58143606, 
-                            19.58005578, 19.59073213, 19.60190899, 19.60001831, 21.67763741, 
-                            21.70451784, 21.85430468, 21.97291208, 21.97017228, 22.25518398, 
-                            22.43969757, 23.21870602, 24.18334822, 24.97772026, 26.37663164, 
-                            29.65075992, 30.46944758, 31.51634777, 33.13353793, 32.89186997, 
-                            38.07083882, 39.2992811, 40.07987878, 35.19951571, 35.97943562, 
-                            37.05601334, 37.42979341, 37.91576867, 38.62775142, 39.4885405, 
-                            37.10609921, 40.03988031, 40.86564363, 41.73645892, 42.6208256, 
-                            43.37786072, 45.38166073, 46.22395387, 50.21419653, 51.05246704, 
-                            53.86896121, 53.90029708, 61.83586775, 64.87563699, 66.91207845, 
+    chi_n_guess = np.array([38.12000874, 33.22762421, 25.34842241, 26.67954008, 24.41097278,
+                            23.15059004, 22.46771332, 21.85495452, 21.46242013, 22.00364263,
+                            21.57322063, 21.53371545, 21.29828515, 21.10144524, 20.8617942,
+                            20.57282, 20.47473172, 20.31111347, 19.04137299, 18.92616951,
+                            20.58517969, 20.48761429, 20.21744847, 19.9577682, 19.66931057,
+                            19.6878927, 19.63107201, 19.63390543, 19.5901486, 19.58143606,
+                            19.58005578, 19.59073213, 19.60190899, 19.60001831, 21.67763741,
+                            21.70451784, 21.85430468, 21.97291208, 21.97017228, 22.25518398,
+                            22.43969757, 23.21870602, 24.18334822, 24.97772026, 26.37663164,
+                            29.65075992, 30.46944758, 31.51634777, 33.13353793, 32.89186997,
+                            38.07083882, 39.2992811, 40.07987878, 35.19951571, 35.97943562,
+                            37.05601334, 37.42979341, 37.91576867, 38.62775142, 39.4885405,
+                            37.10609921, 40.03988031, 40.86564363, 41.73645892, 42.6208256,
+                            43.37786072, 45.38166073, 46.22395387, 50.21419653, 51.05246704,
+                            53.86896121, 53.90029708, 61.83586775, 64.87563699, 66.91207845,
                             68.07449767, 71.27919965, 73.57195873, 74.95045988, 76.62308152])
 
 
@@ -526,14 +526,14 @@ def get_full_parameters(baseline, guid, user_modifiable, metadata):
     # print 'surv diffs: ', (np.absolute(surv_rate- surv_rate2)).max()
     # print 'mort diffs: ', (np.absolute(rho- rho2)).max()
     # print 'g_n_TP diffs: ', (np.absolute(g_n_vector- g_n_vector2)).max()
-    # quit() 
+    # quit()
     #print 'omega_SS shape: ', omega_SS.shape
-    g_n_ss = 0.0   
+    g_n_ss = 0.0
     surv_rate1 = np.ones((S,))# prob start at age S
     surv_rate1[1:] = np.cumprod(surv_rate[:-1], dtype=float)
     omega_SS = np.ones(S)*surv_rate1# number of each age alive at any time
     omega_SS = omega_SS/omega_SS.sum()
-    
+
     omega = np.tile(np.reshape(omega_SS,(1,S)),(T+S,1))
     g_n_vector = np.tile(g_n_ss,(T+S,))
 
