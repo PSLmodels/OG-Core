still working...  scaling issue fixed, still getting nan's

Python/ogusa/KLP_TaxFuncPlot.py
@@ -10,77 +10,24 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from KLP_TaxFuncs import TaxFuncCES
 
-# def TaxFuncCD(x, y, par):
-
-#     # unpack par
-#     A = par[0]
-#     B = par[1]
-#     C = par[2]
-#     D = par[3]
-#     E = par[4]
-#     F = par[5]
-#     minx = par[6]
-#     miny = par[7]
-#     maxx = par[8]
-#     maxy = par[9]
-#     alf = par[10]
-
-#     # Calculate x and y Tax Functions
-#     taux = ((A*x**2 + B*x) / (A*x**2 + B*x + C))*(maxx - minx) + minx
-#     tauy = ((D*y**2 + E*y) / (D*y**2 + E*y + F))*(maxy - miny) + miny
-
-#     # Combine These via Cobb-Douglas aggregator
-#     tau = (taux - minx)**alf * (tauy - miny)**(1-alf)
-#     adj = alf*minx + (1-alf)*miny
-#     tau = tau + adj
-#     return tau
-
-
-def TaxFuncCES(x, y, par):
-
-    # unpack par
-    A = par[0]
-    B = par[1]
-    C = par[2]
-    D = par[3]
-    E = par[4]
-    F = par[5]
-    minx = par[6]
-    miny = par[7]
-    maxx = par[8]
-    maxy = par[9]
-    alf = par[10]
-    eta = par[11]
-
-    # Calculate x and y Tax Functions
-    taux = ((A*x**2 + B*x) / (A*x**2 + B*x + C))*(maxx - minx) + minx
-    tauy = ((D*y**2 + E*y) / (D*y**2 + E*y + F))*(maxy - miny) + miny
-
-    # Combine These via Cobb-Douglas aggregator
-    tau = (alf*(taux - minx)**eta + (1-alf)*(tauy - miny)**eta)**(1/eta)
-    adj = alf*minx + (1-alf)*miny
-    tau = tau + adj
-    return tau
-
 
 # Declare Function Parameters
-A = 1.0
-B = 1.0
-C = 1.0
-D = 1.0
-E = 1.0
-F = 1.0
-minx = -.1
-miny = -.1
-maxx = .5
-maxy = .25
+A = 1.0E-10
+B = 0.0001
+D = 1.0E-10
+E = 0.0001
+minx = 0.0
+miny = -.05
+maxx = .35
+maxy = .3
 alf = .5
 eta = 2.0
-par = np.array([A, B, C, D, E, F, minx, miny, maxx, maxy, alf, eta])
+par = np.array([A, B, D, E, minx, miny, maxx, maxy, alf, eta])
 
 # Grid Size
-xsize = 10
-ysize = 10
+xsize = 8
+ysize = 8
+scale = 20000
 
 # Initialize Matrix of Inputs and Tax Rates
 Xmat = np.zeros((xsize,ysize))
@@ -91,19 +38,10 @@ tauCES = np.zeros((xsize,ysize))
 # Calculate Taxes
 for x in range(0, xsize):
     for y in range(0, ysize):
-        Xmat[x, y] = x
-        Ymat[x, y] = y
+        Xmat[x, y] = x*scale
+        Ymat[x, y] = y*scale
         # tauCD[x, y] = TaxFuncCD(x, y, par)
-        tauCES[x, y] = TaxFuncCES(x, y, par)
-
-# Plot Surface Plot of Tax Rates
-# # Twice as wide as it is tall.
-# fig = plt.figure(figsize=plt.figaspect(0.5))
-
-# # CD plot
-# ax = fig.add_subplot(1, 2, 1, projection='3d')
-# surf = ax.plot_wireframe(Xmat, Ymat, tauCD)
-# plt.title('Cobb-Douglas')
+        tauCES[x, y] = TaxFuncCES(x*scale, y*scale, par)
 
 # CES plot
 fig = plt.figure()
@@ -113,3 +51,5 @@ surf = ax.plot_wireframe(Xmat, Ymat, tauCES)
 plt.title('CES')
 
 plt.show()
+
+print(tauCES)

Python/ogusa/KLP_fitnewfunc.py
@@ -22,7 +22,7 @@ def TaxSSD(x, y, par, nobs):
         pred = TaxFuncCES(x[i], y[i], par)
         SSD = SSD + (ATR[i] - pred)**2
 
-    print(par, SSD)
+    # print(par, SSD)
 
     return SSD
 
@@ -44,20 +44,21 @@ dimen = data.shape
 nobs = dimen[0]
 
 # choose guesses for parameter starting values
-parguess = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                     0.0, 0.0, .35, .35,
+parguess = np.array([1.0E-10, .0001, 1.0E-10, .0001,
+                     -.1, 0.0, .35, .25,
                      .5, 2.0])
 
 # choose min and max values for each parameter
-eps = .1
-parbounds = ((eps, None), (eps, None), (eps, None), (eps, None), (eps, None), (eps, None),
+eps = .000000001
+parbounds = ((eps, None), (eps, None), (eps, None), (eps, None),
           (eps - 1.0, 1.0 - eps), (eps - 1.0, 1.0 - eps), (eps - 1.0, 1.0 - eps), (eps - 1.0, 1.0 - eps),
-          (eps, 1.0 - eps), (1 + eps, None))
+          (eps, 1.0 - eps), (eps, None))
 
 # Lambda Function for Finding par using fmincon
 SSD = lambda par:TaxSSD(x, y, par, nobs)
-
 look = TaxSSD(x, y, parguess, nobs)
+print(look)
 
 # find best fitting parameters
 parfit = opt.minimize(SSD, parguess, bounds = parbounds, method = 'L-BFGS-B')
+print(parfit)