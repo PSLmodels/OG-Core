8b0956217a190a24c89dfe3f8fffbe8962a1bf80
Peter Steinberg <psteinberg@continuum.io>
2016-10-25 16:03:27 -0700

    try another way of Jenkins CI testing

---
+++ Python/regression/run_reforms_bash.sh
@@ -0,0 +1,12 @@
+export JENKINS_DOMAIN="http://54.159.16.16:8080"
+
+export r=reform
+submit_jobs(){
+    for reform in ${r}0 ${r}1 ${r}2 ${r}3 ${r}4 ${r}5 ${r}6 ${r}7 ${r}8 ${r}9 t1 t2;
+       do
+          export JENKINS_URL=${JENKINS_DOMAIN}/job/ci-mode-${reform}/build
+          curl "$JENKINS_URL" || return 1;
+       done
+}
+submit_jobs || echo "Failed on $JENKINS_URL"
+
