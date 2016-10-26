92506316a1c2a74651d3114755e239bf548d1358
Peter Steinberg <psteinberg@continuum.io>
2016-10-26 09:32:40 -0700

    make bash script use secret OSPC_API_KEY

--- Python/regression/run_reforms_bash.sh
+++ Python/regression/run_reforms_bash.sh
@@ -5,7 +5,10 @@ submit_jobs(){
     for reform in ${r}0 ${r}1 ${r}2 ${r}3 ${r}4 ${r}5 ${r}6 ${r}7 ${r}8 ${r}9 t1 t2;
        do
           export JENKINS_URL=${JENKINS_DOMAIN}/job/ci-mode-${reform}/build
-          curl "$JENKINS_URL" || return 1;
+          if [ "$OSPC_API_KEY" = "" ];then
+              echo OSPC_API_KEY is empty - wont work
+	  fi
+	  curl --user ospctaxbrain:$OSPC_API_KEY "$JENKINS_URL" || return 1;
        done
 }
 submit_jobs || echo "Failed on $JENKINS_URL"
