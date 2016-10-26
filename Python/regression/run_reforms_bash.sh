976a5bb4c84e167d1e203288675f6f363b1c1ddf
Peter Steinberg <psteinberg@continuum.io>
2016-10-26 14:11:34 -0700

    figure out git branch in CI

--- Python/regression/run_reforms_bash.sh
+++ Python/regression/run_reforms_bash.sh
@@ -2,14 +2,13 @@
 # TODO also these reforms: ${r}1 ${r}2 ${r}3 ${r}4 ${r}5 ${r}6 ${r}7 ${r}8 ${r}9 t1 t2
 
 export JENKINS_DOMAIN="http://54.159.16.16:8080"
-env | sort
 export r=reform
 submit_jobs(){
     for reform in ${r}0 ;
        do
           export token_part="token=${REMOTE_BUILD_TOKEN}"
           export cause="cause=Cause+CI+Build"
-          export branch="ogusabranch=${GIT_BRANCH}"
+          export branch="ogusabranch=${BRANCH_NAME}"
           export middle="/job/ci-mode-${reform}/build"
           export JENKINS_URL="${JENKINS_DOMAIN}${middle}?${token_part}&${branch}&{cause}"
           echo Attempt to curl $JENKINS_URL with OSPC_API_KEY secret
