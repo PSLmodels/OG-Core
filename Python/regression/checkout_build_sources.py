b7e98e261dfdad1322dd02845a7a460038d5aa27
Peter Steinberg <psteinberg@continuum.io>
2016-10-26 10:39:03 -0700

    fix bug in checkout_build_sources.py

--- Python/regression/checkout_build_sources.py
+++ Python/regression/checkout_build_sources.py
@@ -9,8 +9,8 @@ def get_ogusa_git_branch():
     return [line for line in run_cmd('git branch')
             if line.strip() and '*' == line.strip()[0]][0]
 
-
-REGRESSION_CONFIG = os.path.join(os.path.dirname(__file__), '.regression.yml')
+_d = os.path.dirname
+REGRESSION_CONFIG = os.path.join(_d(_d(_d(__file__))), '.regression.yml')
 REGRESSION_CONFIG = yaml.load(open(REGRESSION_CONFIG))
 REQUIRED = set(('compare_taxcalc_version',
                 'compare_ogusa_version',
