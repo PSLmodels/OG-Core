e732f05e03b46a89f0a8a44b10e135bd7f9e305a
Peter Steinberg <psteinberg@continuum.io>
2016-10-28 14:28:35 -0700

    set up now for all reforms - taken from .regression.txt

--- Python/regression/run_reforms_bash.sh
+++ Python/regression/run_reforms_bash.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# TODO also these reforms: ${r}1 ${r}2 ${r}3 ${r}4 ${r}5 ${r}6 ${r}7 ${r}8 ${r}9 t1 t2
 
 export JENKINS_DOMAIN="http://54.159.16.16:8080"
-export r=reform
+
 submit_jobs(){
-    for reform in ${r}0 ;
+    export REFORMS_TO_RUN=(cat ../../.regression.txt | grep reforms_to_run | sed 's/reforms_to_run//');
+    if [ "$REFORMS_TO_RUN" = "" ];then
+        echo Error - Must set REFORMS_TO_RUN env var - space separated list of reform strings;
+        return 1;
+    fi
+
+    for reform in $(echo $REFORMS_TO_RUN | tr " " "\n");
        do
-          export token_part="token=${REMOTE_BUILD_TOKEN}"
-          export cause="cause=Cause+CI+Build"
-          export branch="ogusabranch=${BRANCH_NAME}"
-          export middle="/job/ci-mode-simple-${reform}/buildWithParameters"
-          export JENKINS_URL="${JENKINS_DOMAIN}${middle}?${token_part}&${branch}&${cause}"
+          export token_part="token=${REMOTE_BUILD_TOKEN}";
+          export cause="cause=Cause+CI+Build";
+          export branch="ogusabranch=${BRANCH_NAME}";
+          export middle="/job/ci-mode-simple-${reform}/buildWithParameters";
+          export JENKINS_URL="${JENKINS_DOMAIN}${middle}?${token_part}&${branch}&${cause}";
           echo Attempt to curl $JENKINS_URL with OSPC_API_KEY secret
           if [ "$OSPC_API_KEY" = "" ];then
-              echo OSPC_API_KEY is empty - wont work
+              echo OSPC_API_KEY is empty - wont work;
     	  fi
 	   curl --user ospctaxbrain:$OSPC_API_KEY "$JENKINS_URL" || return 1;
        done
