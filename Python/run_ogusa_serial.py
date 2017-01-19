2cf07986c4733a2a0c9a3324253e3338b2416bcd
Jason DeBacker <jason.debacker@a02655jdebacker.attlocal.net>
2017-01-18 20:51:31 -0500

    restore default parameter values

--- Python/run_ogusa_serial.py
+++ Python/run_ogusa_serial.py
@@ -74,7 +74,7 @@ def run_micro_macro(user_params):
     kwargs={'output_base':output_base, 'baseline_dir':BASELINE_DIR,
             'baseline':True, 'analytical_mtrs':False, 'age_specific':True,
             'user_params':user_params,'guid':'',
-            'run_micro':False, 'small_open': True, 'budget_balance':True}
+            'run_micro':False, 'small_open': True, 'budget_balance':False}
     #p1 = Process(target=runner, kwargs=kwargs)
     #p1.start()
     runner(**kwargs)
