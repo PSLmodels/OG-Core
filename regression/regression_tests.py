c8b6599c2f98d8043dac82f2281fd2132daacd66
Henry Doupe <hdoupe@yahoo.com>
2017-10-30 19:28:38 -0400

    Add test for tpi output

--- regression/regression_tests.py
+++ regression/regression_tests.py
@@ -5,7 +5,7 @@ import pickle
 import pytest
 
 REG_BASELINE = '../regression/REG_OUTPUT_BASELINE'
-REG_REFORM = '../regression/REG_OUTPUT_REFORM_{id}'
+REG_REFORM = '../regression/REG_OUTPUT_REFORM_{ref_idx}'
 REF_IDXS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 #SWAP TO NEW DIRECTORY -- for test development only!!!!
@@ -17,11 +17,11 @@ def macro_outputs(request):
     (pct_changes,
         baseline_macros,
         policy_macros) = dump_diff_output(BASELINE,
-                                          REFORM.format(id=request.param))
+                                          REFORM.format(ref_idx=request.param))
     (reg_pct_changes,
         reg_baseline_macros,
         reg_policy_macros) = dump_diff_output(REG_BASELINE,
-                                              REG_REFORM.format(id=request.param))
+                                              REG_REFORM.format(ref_idx=request.param))
 
     return {"new":{
                    "pct_changes": pct_changes,
@@ -44,47 +44,37 @@ def test_macro_output(macro_outputs, output_var_idx):
     baseline_dir: directory for baseline input to compare to regression results
     reform_dir: directory for reform input to compare to regression results
     """
-
-    # output_vars = ["Y", "C", "I", "L", "w", "r", "Revenue"]
-
-    # for i in range(len(output_vars)):
     assert np.allclose(
         macro_outputs["new"]["pct_changes"][output_var_idx, :],
         macro_outputs["reg"]["pct_changes"][output_var_idx, :],
         atol=0.0, rtol=0.001
     )
 
-# def get_tpi_vars(path):
-#     with open(path, 'rb') as f:
-#         return pickle.loads(path)
-#
-# def comp_TPI_vars(baseline_dir, output_dir, ref_idx=0):
-#     """
-#     Compare TPI_vars
-#     """
-#     tpi = get_tpi_vars(path=REG )
+
+@pytest.fixture(scope="module", params=REF_IDXS + ["baseline"])
+def tpi_output(request):
+    def get_tpi_output(path):
+        with open(path + "/TPI/TPI_vars.pkl", 'rb') as f:
+            return pickle.load(f)
+
+    ref_idx = request.param
+    if ref_idx == "baseline":
+        return (get_tpi_output(REG_BASELINE), get_tpi_output(BASELINE))
+    else:
+        return (get_tpi_output(path=REG_REFORM.format(ref_idx=request.param)),
+                get_tpi_output(path=REFORM.format(ref_idx=request.param)))
 
 
-
-def plot_reforms(r1_ix, r2_ix=None, r3_ix=None):
-    plot_vars = ["Y", "C", "I", "L", "w", "r", "Revenue"]
-    plt.figure(figsize=(3, 3))
-    for i in range(0, len(plot_vars)):
-        t = list(range(12))
-        t = [n + 2016 for n in t]
-        plt.subplot(331 + i)
-        plt.plot(t, pct_changes[r1_ix][i,:],'-b', label='reform {}'.format(r1_ix))
-        if r2_ix is not None:
-            plt.plot(t, pct_changes[r2_ix][i,:], '-r', label='reform {}'.format(r2_ix))
-        if r3_ix is not None:
-            plt.plot(t, pct_changes[r3_ix][i,:], '-g', label='reform {}'.format(r3_ix))
-        plt.title("PCT Changes {0} from 2016 to steady state".format(plot_vars[i]))
-        plt.legend()
-    plt.show()
-    plt.figure()
+TPI_VARS = ['C', 'D', 'G', 'REVENUE', 'I', 'K', 'tax_path', 'L',
+            'eul_laborleisure', 'T_H', 'r', 'n_mat', 'BQ', 'w', 'Y',
+            'eul_savings', 'c_path', 'b_mat']
 
 
-# if __name__ == '__main__':
-#     pct_changes, baseline_macros, policy_macros = get_macros()
-#
-#     plot_reforms(9)
+@pytest.mark.parametrize("tpi_var", TPI_VARS)
+def test_tpi_vars(tpi_output, tpi_var):
+    """
+    Compare TPI_vars
+    """
+    reg = tpi_output[0][tpi_var]
+    new = tpi_output[1][tpi_var]
+    assert np.allclose(reg, new, atol=0.0, rtol=0.001)
