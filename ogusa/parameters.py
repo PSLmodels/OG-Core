24c0c4f7f1c9460ac77df4fb350e1636f3712b67
jdebacker <jason.debacker@gmail.com>
2017-11-29 19:24:58 -0500

    remove commenting for constant tax functions

--- ogusa/parameters.py
+++ ogusa/parameters.py
@@ -396,16 +396,16 @@ def get_parameters(test=False, baseline=False, guid='', user_modifiable=False, m
     mtry_params = dict_params['tfunc_mtry_params_S'][:S,:BW,:]
 
     # # Make all ETRs equal the average
-    # etr_params = np.zeros(etr_params.shape)
-    # etr_params[:, :, 10] = dict_params['tfunc_avg_etr'] # set shift to average rate
+    etr_params = np.zeros(etr_params.shape)
+    etr_params[:, :, 10] = dict_params['tfunc_avg_etr'] # set shift to average rate
 
     # # Make all MTRx equal the average
-    # mtrx_params = np.zeros(mtrx_params.shape)
-    # mtrx_params[:, :, 10] = dict_params['tfunc_avg_mtrx'] # set shift to average rate
+    mtrx_params = np.zeros(mtrx_params.shape)
+    mtrx_params[:, :, 10] = dict_params['tfunc_avg_mtrx'] # set shift to average rate
 
     # # Make all MTRy equal the average
-    # mtry_params = np.zeros(mtry_params.shape)
-    # mtry_params[:, :, 10] = dict_params['tfunc_avg_mtry'] # set shift to average rate
+    mtry_params = np.zeros(mtry_params.shape)
+    mtry_params[:, :, 10] = dict_params['tfunc_avg_mtry'] # set shift to average rate
 
     # # Make MTRx depend only on labor income
     # mtrx_params[:, :, 11] = 1.0 # set share parameter to 1
