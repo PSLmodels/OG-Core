0a8c30b47f68a3cc9c89e37f6f51d41011f08ac7
jdebacker <jason.debacker@gmail.com>
2018-02-01 15:44:01 -0500

    use pytest parameterize instead of loops

--- ogusa/tests/test_user_inputs.py
+++ ogusa/tests/test_user_inputs.py
@@ -6,33 +6,34 @@ from ogusa import SS, TPI
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 PUF_PATH = os.path.join(CUR_PATH, '../puf.csv')
 
+
 @pytest.mark.full_run
-def test_frisch():
+@pytest.mark.parametrize('frisch', [0.1, 0.4, 0.8],
+                         ids=['Frisch 0.1', 'Frisch 0.4', 'Frisch 0.8'])
+def test_frisch(frisch):
     from ogusa.scripts.execute import runner
     output_base = "./OUTPUT"
     input_dir = "./OUTPUT"
-    for frisch in [0.1, 0.4, 0.8]:
-        print('Frisch elasticity = ', frisch)
-        user_params = {'frisch': frisch, 'debt_ratio_ss': 1.0,
-                       'start_year': 2018}
-        runner(output_base=output_base, baseline_dir=input_dir, test=False,
-               time_path=False, baseline=True, age_specific=True,
-               user_params=user_params, run_micro=False,
-               small_open=False, budget_balance=False, data=PUF_PATH)
+    user_params = {'frisch': frisch, 'debt_ratio_ss': 1.0}
+    runner(output_base=output_base, baseline_dir=input_dir, test=False,
+           time_path=False, baseline=True, age_specific=True,
+           user_params=user_params, run_micro=False,
+           small_open=False, budget_balance=False, data=PUF_PATH)
+
 
 @pytest.mark.full_run
-def test_gy():
+@pytest.mark.parametrize('g_y_annual', [-0.01,  0.0, 0.03, 0.08, 0.2, 1.0],
+                         ids=['-0.01', '0.0', '0.03', '0.08', '0.2', '1.0'])
+def test_gy(g_y_annual):
     from ogusa.scripts.execute import runner
     # # Monkey patch enforcement flag since small data won't pass checks
     # SS.ENFORCE_SOLUTION_CHECKS = True
     # TPI.ENFORCE_SOLUTION_CHECKS = True
     output_base = "./OUTPUT"
     input_dir = "./OUTPUT"
-    for gy in [-0.01,  0.0, 0.03, 0.08, 0.2, 1.0]:
-        print('Growth rates = ', gy)
-        user_params = {'frisch': 0.41, 'debt_ratio_ss': 1.0,
-                       'start_year': 2018, 'g_y_annual': gy}
-        runner(output_base=output_base, baseline_dir=input_dir, test=False,
-               time_path=False, baseline=True, age_specific=True,
-               user_params=user_params, run_micro=False,
-               small_open=False, budget_balance=False, data=PUF_PATH)
+    user_params = {'frisch': 0.41, 'debt_ratio_ss': 1.0,
+                   'g_y_annual': g_y_annual}
+    runner(output_base=output_base, baseline_dir=input_dir, test=False,
+           time_path=False, baseline=True, age_specific=True,
+           user_params=user_params, run_micro=False,
+           small_open=False, budget_balance=False, data=PUF_PATH)
