7514ecb90e2ea92ba5e94e48769816c7023138f1
jdebacker <jason.debacker@gmail.com>
2018-02-01 12:47:32 -0500

    use decorator instread of loop

--- ogusa/tests/test_startyears.py
+++ ogusa/tests/test_startyears.py
@@ -6,18 +6,21 @@ from ogusa import SS, TPI
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 PUF_PATH = os.path.join(CUR_PATH, '../puf.csv')
 
-def test_diff_start_year():
+
+@pytest.mark.parametrize(
+        'year',
+        [2013, 2017, 2026],
+        ids=['2013', '2017', '2026'])
+def test_diff_start_year(year):
     from ogusa.scripts.execute import runner
     # Monkey patch enforcement flag since small data won't pass checks
     SS.ENFORCE_SOLUTION_CHECKS = False
     TPI.ENFORCE_SOLUTION_CHECKS = False
     output_base = "./OUTPUT"
     input_dir = "./OUTPUT"
-    for year in [2013, 2017, 2026]:
-        print('year = ', year)
-        user_params = {'frisch': 0.41, 'debt_ratio_ss': 1.0,
-                       'start_year': year}
-        runner(output_base=output_base, baseline_dir=input_dir, test=True,
-               time_path=True, baseline=True, age_specific=False,
-               user_params=user_params, run_micro=True,
-               small_open=False, budget_balance=False, data=PUF_PATH)
+    user_params = {'frisch': 0.41, 'debt_ratio_ss': 1.0,
+                   'start_year': year}
+    runner(output_base=output_base, baseline_dir=input_dir, test=True,
+           time_path=True, baseline=True, age_specific=False,
+           user_params=user_params, run_micro=True,
+           small_open=False, budget_balance=False, data=PUF_PATH)
