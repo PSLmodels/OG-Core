db7d67c2bb96e4f4aa9f886ce5c4c7da772bbef8
jdebacker <jason.debacker@gmail.com>
2018-02-01 09:23:33 -0500

    delete commented out lines

--- ogusa/tests/test_startyears.py
+++ ogusa/tests/test_startyears.py
@@ -21,38 +21,3 @@ def test_diff_start_year():
                time_path=True, baseline=True, age_specific=False,
                user_params=user_params, run_micro=True,
                small_open=False, budget_balance=False, data=PUF_PATH)
-
-#
-# def test_get_micro_data_get_calculator():
-#
-#     reform = {
-#     2017: {
-#         '_II_rt1': [.09],
-#         '_II_rt2': [.135],
-#         '_II_rt3': [.225],
-#         '_II_rt4': [.252],
-#         '_II_rt5': [.297],
-#         '_II_rt6': [.315],
-#         '_II_rt7': [0.3564],
-#     }, }
-#
-#     calc = get_calculator(baseline=False, calculator_start_year=2017,
-#                           reform=reform, data=TAXDATA,
-#                           weights=WEIGHTS, records_start_year=2009)
-#     assert calc.current_year == 2017
-#
-#     reform = {
-#     2017: {
-#         '_II_rt1': [.09],
-#         '_II_rt2': [.135],
-#         '_II_rt3': [.225],
-#         '_II_rt4': [.252],
-#         '_II_rt5': [.297],
-#         '_II_rt6': [.315],
-#         '_II_rt7': [0.3564]
-#     }, }
-#
-#     calc2 = get_calculator(baseline=False, calculator_start_year=2017,
-#                            reform=reform, data=TAXDATA,
-#                            weights=WEIGHTS, records_start_year=2009)
-#     assert calc2.current_year == 2017
