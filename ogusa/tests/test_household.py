f4460c2991a370d01e0e8ade4d9a4d4db0dace74
jdebacker <jason.debacker@gmail.com>
2018-02-07 21:32:35 -0500

    test MU_l

--- ogusa/tests/test_household.py
+++ ogusa/tests/test_household.py
@@ -18,11 +18,29 @@ def test_marg_ut_cons(c, sigma, expected):
     assert np.allclose(test_value, expected)
 
 
-# def test_marg_ut_labor():
-#
-#     assert np.allclose()
-#
-#
+# Note that params tuple in order: b_ellipse, upsilon, ltilde, chi_n
+test_data = [(0.87, (0.527, 1.497, 1.0, 3.3), 2.825570309),
+             (0.0, (0.527, 1.497, 1.0, 3.3), 0),
+             (0.99999, (0.527, 1.497, 1.0, 3.3), 69.52423604),
+             (0.00001, (0.527, 1.497, 1.0, 3.3), 0.005692782),
+             (0.8, (0.527, 0.9, 1.0, 3.3), 1.471592068),
+             (0.8, (0.527, 0.9, 2.3, 3.3), 0.795937549),
+             (0.8, (2.6, 1.497, 1.0, 3.3), 11.66354267),
+             (np.array([[0.8, 0.9, 0.3], [0.5, 0.2, 0.99]]),
+              (0.527, 1.497, 1.0, 3.3),
+              np.array([[2.364110379, 3.126796062, 1.014935377],
+                        [1.4248841, 0.806333875, 6.987729463]]))]
+
+
+@pytest.mark.parametrize('n,params,expected', test_data,
+                         ids=['1', '2', '3', '4', '5', '6', '7', '8'])
+def test_marg_ut_labor(n, params, expected):
+    # Test marginal utility of labor calculation
+    test_value = household.marg_ut_labor(n, params)
+
+    assert np.allclose(test_value, expected)
+
+
 # def test_get_cons():
 #
 #     assert np.allclose()
