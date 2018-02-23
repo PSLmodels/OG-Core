4001fd322423b137f1168c2afada656d1b9f68e6
jdebacker <jason.debacker@gmail.com>
2018-02-23 09:36:04 -0500

    succes with pickles

--- ogusa/tests/test_SS.py
+++ ogusa/tests/test_SS.py
@@ -1,23 +1,43 @@
 import pytest
 import json
-import numpy
+import pickle
+import numpy as np
 import os
 from ogusa import SS
-os.path.abspath(os.path.dirname(__file__))
 
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+# def test_SS_fsolve():
+#     # Test SS.SS_fsolve function.  Provide inputs to function and
+#     # ensure that output returned matches what it has been before.
+#     input_tuple = tuple(json.load(open(os.path.join(
+#         CUR_PATH, 'SS_fsolve_inputs.json'))))
+#     guesses, params = input_tuple
+#     params = tuple(params)
+#     bssmat = np.array(params[0])
+#     nssmat = np.array(params[1])
+#
+#     (bssmat, nssmat, chi_params, ss_params, income_tax_params,
+#      iterative_params, small_open_params) = params
+#     print 'Guesses = ', type(params[0]), type(params[1])
+#     test_list = SS.SS_fsolve(guesses, params)
+#
+#     expected_list = json.load(open(os.path.join(
+#         CUR_PATH, 'SS_fsolve_outputs.json')))
+#
+#     assert(np.allclose(np.array(test_list), np.array(expected_list)))
 
 
 def test_SS_fsolve():
     # Test SS.SS_fsolve function.  Provide inputs to function and
     # ensure that output returned matches what it has been before.
-    input_tuple = tuple(json.load(open(os.path.join(
-        CUR_PATH, 'SS_fsolve_inputs.json'))))
-
+    input_tuple = pickle.load(open(os.path.join(
+        CUR_PATH, 'SS_fsolve_inputs.pkl'), 'rb'))
     guesses, params = input_tuple
     test_list = SS.SS_fsolve(guesses, params)
 
-    expected_list = tuple(json.load(open(os.path.join(
-        CUR_PATH, 'SS_fsolve_outputs.json'))))
+    expected_list = pickle.load(open(os.path.join(
+        CUR_PATH, 'SS_fsolve_outputs.pkl'), 'rb'))
 
-    assert(numpy.allclose(np.array(test_list), np.array(expected_list)))
+    assert(np.allclose(np.array(test_list), np.array(expected_list)))
