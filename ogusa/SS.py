fix broadcasting issues for SS solution

ogusa/SS.py
@@ -144,12 +144,12 @@ def euler_equation_solver(guesses, *args):
     error2[mask4] = 1e14
 
     tax1 = tax.total_taxes(r, w, b_s, n_guess, BQ, factor, T_H, theta, j, False,
-                           p, 'SS')
-    cons = household.get_cons(r, w, b_s, b_splus1, n_guess, BQ, tax1, j, p)
+                           p.etr_params[:, -1, :], p, 'SS')   ## only works with SS because of etr params
+    cons = household.get_cons(r, w, b_s, b_splus1, n_guess, BQ, tax1, j, p.e[:, j], p)
     mask6 = cons < 0
     error1[mask6] = 1e14
 
-    return list(error1.flatten()) + list(error2.flatten())
+    return np.hstack((error1, error2))#list(error1.flatten()) + list(error2.flatten())
 
 
 def inner_loop(outer_loop_vars, p, client):
@@ -208,21 +208,34 @@ def inner_loop(outer_loop_vars, p, client):
     #                         full_output=True)
     # results = p.client.gather(lazy_values)
 
+    # lazy_values = []
+    # for j in range(p.J):
+    #     # guesses = np.append(bssmat[:, j], nssmat[:, j])
+    #     guesses = np.hstack((bssmat[:, j], nssmat[:, j]))
+    #     print('length guesses: ', guesses.shape)
+    #     euler_args = (r, w, T_H, factor, j, p)
+    #     lazy_values.append(client.submit(
+    #         opt.fsolve, euler_equation_solver, guesses * 0.9,
+    #         args=euler_args, xtol=MINIMIZER_TOL, full_output=True))
+    #
+    # results = client.gather(lazy_values)
+    #
+    # for j, result in enumerate(results):
+    #     [solutions, infodict, ier, message] = result
+    #     euler_errors[:, j] = infodict['fvec']
+    #     bssmat[:, j] = solutions[:p.S]
+    #     nssmat[:, j] = solutions[p.S:]
     lazy_values = []
     for j in range(p.J):
-        guesses = np.append(bssmat[:, j], nssmat[:, j])
+        # guesses = np.append(bssmat[:, j], nssmat[:, j])
+        guesses = np.hstack((bssmat[:, j], nssmat[:, j]))
         euler_args = (r, w, T_H, factor, j, p)
-        lazy_values.append(client.submit(
-            opt.fsolve, euler_equation_solver, guesses * 0.9,
-            args=euler_args, xtol=MINIMIZER_TOL, full_output=True))
-
-    results = client.gather(lazy_values)
-
-    for j, result in enumerate(results):
-        [solutions, infodict, ier, message] = result
-        euler_errors[:, j] = infodict['fvec']
-        bssmat[:, j] = solutions[:p.S]
-        nssmat[:, j] = solutions[p.S:]
+        results = opt.root(euler_equation_solver, guesses * 0.9,
+                           method='lm', args=euler_args,
+                           tol=MINIMIZER_TOL)
+        bssmat[:, j] = results.x[:p.S]
+        nssmat[:, j] = results.x[p.S:]
+        euler_errors[:, j] = results.fun
 
     L = aggr.get_L(nssmat, p, 'SS')
     if not p.small_open:
@@ -236,7 +249,7 @@ def inner_loop(outer_loop_vars, p, client):
     new_Y = firm.get_Y(K, L, p)
     if p.budget_balance:
         Y = new_Y
-    if not p.mall_open:
+    if not p.small_open:
         new_r = firm.get_r(Y, K, p)
         # new_w = firm.get_w_from_r(new_r, p)
     else:
@@ -253,8 +266,8 @@ def inner_loop(outer_loop_vars, p, client):
         new_factor = p.mean_income_data / average_income_model
     else:
         new_factor = factor
-    new_BQ = aggr.get_BQ(new_r, bssmat, p, 'SS')
-    theta = tax.replacement_rate_vals(nssmat, new_w, new_factor, p)
+    new_BQ = aggr.get_BQ(new_r, bssmat, None, p, 'SS')
+    theta = tax.replacement_rate_vals(nssmat, new_w, new_factor, None, p)
 
     if p.budget_balance:
         new_T_H = aggr.revenue(new_r, new_w, b_s, nssmat, new_BQ, new_Y,
@@ -268,7 +281,7 @@ def inner_loop(outer_loop_vars, p, client):
         new_T_H, new_Y, new_factor, new_BQ, average_income_model
 
 
-def SS_solver(bmat, nmat, r, T_H, factor, Y, p, fsolve_flag=False):
+def SS_solver(bmat, nmat, r, T_H, factor, Y, p, client, fsolve_flag=False):
     '''
     --------------------------------------------------------------------
     Solves for the steady state distribution of capital, labor, as well
@@ -357,7 +370,7 @@ def SS_solver(bmat, nmat, r, T_H, factor, Y, p, fsolve_flag=False):
             outer_loop_vars = (bmat, nmat, r, Y, T_H, factor)
         (euler_errors, new_bmat, new_nmat, new_r, new_w, new_T_H, new_Y,
          new_factor, new_BQ, average_income_model) =\
-            inner_loop(outer_loop_vars, p)
+            inner_loop(outer_loop_vars, p, client)
 
         r = utils.convex_combo(new_r, r, nu_ss)
         factor = utils.convex_combo(new_factor, factor, nu_ss)
@@ -414,7 +427,7 @@ def SS_solver(bmat, nmat, r, T_H, factor, Y, p, fsolve_flag=False):
         else:
             debt_ss = p.debt_ratio_ss * Y
         Kss = Bss - debt_ss
-        Iss = aggr.get_I(bssmat_splus1, p, 'SS')
+        Iss = aggr.get_I(bssmat_splus1, Kss, Kss, p, 'SS')
     else:
         # Compute capital (K) and wealth (B) separately
         Kss = firm.get_K(Lss, p.ss_firm_r, p)
@@ -429,7 +442,7 @@ def SS_solver(bmat, nmat, r, T_H, factor, Y, p, fsolve_flag=False):
 
     Yss = firm.get_Y(Kss, Lss, p)
     BQss = new_BQ
-    theta = tax.replacement_rate_vals(nssmat, wss, factor_ss, p)
+    theta = tax.replacement_rate_vals(nssmat, wss, factor_ss, None, p)
 
     revenue_ss = aggr.revenue(rss, wss, bssmat_s, nssmat, BQss, Yss,
                               Lss, Kss, factor, theta, p, 'SS')
@@ -444,7 +457,7 @@ def SS_solver(bmat, nmat, r, T_H, factor, Y, p, fsolve_flag=False):
 
     # solve resource constraint
     etr_params_3D = np.tile(np.reshape(
-        p.etr_params, (p.S, 1, p.etr_params.shape[1])), (1, p.J, 1))
+        p.etr_params[:, -1, :], (p.S, 1, p.etr_params.shape[2])), (1, p.J, 1))
     # mtrx_params_3D = np.tile(
     #     np.reshape(p.mtrx_params, (p.S, 1, p.mtrx_params.shape[1])), (1, p.J, 1))
 
@@ -492,14 +505,14 @@ def SS_solver(bmat, nmat, r, T_H, factor, Y, p, fsolve_flag=False):
 
     # solve resource constraint
     taxss = tax.total_taxes(rss, wss, bssmat_s, nssmat, BQss, factor_ss,
-                            T_Hss, theta, None, False, p, etr_params_3D, 'SS')
+                            T_Hss, theta, None, False, etr_params_3D, p, 'SS')
     cssmat = household.get_cons(rss, wss, bssmat_s, bssmat_splus1,
-                                nssmat, BQss.reshape(1, p.J), taxss,
-                                p, p.lambdas.reshape(1, p.J))
+                                nssmat, BQss.reshape(1, p.J), taxss, None,
+                                p.e, p)
     business_revenue = tax.get_biz_tax(wss, Yss, Lss, Kss, p)
     IITpayroll_revenue = revenue_ss - business_revenue
 
-    Css = aggr.get_C(cssmat, p.omega_SS.reshape(p.S, 1), p.lambdas, 'SS')
+    Css = aggr.get_C(cssmat, p, 'SS')
 
     # solve resource constraint
     if p.small_open:
@@ -653,7 +666,7 @@ def SS_fsolve_reform(guesses, *args):
         solutions = steady state values of b, n, w, r, factor,
                     T_H ((2*S*J+4)x1 array)
     '''
-    (bssmat, nssmat, factor, p) = args
+    (bssmat, nssmat, factor, p, client) = args
 
     # Rename the inputs
     r = guesses[0]
@@ -669,7 +682,7 @@ def SS_fsolve_reform(guesses, *args):
 
     (euler_errors, bssmat, nssmat, new_r, new_w, new_T_H, new_Y,
      new_factor, new_BQ, average_income_model) =\
-        inner_loop(outer_loop_vars, p)
+        inner_loop(outer_loop_vars, p, client)
     error1 = new_r - r
     if p.budget_balance:
         error2 = new_T_H - T_H
@@ -715,7 +728,7 @@ def SS_fsolve_reform_baselinespend(guesses, *args):
         solutions = steady state values of b, n, w, r, factor,
                     T_H ((2*S*J+4)x1 array)
     '''
-    (bssmat, nssmat, T_Hss, factor_ss, p) = args
+    (bssmat, nssmat, T_Hss, factor_ss, p, client) = args
 
     # Rename the inputs
     r = guesses[0]
@@ -726,7 +739,7 @@ def SS_fsolve_reform_baselinespend(guesses, *args):
     outer_loop_vars = (bssmat, nssmat, r, Y, T_Hss, factor_ss)
     (euler_errors, bssmat, nssmat, new_r, new_w, new_T_H, new_Y,
      new_factor, new_BQ, average_income_model) =\
-        inner_loop(outer_loop_vars, p)
+        inner_loop(outer_loop_vars, p, client)
 
     error1 = new_r - r
     error2 = new_Y - Y
@@ -812,7 +825,7 @@ def run_SS(p, client=None):
         fsolve_flag = True
         # Return SS values of variables
         output = SS_solver(b_guess, n_guess, rss, T_Hss, factor_ss, Yss, p,
-                           fsolve_flag)
+                           client, fsolve_flag)
         # print("solved output", wss, rss, T_Hss, factor_ss)
         # print('analytical mtrs in SS: ', analytical_mtrs)
         # print('tax function type in SS: ', tax_func_type)
@@ -824,7 +837,7 @@ def run_SS(p, client=None):
              ss_solutions['Yss'], ss_solutions['factor_ss'])
         if p.baseline_spending:
             T_Hss = T_Hguess
-            ss_params_reform = [b_guess, n_guess, T_Hss, factor, p]
+            ss_params_reform = [b_guess, n_guess, T_Hss, factor, p, client]
             guesses = [rguess, Yguess]
             [solutions_fsolve, infodict, ier, message] =\
                 opt.fsolve(SS_fsolve_reform_baselinespend, guesses,
@@ -832,7 +845,7 @@ def run_SS(p, client=None):
                            full_output=True)
             [rss, Yss] = solutions_fsolve
         else:
-            ss_params_reform = [b_guess, n_guess, factor, p]
+            ss_params_reform = [b_guess, n_guess, factor, p, client]
             guesses = [rguess, T_Hguess]
             [solutions_fsolve, infodict, ier, message] =\
                 opt.fsolve(SS_fsolve_reform, guesses,
@@ -848,7 +861,7 @@ def run_SS(p, client=None):
         fsolve_flag = True
         # Return SS values of variables
         output = SS_solver(b_guess, n_guess, rss, T_Hss, factor, Yss, p,
-                           fsolve_flag)
+                           client, fsolve_flag)
         if output['Gss'] < 0.:
             warnings.warn('Warning: The combination of the tax policy '
                           + 'you specified and your target debt-to-GDP '

ogusa/aggregates.py
@@ -17,7 +17,7 @@ from . import tax
 ------------------------------------------------------------------------
 '''
 
-def get_L(n, params):
+def get_L(n, p, method):
     '''
     Generates vector of aggregate labor supply.
 
@@ -38,17 +38,18 @@ def get_L(n, params):
     Returns: L
 
     '''
-    e, omega, lambdas, method = params
 
-    L_presum = e * omega * lambdas * n
+
     if method == 'SS':
+        L_presum = p.e * np.transpose(p.omega_SS * p.lambdas) * n
         L = L_presum.sum()
     elif method == 'TPI':
+        L_presum = p.e * p.omega * p.lambdas * n
         L = L_presum.sum(1).sum(1)
     return L
 
 
-def get_I(b_splus1, K_p1, K, params):
+def get_I(b_splus1, K_p1, K, p, method):
     '''
     Generates vector of aggregate investment.
 
@@ -68,32 +69,29 @@ def get_I(b_splus1, K_p1, K, params):
     Returns: aggI
 
     '''
-    delta, g_y, omega, lambdas, imm_rates, g_n, method = params
     if method == 'SS':
-        omega_extended = np.append(omega[1:], [0.0])
-        imm_extended = np.append(imm_rates[1:], [0.0])
-        part2 = ((((b_splus1 *
-                   (omega_extended*imm_extended).reshape(omega.shape[0], 1)) *
-                   lambdas).sum())/(1+g_n))
-        aggI = (1+g_n)*np.exp(g_y)*(K_p1 - part2) - (1.0 - delta) * K
+        omega_extended = np.append(p.omega_SS[1:], [0.0])
+        imm_extended = np.append(p.imm_rates[-1, 1:], [0.0])
+        part2 = (((b_splus1 *
+                   np.transpose((omega_extended * imm_extended) *
+                   p.lambdas)).sum()) / (1 + p.g_n_ss))
+        aggI = (1 + p.g_n_ss) * np.exp(p.g_y) * (K_p1 - part2) - (1.0 - p.delta) * K
     elif method == 'TPI':
-        # omega_extended = np.append(omega[1:,:,:],np.zeros((1,omega.shape[1],omega.shape[2])),axis=0)
-        # imm_extended = np.append(imm_rates[1:,:,:],np.zeros((1,imm_rates.shape[1],imm_rates.shape[2])),axis=0)
-        # part2 = ((b_splus1*omega_extended*imm_extended*lambdas).sum(1).sum(1))/(1+g_n)
-        omega_shift = np.append(omega[:, 1:, :],
-                                np.zeros((omega.shape[0], 1, omega.shape[2]))
+        omega_shift = np.append(p.omega[:, 1:, :],
+                                np.zeros((p.omega.shape[0], 1, p.omega.shape[2]))
                                 , axis=1)
-        imm_shift = np.append(imm_rates[:, 1:, :],
-                              np.zeros((imm_rates.shape[0],
-                                       1, imm_rates.shape[2])),
+        imm_shift = np.append(p.imm_rates[:, 1:, :],
+                              np.zeros((p.imm_rates.shape[0],
+                                       1, p.imm_rates.shape[2])),
                               axis=1)
-        part2 = (((b_splus1*imm_shift*omega_shift*lambdas).sum(1).sum(1)) /
-                 (1+g_n))
-        aggI = (1+g_n)*np.exp(g_y)*(K_p1 - part2) - (1.0 - delta) * K
+        part2 = (((b_splus1 * imm_shift * omega_shift * p.lambdas).sum(1).sum(1)) /
+                 (1 + p.g_n_ss))
+        aggI = (1 + p.g_n_ss) * np.exp(p.g_y) * (K_p1 - part2) - (1.0 - p.delta) * K
 
     return aggI
 
-def get_K(b, params):
+
+def get_K(b, p, method):
     '''
     Calculates aggregate capital supplied.
 
@@ -114,26 +112,25 @@ def get_K(b, params):
     Returns: K
     '''
 
-    omega, lambdas, imm_rates, g_n, method = params
-
     if method == 'SS':
-        part1 = b * omega * lambdas
-        omega_extended = np.append(omega[1:],[0.0])
-        imm_extended = np.append(imm_rates[1:],[0.0])
-        part2 = b*(omega_extended*imm_extended).reshape(omega.shape[0],1)*lambdas
-        K_presum = part1+part2
+        part1 = b * np.transpose(p.omega_SS * p.lambdas)
+        omega_extended = np.append(p.omega_SS[1:], [0.0])
+        imm_extended = np.append(p.imm_rates[-1, 1:], [0.0])
+        part2 = b * np.transpose(omega_extended * imm_extended * p.lambdas)
+        K_presum = part1 + part2
         K = K_presum.sum()
+        K /= (1.0 + p.g_n_ss)
     elif method == 'TPI':
-        part1 = b * omega * lambdas
+        part1 = b * p.omega * p.lambdas
         #omega_extended = np.append(omega[1:,:,:],np.zeros((1,omega.shape[1],omega.shape[2])),axis=0)
-        omega_shift = np.append(omega[:,1:,:],np.zeros((omega.shape[0],1,omega.shape[2])),axis=1)
+        omega_shift = np.append(p.omega[:, 1:, :], np.zeros((p.omega.shape[0], 1, p.omega.shape[2])), axis=1)
         #imm_extended = np.append(imm_rates[1:,:,:],np.zeros((1,imm_rates.shape[1],imm_rates.shape[2])),axis=0)
-        imm_shift = np.append(imm_rates[:,1:,:],np.zeros((imm_rates.shape[0],1,imm_rates.shape[2])),axis=1)
+        imm_shift = np.append(p.imm_rates[:, 1:, :], np.zeros((p.imm_rates.shape[0], 1, p.imm_rates.shape[2])), axis=1)
         #part2 = b*(omega_extended*imm_extended)*lambdas
-        part2 = b*imm_shift*omega_shift*lambdas
-        K_presum = part1+part2
+        part2 = b * imm_shift * omega_shift * p.lambdas
+        K_presum = part1 + part2
         K = K_presum.sum(1).sum(1)
-    K /= (1.0 + g_n)
+        K /= (1.0 + p.g_n)
     return K
 
 
@@ -159,20 +156,24 @@ def get_BQ(r, b_splus1, j, p, method):
 
     Returns: BQ
     '''
-    if j is not None:
-        BQ_presum = p.omega * p.rho * b_splus1 * p.lambdas[j]
-    else:
-        BQ_presum = p.omega * p.rho * b_splus1 * p.lambdas
     if method == 'SS':
+        if j is not None:
+            BQ_presum = p.omega_SS * p.rho * b_splus1 * p.lambdas[j]
+        else:
+            BQ_presum = np.transpose(p.omega_SS * (p.rho * p.lambdas)) * b_splus1
         BQ = BQ_presum.sum(0)
         BQ *= (1.0 + r) / (1.0 + p.g_n_ss)
     elif method == 'TPI':
+        if j is not None:
+            BQ_presum = p.omega * p.rho * b_splus1 * p.lambdas[j]
+        else:
+            BQ_presum = p.omega * p.rho * b_splus1 * p.lambdas
         BQ = BQ_presum.sum(1)
         BQ *= (1.0 + r) / (1.0 + p.g_n)
     return BQ
 
 
-def get_C(c, params):
+def get_C(c, p, method):
     '''
     Calculation of aggregate consumption.
 
@@ -192,17 +193,14 @@ def get_C(c, params):
     Returns: aggC
     '''
 
-    omega, lambdas, method = params
-
-    aggC_presum = c * omega * lambdas
     if method == 'SS':
-        aggC = aggC_presum.sum()
+        aggC = (c * np.transpose(p.omega_SS * p.lambdas)).sum()
     elif method == 'TPI':
-        aggC = aggC_presum.sum(1).sum(1)
+        aggC = (c * p.omega * p.lambdas).sum(1).sum(1)
     return aggC
 
 
-def revenue(r, w, b, n, BQ, Y, L, K, factor, params):
+def revenue(r, w, b, n, BQ, Y, L, K, factor, theta, p, method):
     '''
     Gives lump sum transfer value.
     Inputs:
@@ -244,40 +242,32 @@ def revenue(r, w, b, n, BQ, Y, L, K, factor, params):
     Returns: T_H
 
     '''
-
-    e, lambdas, omega, method, etr_params, tax_func_type, theta, tau_bq, \
-        tau_payroll, h_wealth, p_wealth, m_wealth, retire, T, S, J,\
-        tau_b, delta_tau = params
-
-    I = r * b + w * e * n
+    I = r * b + w * p.e * n
 
     if I.ndim == 2:
-        T_I = np.zeros((S,J))
-        for j in range(J):
-            TI_params = (e[:,j], etr_params, tax_func_type)
-            T_I[:,j] = tax.ETR_income(r, w, b[:,j], n[:,j], factor, TI_params) * I[:,j]
+        T_I = np.zeros((p.S, p.J))
+        for j in range(p.J):
+            TI_params = (p.e[:, j], p.etr_params[:, -1, :], p.tax_func_type) ## this is written for SS - check if ever go here in TPI
+            T_I[:, j] = tax.ETR_income(r, w, b[:, j], n[:, j], factor, TI_params) * I[:, j]
     if I.ndim == 3:
-        T_I = np.zeros((T,S,J))
-        for j in range(J):
-            if etr_params.ndim == 3:
-                tau_inc_params3D = etr_params[:,j,:]
-            if etr_params.ndim == 4:
-                tau_inc_params3D = etr_params[:,:,j,:]
-            TI_params = (e[:,:,j], tau_inc_params3D, tax_func_type)
-            T_I[:,:,j] = tax.ETR_income(r[:,:,j], w[:,:,j], b[:,:,j], n[:,:,j], factor, TI_params) * I[:,:,j]
-    T_P = tau_payroll * w * e * n
-    TW_params = (h_wealth, p_wealth, m_wealth)
-    T_W = tax.ETR_wealth(b, TW_params) * b
+        T_I = np.zeros((p.T, p.S, p.J))
+        for j in range(p.J):
+            if p.etr_params.ndim == 3:
+                tau_inc_params3D = p.etr_params[:, j, :]
+            if p.etr_params.ndim == 4:
+                tau_inc_params3D = p.etr_params[:, :, j, :]
+            TI_params = (p.e[:, :, j], tau_inc_params3D, p.tax_func_type)
+            T_I[:, :, j] = tax.ETR_income(r[:, : , j], w[:, :, j], b[:, :, j], n[: ,: ,j], factor, TI_params) * I[:, :, j]
+    T_P = p.tau_payroll * w * p.e * n
+    T_W = tax.ETR_wealth(b, p) * b
     if method == 'SS':
-        T_P[retire:] -= theta * w
-        T_BQ = tau_bq * BQ / lambdas
-        biz_params = (tau_b, delta_tau)
-        business_revenue = tax.get_biz_tax(w, Y, L, K, biz_params)
-        REVENUE = (omega * lambdas * (T_I + T_P + T_BQ + T_W)).sum() + business_revenue
+        T_P[p.retire:] -= theta * w
+        T_BQ = p.tau_bq * (BQ / np.transpose(p.lambdas))
+        business_revenue = tax.get_biz_tax(w, Y, L, K, p)
+        REVENUE = (np.transpose(p.omega_SS * p.lambdas) * (T_I + T_P + T_BQ + T_W)).sum() + business_revenue
     elif method == 'TPI':
-        T_P[:, retire:, :] -= theta.reshape(1, 1, J) * w[:,retire:,:]
-        T_BQ = tau_bq.reshape(1, 1, J) * BQ / lambdas
-        biz_params = (tau_b, delta_tau)
-        business_revenue = tax.get_biz_tax(w[:T,0,0], Y, L, K, biz_params)
-        REVENUE = (omega * lambdas * (T_I + T_P + T_BQ + T_W)).sum(1).sum(1) + business_revenue
+        T_P[:, p.retire:, :] -= theta.reshape(1, 1, p.J) * w[:, p.retire:, :]
+        T_BQ = p.tau_bq.reshape(1, 1, p.J) * BQ / p.lambdas
+        business_revenue = tax.get_biz_tax(w[:p.T, 0, 0], Y, L, K, p)
+        REVENUE = (p.omega * p.lambdas * (T_I + T_P + T_BQ + T_W)).sum(1).sum(1) + business_revenue
     return REVENUE

ogusa/household.py
@@ -95,7 +95,7 @@ def marg_ut_labor(n, p):
           (p.upsilon - 1)) * ((1 - ((eps_high / p.ltilde) ** p.upsilon)) **
           ((1 - p.upsilon) / p.upsilon)) - (2 * d2 * eps_high))
     MDU_n[nvec_high] = 2 * d2 * nvec[nvec_high] + d1
-    output = MDU_n * p.chi_n
+    output = MDU_n * np.squeeze(p.chi_n)
     output = np.squeeze(output)
 
     return output
@@ -131,8 +131,7 @@ def get_cons(r, w, b, b_splus1, n, BQ, net_tax, j, e, p):
     if j is not None:
         lambdas = p.lambdas[j]
     else:
-        lambdas = p.lambdas
-
+        lambdas = np.transpose(p.lambdas)
     cons = ((1 + r) * b + w * e * n + BQ / lambdas - b_splus1 *
             np.exp(p.g_y) - net_tax)
     return cons
@@ -222,13 +221,13 @@ def FOC_savings(r, w, b, b_splus1, b_splus2, n, BQ, factor, T_H, theta,
     e_extended = np.array(list(e) + [0])
     n_extended = np.array(list(n) + [0])
     etr_params_extended = np.append(
-        p.etr_params, np.reshape(p.etr_params[-1, :],
-                                 (1, p.etr_params.shape[1])),
-        axis=0)[1:, :]
+        np.reshape(p.etr_params[:, -1, :], (p.S, p.etr_params.shape[2])), np.reshape(p.etr_params[-1, -1, :],
+                                 (1, p.etr_params.shape[2])),
+        axis=0)[1:, :]  # this only works for SS - taking last period of BW
     mtry_params_extended = np.append(
-        p.mtry_params, np.reshape(p.mtry_params[-1, :],
-                                  (1, p.mtry_params.shape[1])),
-        axis=0)[1:, :]
+        np.reshape(p.mtry_params[:, -1, :], (p.S, p.mtry_params.shape[2])), np.reshape(p.mtry_params[-1, -1, :],
+                                 (1, p.mtry_params.shape[2])),
+        axis=0)[1:, :] # this only works for SS - taking last period of BW
     if method == 'TPI':
         r_extended = np.append(r, r[-1])
         w_extended = np.append(w, w[-1])
@@ -241,7 +240,7 @@ def FOC_savings(r, w, b, b_splus1, b_splus2, n, BQ, factor, T_H, theta,
         T_H_extended = np.array([T_H, T_H])
 
     tax1 = tax.total_taxes(r, w, b, n, BQ, factor, T_H, theta, j, False,
-                           p.etr_params, p, method)
+                           p.etr_params[:, -1, :], p, method)
     tax2 = tax.total_taxes(r_extended[1:], w_extended[1:], b_splus1,
                            n_extended[1:], BQ_extended[1:], factor,
                            T_H_extended[1:], theta, j, True,
@@ -261,7 +260,7 @@ def FOC_savings(r, w, b, b_splus1, b_splus2, n, BQ, factor, T_H, theta,
                              n_extended[1:], factor, mtr_cap_params,
                              True)))
 
-    savings_ut = (p.rho * np.exp(-p.sigma * p.g_y) * p.chi_b *
+    savings_ut = (p.rho * np.exp(-p.sigma * p.g_y) * p.chi_b[j] *
                   b_splus1 ** (-p.sigma))
 
     euler_error = (marg_ut_cons(cons1, p.sigma) - p.beta * (1 - p.rho) *
@@ -341,11 +340,10 @@ def FOC_labor(r, w, b, b_splus1, n, BQ, factor, T_H, theta, j, p,
         e = p.e[:, j]
     else:
         e = p.e
-    tax1 = tax.total_taxes(r, w, b, n, BQ, factor, T_H, j, False,
-                           p.etr_params, p, method)
+    tax1 = tax.total_taxes(r, w, b, n, BQ, factor, T_H, theta, j, False,
+                           p.etr_params[:, -1, :], p, method)  # this only works with SS as written bc of tax params
     cons = get_cons(r, w, b, b_splus1, n, BQ, tax1, j, e, p)
-    mtr_lab_params = (e, p.etr_params, p.mtrx_params, p.tax_func_type,
-                      p.analytical_mtrs)
+    mtr_lab_params = (e, p.etr_params[:, -1, :], p.mtrx_params[:, -1, :], p) # this only works with SS as written bc of tax params
     deriv = (1 - p.tau_payroll - tax.MTR_income(r, w, b, n, factor,
                                                 mtr_lab_params, False))
 

ogusa/tax.py
@@ -36,7 +36,7 @@ def replacement_rate_vals(nssmat, wss, factor_ss, j, p):
                           group
     Returns: theta
     '''
-    if j:
+    if j is not None:
         e = p.e[:, j]
     else:
         e = p.e
@@ -86,7 +86,7 @@ def ETR_wealth(b, p):
     return tau_w
 
 
-def MTR_wealth(b, params):
+def MTR_wealth(b, p):
     '''
     Calculates the marginal tax rate on wealth from the wealth tax.
     Inputs:
@@ -101,12 +101,8 @@ def MTR_wealth(b, params):
                                      wealth tax
     Returns: tau_w_prime
     '''
-    h_wealth, p_wealth, m_wealth = params
-
-    h = h_wealth
-    m = m_wealth
-    p = p_wealth
-    tau_prime = h * m * p / (b * h + m) ** 2
+    tau_prime = (p.h_wealth * p.m_wealth * p.p_wealth/
+                 (b * p.h_wealth + p.m_wealth) ** 2)
     return tau_prime
 
 
@@ -257,7 +253,7 @@ def MTR_income(r, w, b, n, factor, params, mtr_capital):
     RETURNS: tau
     --------------------------------------------------------------------
     '''
-    e, etr_params, mtr_params, tax_func_type, analytical_mtrs = params
+    e, etr_params, mtr_params, p = params
 
     X = (w * e * n) * factor
     Y = (r * b) * factor
@@ -266,8 +262,8 @@ def MTR_income(r, w, b, n, factor, params, mtr_capital):
     I = X + Y
     I2 = I ** 2
 
-    if tax_func_type == 'GS':
-        if analytical_mtrs:
+    if p.tax_func_type == 'GS':
+        if p.analytical_mtrs:
             phi0 = etr_params[..., 0]
             phi1 = etr_params[..., 1]
             phi2 = etr_params[..., 2]
@@ -277,8 +273,8 @@ def MTR_income(r, w, b, n, factor, params, mtr_capital):
             phi2 = mtr_params[..., 2]
         tau = (phi0*(1 - (I ** (-phi1 - 1) * ((I ** -phi1) + phi2)
                           ** ((-1 - phi1) / phi1))))
-    elif tax_func_type == 'DEP_totalinc':
-        if analytical_mtrs:
+    elif p.tax_func_type == 'DEP_totalinc':
+        if p.analytical_mtrs:
             A = etr_params[..., 0]
             B = etr_params[..., 1]
             max_I = etr_params[..., 4]
@@ -301,7 +297,7 @@ def MTR_income(r, w, b, n, factor, params, mtr_capital):
                      (A * I2 + B * I + 1)) + min_I)
             tau = tau_I + shift_I + shift
     else:  # DEP or linear
-        if analytical_mtrs:
+        if p.analytical_mtrs:
             A = etr_params[..., 0]
             B = etr_params[..., 1]
             C = etr_params[..., 2]
@@ -359,7 +355,7 @@ def MTR_income(r, w, b, n, factor, params, mtr_capital):
     return tau
 
 
-def get_biz_tax(w, Y, L, K, params):
+def get_biz_tax(w, Y, L, K, p):
     '''
     Finds total business income tax receipts
     Inputs:
@@ -373,9 +369,7 @@ def get_biz_tax(w, Y, L, K, params):
     Returns: T_H
 
     '''
-
-    tau_b, delta_tau = params
-    business_revenue = tau_b * (Y - w * L) - tau_b * delta_tau * K
+    business_revenue = p.tau_b * (Y - w * L) - p.tau_b * p.delta_tau * K
     return business_revenue
 
 
@@ -426,12 +420,12 @@ def total_taxes(r, w, b, n, BQ, factor, T_H, theta, j, shift,
     Returns: total_taxes
 
     '''
-    if j:
+    if j is not None:
         e = p.e[:, j]
         lambdas = p.lambdas[j]
     else:
         e = p.e
-        lambdas = p.lambdas
+        lambdas = np.transpose(p.lambdas)
 
     I = r * b + w * e * n
     TI_params = (e, etr_params, p.tax_func_type)
@@ -449,7 +443,7 @@ def total_taxes(r, w, b, n, BQ, factor, T_H, theta, j, shift,
             T_P[p.retire:] -= theta * w
         else:
             T_P[p.retire - 1:] -= theta * w
-        T_BQ = p.tau_bq * BQ / lambdas
+        T_BQ = p.tau_bq * (BQ / lambdas)
     elif method == 'TPI':
         if not shift:
             # retireTPI is different from retire, because in TPI we are