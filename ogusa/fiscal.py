07c2207e8354457ddc0de318d85a80a3e3e39d22
jdebacker <jason.debacker@gmail.com>
2018-11-13 16:12:36 -0500

    change name for REVENUE

--- ogusa/fiscal.py
+++ ogusa/fiscal.py
@@ -22,7 +22,7 @@ def D_G_path(r_gov, dg_fixed_values, Gbaseline, p):
     '''
     Calculate the time paths of debt and government spending
     '''
-    Y, REVENUE, T_H, D0, G0 = dg_fixed_values
+    Y, total_revenue, T_H, D0, G0 = dg_fixed_values
 
     D = np.zeros(p.T + 1)
     D[0] = D0
@@ -37,25 +37,25 @@ def D_G_path(r_gov, dg_fixed_values, Gbaseline, p):
     while t < p.T-1:
         D[t] = ((1 / growth[t]) * ((1 + r_gov[t - 1]) * D[t - 1] +
                                    G[t - 1] + T_H[t - 1] -
-                                   REVENUE[t - 1]))
+                                   total_revenue[t - 1]))
         if (t >= p.tG1) and (t < p.tG2):
             G[t] = (growth[t + 1] * (p.rho_G * p.debt_ratio_ss * Y[t] +
                                      (1 - p.rho_G) * D[t]) -
-                    (1 + r_gov[t]) * D[t] + REVENUE[t] - T_H[t])
+                    (1 + r_gov[t]) * D[t] + total_revenue[t] - T_H[t])
         elif t >= p.tG2:
             G[t] = (growth[t + 1] * (p.debt_ratio_ss * Y[t]) -
-                    (1 + r_gov[t]) * D[t] + REVENUE[t] - T_H[t])
+                    (1 + r_gov[t]) * D[t] + total_revenue[t] - T_H[t])
         t += 1
 
     # in final period, growth rate has stabilized, so we can replace
     # growth[t+1] with growth[t]
     t = p.T - 1
     D[t] = ((1 / growth[t]) * ((1 + r_gov[t - 1]) * D[t - 1] + G[t - 1]
-                               + T_H[t - 1] - REVENUE[t - 1]))
+                               + T_H[t - 1] - total_revenue[t - 1]))
     G[t] = (growth[t] * (p.debt_ratio_ss * Y[t]) - (1 + r_gov[t]) * D[t]
-            + REVENUE[t] - T_H[t])
+            + total_revenue[t] - T_H[t])
     D[t + 1] = ((1 / growth[t + 1]) * ((1 + r_gov[t]) * D[t] + G[t] +
-                                       T_H[t] - REVENUE[t]))
+                                       T_H[t] - total_revenue[t]))
     D_ratio_max = np.amax(D[:p.T] / Y[:p.T])
     print('Maximum debt ratio: ', D_ratio_max)
 
