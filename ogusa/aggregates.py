751f7a7791bb4700ad556aad450f62aec1712edc
jdebacker <jason.debacker@gmail.com>
2019-01-06 19:24:18 -0500

    adjust theta in aggr and tax calcs

--- ogusa/aggregates.py
+++ ogusa/aggregates.py
@@ -306,6 +306,7 @@ def revenue(r, w, b, n, BQ, Y, L, K, factor, theta, etr_params, p, method):
         T_P = p.tau_payroll[:p.T].reshape(p.T, 1, 1) * w_array * n * p.e
         for t in range(T_P.shape[0]):
             T_P[t, p.retire[t]:, :] -= (theta.reshape(1, p.J) *
+                                        p.replacement_rate_adjust[t] *
                                         w_array[t])
         # T_P[:, p.retire:, :] -= theta.reshape(1, 1, p.J) * w_array
         # T_W = tax.ETR_wealth(b, p) * b
