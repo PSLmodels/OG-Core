fix I_d computation

ogusa/SS.py
@@ -241,7 +241,6 @@ def inner_loop(outer_loop_vars, p, client):
         K_d = B - D_d
         K_f = K_demand - B + D_d
         K = K_f + K_d
-    print('Capital market clearing as solve model = ', K - K_demand)
     new_Y = firm.get_Y(K, L, p, 'SS')
     if p.budget_balance:
         Y = new_Y
@@ -448,19 +447,16 @@ def SS_solver(bmat, nmat, r, BQ, T_H, factor, Y, p, client,
     if not p.small_open:
         K_f_ss = p.zeta_K * (K_demand_ss - Bss + D_d_ss)
         Kss = K_f_ss + K_d_ss
-        I_d_ss = aggr.get_I(bssmat_splus1 * (K_d_ss / Bss), K_d_ss, K_d_ss, p, 'SS') # scaling bssmat to account for fraction household portfolio that is debt not capital
-        Iss = aggr.get_I(bssmat_splus1, Kss, Kss, p, 'SS') ## Check this
+        I_d_ss = aggr.get_I(bssmat_splus1, K_d_ss, K_d_ss, p, 'SS')
+        Iss = aggr.get_I(bssmat_splus1, Kss, Kss, p, 'SS')
     else:
-        # Compute capital (K) and wealth (B) separately
         K_d_ss = Bss - D_d_ss
         K_f_ss = K_demand_ss - Bss + D_d_ss
         Kss = K_f_ss + K_d_ss
-        # Kss = firm.get_K(Lss, p.firm_r[-1], p, 'SS')
         InvestmentPlaceholder = np.zeros(bssmat_splus1.shape)
         Iss = aggr.get_I(InvestmentPlaceholder, Kss, Kss, p, 'SS')
         BIss = aggr.get_I(bssmat_splus1, Bss, Bss, p, 'BI_SS')
-        I_d_ss = aggr.get_I(bssmat_splus1 * (K_d_ss / Bss), K_d_ss, K_d_ss, p, 'SS') # scaling bssmat to account for fraction household portfolio that is debt not capital
-
+        I_d_ss = aggr.get_I(bssmat_splus1, K_d_ss, K_d_ss, p, 'SS')
     if p.budget_balance:
         r_hh_ss = rss
     else:
@@ -521,6 +517,7 @@ def SS_solver(bmat, nmat, r, BQ, T_H, factor, Y, p, client,
 
     # solve resource constraint
     # net foreign borrowing
+    print('Foreign debt holdings = ', D_f_ss)
     new_borrowing_f = D_f_ss * (np.exp(p.g_y) * (1 + p.g_n_ss) - 1)
     debt_service_f = D_f_ss * r_gov_ss
     RC = aggr.resource_constraint(Yss, Css, Gss, I_d_ss, K_f_ss,