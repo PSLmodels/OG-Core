31f8da98db09a3512d92e532fb6406d122a2aacb
jdebacker <jason.debacker@gmail.com>
2019-04-04 23:01:05 -0400

    del new vars from test dict

--- ogusa/tests/test_SS.py
+++ ogusa/tests/test_SS.py
@@ -230,6 +230,9 @@ def test_SS_solver():
     test_dict['IITpayroll_revenue'] = (test_dict['total_revenue_ss'] -
                                        test_dict['business_revenue'])
     del test_dict['T_Pss'], test_dict['T_BQss'], test_dict['T_Wss']
+    del test_dict['K_d_ss'], test_dict['K_f_ss'], test_dict['D_d_ss']
+    del test_dict['D_f_ss'], test_dict['I_d_ss']
+    del test_dict['debt_service_f'], test_dict['new_borrowing_f']
     del test_dict['bqssmat'], test_dict['T_Css']
     test_dict['revenue_ss'] = test_dict.pop('total_revenue_ss')
 
@@ -445,6 +448,9 @@ def test_run_SS(input_path, expected_path):
     del test_dict['T_Pss'], test_dict['T_BQss'], test_dict['T_Wss']
     del test_dict['resource_constraint_error'], test_dict['T_Css']
     del test_dict['r_gov_ss'], test_dict['r_hh_ss']
+    del test_dict['K_d_ss'], test_dict['K_f_ss'], test_dict['D_d_ss']
+    del test_dict['D_f_ss'], test_dict['I_d_ss']
+    del test_dict['debt_service_f'], test_dict['new_borrowing_f']
     test_dict['revenue_ss'] = test_dict.pop('total_revenue_ss')
 
     for k, v in expected_dict.items():
