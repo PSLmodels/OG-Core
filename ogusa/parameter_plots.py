a0f62270c91090f07526e034ce302370f2597265
jdebacker <jason.debacker@gmail.com>
2019-05-10 08:46:58 -0400

    fix path to style file

--- ogusa/parameter_plots.py
+++ ogusa/parameter_plots.py
@@ -1,12 +1,22 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-plt.style.use('OGUSAplots.mplstyle')
+cur_path = os.path.split(os.path.abspath(__file__))[0]
+style_file = os.path.join(cur_path, 'OGUSAplots.mplstyle')
+plt.style.use(style_file)
 
 
 def plot_imm_rates(p, year=2019, path=None):
     '''
     Create a plot of immigration rates from OG-USA parameterization.
+
+    Args:
+        p (OG-USA Specifications class): parameters object
+        year (integer): year of mortality ratese to plot
+        path (string): path to save figure to
+
+    Returns:
+        fig (Matplotlib plot object): plot of immigration rates
     '''
     age_per = np.linspace(p.E, p.E + p.S, p.S)
     fig, ax = plt.subplots()
