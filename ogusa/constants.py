e55f4a745b924fedd03ee88815113c21b399c20f
jdebacker <jason.debacker@gmail.com>
2019-05-11 22:04:07 -0400

    add more labels

--- ogusa/constants.py
+++ ogusa/constants.py
@@ -1,4 +1,5 @@
 VAR_LABELS = {'Y': 'GDP ($Y_t$)', 'C': 'Consumption ($C_t$)',
-              'K': 'Capital Stock ($K_t$)', 'L': 'Labor ($L_t$)'}
+              'K': 'Capital Stock ($K_t$)', 'L': 'Labor ($L_t$)',
+              'nssmat': 'Labor Supply', 'n_mat': 'Labor Supply'}
 
 ToGDP_LABELS = {'D': 'Debt-to-GDP ($D_{t}/Y_t$)'}
