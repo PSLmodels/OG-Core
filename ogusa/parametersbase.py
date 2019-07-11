19feae622373c3927ac2a737ce0e86dbe3f8660d
jdebacker <jason.debacker@gmail.com>
2019-07-11 15:29:58 -0400

    parameterbase and postprocess docstrings

--- ogusa/parametersbase.py
+++ ogusa/parametersbase.py
@@ -1,6 +1,6 @@
-"""
-Tax-Calculator abstract base parameters class.
-"""
+'''
+OG-USA abstract base parameters class.
+'''
 # CODING-STYLE CHECKS:
 # pep8 parameters.py
 
@@ -14,26 +14,26 @@ import numpy as np
 
 
 class ParametersBase(object):
-    """
+    '''
     Inherit from this class for OG-USA parameter classes. Override this
     __init__ method and DEFAULTS_FILENAME.
-    """
+    '''
     __metaclass__ = abc.ABCMeta
 
     DEFAULTS_FILENAME = None
 
     @classmethod
     def default_data(cls, metadata=False):
-        """
+        '''
         Return parameter data read from the subclass's json file.
-        Parameters
-        ----------
-        metadata: boolean
-        start_year: int or None
-        Returns
-        -------
-        params: dictionary of data
-        """
+
+        Args:
+        metadata (bool): whether to return parameter metadata
+
+        Returns:
+        params (dict): dictionary of data
+
+        '''
         params = cls._params_dict_from_json_file()
         # return different data from params dict depending on metadata value
         if metadata:
@@ -48,16 +48,17 @@ class ParametersBase(object):
 
     @classmethod
     def _params_dict_from_json_file(cls):
-        """
+        '''
         Read DEFAULTS_FILENAME file and return complete dictionary.
-        Parameters
-        ----------
-        nothing: void
-        Returns
-        -------
-        params: dictionary
-            containing complete contents of DEFAULTS_FILENAME file.
-        """
+
+        Args:
+            None
+
+        Returns:
+        params (dict): dictionary containing complete contents of
+            DEFAULTS_FILENAME file.
+
+        '''
         if cls.DEFAULTS_FILENAME is None:
             msg = 'DEFAULTS_FILENAME must be overridden by inheriting class'
             raise NotImplementedError(msg)
@@ -70,22 +71,20 @@ class ParametersBase(object):
             return params_dict
 
     def _update(self, mods):
-        """
+        '''
         Private method used by public implement_reform and update_* methods
         in inheriting classes.
-        Parameters
-        ----------
-        mods: dictionary containing a parameter:value pairs
-        Raises
-        ------
-        ValueError:
-            if mods is not a dict.
-        Returns
-        -------
-        nothing: void
-        Notes
-        -----
-        """
+
+        Args:
+            mods (dict): dictionary containing a parameter:value pairs
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: if mods is not a dict.
+
+        '''
         if not isinstance(mods, dict):
             msg = 'mods is not a dictionary'
             raise ValueError(msg)
@@ -108,24 +107,29 @@ class ParametersBase(object):
 
     @staticmethod
     def _expand_array(x, x_int, x_bool, x_string):
-        """
+        '''
         Private method called only within this abstract base class.
         Dispatch to either _expand_1D or _expand_2D given dimension of x.
-        Parameters
-        ----------
-        x : value to expand
-            x must be either a scalar list or a 1D numpy array, or
-            x must be either a list of scalar lists or a 2D numpy array
-        x_int : boolean
-            True implies x has dtype=np.int8;
-            False implies x has dtype=np.float64 or dtype=np.bool_
-        x_bool : boolean
-            True implies x has dtype=np.bool_;
-            False implies x has dtype=np.float64 or dtype=np.int8
-        Returns
-        -------
-        expanded numpy array with specified dtype
-        """
+
+        Args:
+            x (object): value to expand
+                x must be either a scalar list or a 1D numpy array, or
+                x must be either a list of scalar lists or a 2D numpy
+                array
+            x_int (bool):
+                True implies x has dtype=np.int8;
+                False implies x has dtype=np.float64 or dtype=np.bool_
+            x_bool (bool):
+                True implies x has dtype=np.bool_;
+                False implies x has dtype=np.float64 or dtype=np.int8
+            x_string (bool):
+                True implies x has dtype=np.str_;
+                False implies x has dtype=np.float64 or dtype=np.int8
+
+        Returns:
+            x (Numpy array): expanded numpy array with specified dtype
+
+        '''
         if isinstance(x, list):
             if x_int:
                 x = np.array(x, np.int32)
@@ -156,20 +160,23 @@ class ParametersBase(object):
     }
 
     def simple_eval(self, param_string):
-        """
-        Parses `param_string` and returns result. `param_string can be either:
-            1. `param_name op scalar` -- this will be parsed into param, op, and scalar
-                    where `op` is a key in `OP_DICT`. The corresponding function is
-                    applied to the parameter value and the scalar value.
-            2. `param_name` -- simply return the parameter value that is retrieved
-                    from the object
-        Parameters
-        ----------
-        param_string : string of form `param op scalar` or `param`
-        Returns
-        -------
-        float used for validation
-        """
+        '''
+        Parses `param_string` and returns result. `param_string can be
+            either::
+            1. `param_name op scalar` -- this will be parsed into param,
+                op, and scalar where `op` is a key in `OP_DICT`. The
+                corresponding function is applied to the parameter value
+                and the scalar value.
+            2. `param_name` -- simply return the parameter value that is
+                retrieved from the object
+
+        Args:
+            param_string (str): of form `param op scalar` or `param`
+
+        Returns:
+            float used for validation
+
+        '''
         pieces = param_string.split(' ')
         validate_against = pieces[0]
         # param_string is of the form 'param_name op scalar'
