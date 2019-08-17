d073d0be18c09910b30e9487e6a6de015478d68f
jdebacker <jason.debacker@gmail.com>
2019-08-17 11:29:17 -0400

    mock import taxcalc for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,10 @@ extensions = [
     'alabaster'
 ]
 
+
+# mock import taxcalc
+autodoc_mock_imports = ["taxcalc"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
