646f320bac26aa4619cbb199f86b452708e40f1e
jdebacker <jason.debacker@gmail.com>
2019-08-18 13:06:38 +0530

    add test of parameter_tables

---
+++ ogusa/tests/test_parameter_tables.py
@@ -0,0 +1,21 @@
+'''
+Tests of parameter_plots.py module
+'''
+
+import pytest
+import os
+from ogusa import utils, parameter_tables
+
+
+# Load in test results and parameters
+CUR_PATH = os.path.abspath(os.path.dirname(__file__))
+
+base_params = utils.safe_read_pickle(
+    os.path.join(CUR_PATH, 'test_io_data', 'model_params_baseline.pkl'))
+base_taxfunctions = utils.safe_read_pickle(
+    os.path.join(CUR_PATH, 'test_io_data', 'TxFuncEst_baseline.pkl'))
+
+
+def test_tax_rate_table():
+    str = parameter_tables.tax_rate_table(base_taxfunctions, base_params)
+    assert str
