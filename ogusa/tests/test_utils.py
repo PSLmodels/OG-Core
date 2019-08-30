ff107edd58f6ad79aaced25473f09b4a98b0d0b5
jdebacker <jason.debacker@gmail.com>
2019-08-30 14:08:28 -0400

    add docstrings to utils tests

--- ogusa/tests/test_utils.py
+++ ogusa/tests/test_utils.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def test_rate_conversion():
     '''
-    test of utils.rate_conversion
+    Test of utils.rate_conversion
     '''
     expected_rate = 0.3
     annual_rate = 0.3
@@ -27,29 +27,44 @@ ability_weights = np.array([0.5, 0.5])
 
 
 def test_create_ineq_object():
+    '''
+    Test that Inequality class and be created
+    '''
     ineq = Inequality(dist, pop_weights, ability_weights, S, J)
     assert ineq
 
 
 def test_gini():
+    '''
+    Test of Gini coefficient calculation
+    '''
     ineq = Inequality(dist, pop_weights, ability_weights, S, J)
     gini = ineq.gini()
     assert np.allclose(gini, 0.41190476190476133)
 
 
 def test_var_of_logs():
+    '''
+    Test of variance of logs calculation
+    '''
     ineq = Inequality(dist, pop_weights, ability_weights, S, J)
     var_log = ineq.var_of_logs()
     assert np.allclose(var_log, 0.7187890928713506)
 
 
 def test_ratio_pct1_pct2():
+    '''
+    Test of percentile ratio calculation
+    '''
     ineq = Inequality(dist, pop_weights, ability_weights, S, J)
     ratio = ineq.ratio_pct1_pct2(0.9, 0.1)
     assert np.allclose(ratio, 9)
 
 
 def test_top_share():
+    '''
+    Test of top share calculation
+    '''
     ineq = Inequality(dist, pop_weights, ability_weights, S, J)
     top_share = ineq.top_share(0.05)
     assert np.allclose(top_share, 0.285714286)
