ed0c6e0b8ba8eb34612b5174a6acee5b436179f1
jdebacker <jason.debacker@gmail.com>
2019-09-20 23:56:56 -0400

    add functions for comp

---
+++ compconfig/compconfig/functions.py
@@ -0,0 +1,148 @@
+import ogusa
+from ogusa.parameters import Specifications
+from ogusa.utils import TC_LAST_YEAR, REFORM_DIR, BASELINE_DIR
+from ogusa import output_plots as op
+import os
+import paramtools
+from .helpers import retrieve_puf
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+
+
+class MetaParams(paramtools.Parameters):
+    '''
+    Meta parameters class for COMP.  These parameters will be in a drop
+    down menu on COMP.
+    '''
+    array_first = True
+    defaults = {
+        "year": {
+            "title": "Start year",
+            "description": "Year for parameters.",
+            "type": "int",
+            "value": 2019,
+            "validators": {"range": {"min": 2015, "max": TC_LAST_YEAR}}
+        },
+        "data_source": {
+            "title": "Data source",
+            "description": "Data source for Tax-Calculator to use",
+            "type": "str",
+            "value": "CPS",
+            "validators": {"choice": {"choices": ["PUF", "CPS"]}}
+        }
+    }
+
+
+def get_inputs(meta_params_dict):
+    '''
+    Function to get user input parameters from COMP
+    '''
+    meta_params = MetaParams()
+    meta_params.adjust(meta_params_dict)
+    params = Specifications()
+    spec = params.specification(
+        meta_data=True,
+        serializable=True,
+        year=meta_params.year
+    )
+    return (meta_params.specification(meta_data=True, serializable=True),
+            {"ogusa": spec})
+
+
+def validate_inputs(meta_param_dict, adjustment, errors_warnings):
+    '''
+    Validates user inputs for parameters
+    '''
+    # ogusa doesn't look at meta_param_dict for validating inputs.
+    params = Specifications()
+    params.adjust(adjustment["ogusa"], raise_errors=False)
+    errors_warnings["ogusa"]["errors"].update(params.errors)
+    return errors_warnings
+
+
+def run_model(meta_param_dict, adjustment):
+    '''
+    Initializes classes from OG-USA that compute the model under
+    different policies.  Then calls function get output objects.
+    '''
+    # Create output directory structure
+    ss_dir = os.path.join(output_base, "SS")
+    tpi_dir = os.path.join(output_base, "TPI")
+    dirs = [ss_dir, tpi_dir]
+    for _dir in dirs:
+        try:
+            print("making dir: ", _dir)
+            os.makedirs(_dir)
+        except OSError:
+            pass
+    # Dask parmeters
+    client = None
+    num_workers = 1
+
+    # whether to estimate tax functions from microdata
+    run_micro = False
+
+    # Solve baseline model
+    base_spec = {'start_year': meta_param_dict['year'],
+                 'debt_ratio_ss': 1.5, 'debt_ratio_ss': 2.0,
+                 'r_gov_scale': 1.0, 'r_gov_shift': 0.02,
+                 'zeta_D': [0.4], 'zeta_K': [0.1],
+                 'initial_debt_ratio': 0.78,
+                 'initial_foreign_debt_ratio': 0.4}
+    base_params = Specifications(
+        run_micro=False, output_base=BASELINE_DIR,
+        baseline_dir=BASELINE_DIR, test=False, time_path=False,
+        baseline=True, iit_reform={}, guid='',
+        data=meta_param_dict['data_source'],
+        client=client, num_workers=num_workers)
+    base_params.update_specifications(og_spec)
+    base_params.get_tax_function_parameters(client, run_micro)
+    base_ss = SS.run_SS(base_params, client=client)
+
+    # Solve reform model
+    reform_spec = {'start_year': meta_param_dict['year'],
+                   'debt_ratio_ss': 1.5, 'debt_ratio_ss': 2.0,
+                   'r_gov_scale': 1.0, 'r_gov_shift': 0.02,
+                   'zeta_D': [0.4], 'zeta_K': [0.1],
+                   'initial_debt_ratio': 0.78,
+                   'initial_foreign_debt_ratio': 0.4}.update(adjustment)
+    reform_params = Specifications(
+        run_micro=False, output_base=BASELINE_DIR,
+        baseline_dir=BASELINE_DIR, test=False, time_path=False,
+        baseline=False, iit_reform={}, guid='',
+        data=meta_param_dict['data_source'],
+        client=client, num_workers=num_workers)
+    reform_params.update_specifications(reform_spec)
+    reform_params.get_tax_function_parameters(client, run_micro)
+    reform_ss = SS.run_SS(reform_params, client=client)
+
+    comp_dict = comp_output(base_ss, base_params, reform_ss,
+                            reform_params)
+
+    return comp_dict
+
+
+def comp_output(base_ss, base_params, reform_ss, reform_params,
+                var='nssmat'):
+    '''
+    Function to create output for the COMP platform
+    '''
+    plt = op.ss_profiles(
+        base_ss, base_params, reform_ss=reform_ss,
+        reform_params=reform_params, by_j=True, var=var,
+        plot_title='Labor Supply in Baseline and Reform Policy')
+    comp_dict = {
+        "model_version": ogusa.__version__,
+        "renderable": [
+            {
+              "media_type": "matplotlib",
+              "title": plt.title,
+              "data": {
+                        "png": plt.savefig()
+                    }
+              }
+            ]
+        }
+
+    return comp_dict
---
+++ compconfig/compconfig/tests/test_functions.py
@@ -0,0 +1,22 @@
+from compdevkit import FunctionsTest
+import pandas as pd
+import io
+from compconfig import functions
+
+
+def test_get_parameters():
+    ta = FunctionsTest(
+        get_inputs=functions.get_inputs,
+        validate_inputs=functions.validate_inputs,
+        run_model=functions.run_model,
+        ok_adjustment={"ogusa": {"frisch": 0.41}},
+        bad_adjustment={"ogusa": {"frisch": 1.5}}
+    )
+    ta.test()
+
+# Comment out until do tabular output
+# def test_param_effect():
+#     adjustment = {"ogusa": {"frisch": 0.35}}
+#     comp_dict = functions.run_model({}, adjustment)
+#     df = pd.read_csv(io.StringIO(comp_dict['downloadable'][0]['data']))
+#     assert df.loc[0, 'Change from Baseline (pp)'] != 0
