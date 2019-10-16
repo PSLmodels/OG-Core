1f30eb81f589cafa4bfa30dcfb6113df2cce7cc6
jdebacker <jason.debacker@gmail.com>
2019-10-16 05:01:45 -0400

    update data for txfunc test

--- ogusa/tests/test_get_micro_data.py
+++ ogusa/tests/test_get_micro_data.py
@@ -59,8 +59,8 @@ def test_puf_path():
     if os.path.exists(PUF_PATH):
         calc = get_micro_data.get_calculator(
             baseline, start_year, reform=reform, data=PUF_PATH)
-        # blind_head is only in the CPS file and e00700 is only in the PUF.
-        # See taxcalc/records_variables.json
+        # blind_head is only in the CPS file and e00700 is only in the
+        # PUF.  See taxcalc/records_variables.json
         assert (calc.array('blind_head').sum() == 0 and
                 calc.array('e00700').sum() > 0)
     # we do not have puf.csv
