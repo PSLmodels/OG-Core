700936ce484086b539ff5b638e5a7fdfb3e72d2f
jdebacker <jason.debacker@gmail.com>
2019-11-06 18:10:03 -0500

    add gini table

--- ogusa/output_tables.py
+++ ogusa/output_tables.py
@@ -236,3 +236,64 @@ def ineq_table(base_ss, base_params, reform_ss=None, reform_params=None,
                               precision=3)
 
     return table
+
+
+def gini_table(base_ss, base_params, reform_ss=None,
+               reform_params=None, var_list=['cssmat'],
+               table_format=None, path=None):
+    '''
+    Creates table with measures of the Gini coefficient: overall,
+    across lifetime earnings group, and across age.
+
+    Args:
+        base_ss (dictionary): SS output from baseline run
+        base_params (OG-USA Specifications class): baseline parameters
+            object
+        reform_ss (dictionary): SS output from reform run
+        reform_params (OG-USA Specifications class): reform parameters
+            object
+        var_list (list): names of variable to use in table
+        table_format (string): format to return table in: 'csv', 'tex',
+            'excel', 'json', if None, a DataFrame is returned
+        path (string): path to save table to
+
+    Returns:
+        table (various): table in DataFrame or string format or `None`
+            if saved to disk
+
+    '''
+    table_dict = {'Steady-State Variable': [], 'Gini Type': [],
+                  'Baseline': []}
+    if reform_ss:
+        table_dict['Reform'] = []
+        table_dict['% Change'] = []
+    for i, v in enumerate(var_list):
+        base_ineq = Inequality(
+            base_ss[v], base_params.omega_SS, base_params.lambdas,
+            base_params.S, base_params.J)
+        if reform_ss:
+            reform_ineq = Inequality(
+                reform_ss[v], reform_params.omega_SS,
+                reform_params.lambdas, reform_params.S, reform_params.J)
+        table_dict['Steady-State Variable'].extend(
+            [VAR_LABELS[v], '', ''])
+        table_dict['Gini Type'].extend(
+            ['Overall', 'Lifetime Income Group, $j$', 'Age , $s$'])
+        base_values = np.array([
+            base_ineq.gini(), base_ineq.gini(type='ability'),
+            base_ineq.gini(type='age')])
+        table_dict['Baseline'].extend(list(base_values))
+        if reform_ss:
+            reform_values = np.array([
+                reform_ineq.gini(), reform_ineq.gini(type='ability'),
+                reform_ineq.gini(type='age')])
+            table_dict['Reform'].extend(list(reform_values))
+            table_dict['% Change'].extend(list(
+                ((reform_values - base_values) / base_values) * 100))
+    # Make df with dict so can use pandas functions
+    table_df = pd.DataFrame.from_dict(
+        table_dict)
+    table = save_return_table(table_df, table_format, path,
+                              precision=3)
+
+    return table
