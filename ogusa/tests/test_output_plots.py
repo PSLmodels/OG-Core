c3ba4aa16aa69401837a4e38d3454c48901f4d5d
jdebacker <jason.debacker@gmail.com>
2019-11-12 21:42:19 -0500

    test ss ability bar plot

--- ogusa/tests/test_output_plots.py
+++ ogusa/tests/test_output_plots.py
@@ -44,6 +44,12 @@ def test_ability_bar():
     assert fig
 
 
+def test_ability_bar_ss():
+    fig = output_plots.ability_bar_ss(
+        base_ss, base_params, reform_ss, reform_params)
+    assert fig
+
+
 def test_ss_profiles():
     fig = output_plots.ss_profiles(base_ss, base_params, reform_ss,
                                    reform_params)
