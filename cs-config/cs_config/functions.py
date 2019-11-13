dd39558e79f6dfb237502b378a6ee12d3cd926d4
jdebacker <jason.debacker@gmail.com>
2019-11-13 12:55:17 -0500

    fix typos

--- cs-config/cs_config/functions.py
+++ cs-config/cs_config/functions.py
@@ -194,6 +194,8 @@ def comp_output(base_ss, base_params, reform_ss, reform_params,
     '''
     table_title = 'Percentage Changes in Economic Aggregates Between'
     table_title += ' Baseline and Reform Policy'
+    plot_title = 'Percentage Changes in Consumption Between Baseline'
+    plot_title += ' and Reform Policy'
     out_table = ot.macro_table_SS(
         base_ss, reform_ss,
         var_list=['Yss', 'Css', 'Iss_total', 'Gss', 'total_revenue_ss',
@@ -204,8 +206,7 @@ def comp_output(base_ss, base_params, reform_ss, reform_params,
                   'Lss', 'rss', 'wss'], table_format='html')
     fig = op.ability_bar_ss(
         base_ss, base_params, reform_ss, reform_params, var=var,
-        plot_title='Percentage Changes in Conumption Between Baseline' +
-        ' and Reform Policy')
+        plot_title=plot_title)
     in_memory_file = io.BytesIO()
     fig.savefig(in_memory_file, format="png")
     in_memory_file.seek(0)
@@ -213,7 +214,7 @@ def comp_output(base_ss, base_params, reform_ss, reform_params,
         "renderable": [
             {
               "media_type": "PNG",
-              "title": 'Labor Supply in Baseline and Reform Policy',
+              "title": plot_title,
               "data": in_memory_file.read()
               },
             {
