9e0d1a9c5a8efbc5153e7876b70aadc2f985eb20
jdebacker <jason.debacker@gmail.com>
2019-11-20 17:08:25 -0500

    test arctan deriv func

--- ogusa/tests/test_income.py
+++ ogusa/tests/test_income.py
@@ -19,3 +19,17 @@ def test_artctan_func():
     test_vals = income.arctan_func(xvals, a, b, c)
 
     assert np.allclose(test_vals, expected_vals)
+
+
+def test_artctan_deriv_func():
+    '''
+    Test of arctan_deriv_func()
+    '''
+    expected_vals = np.array([-0.109814991, -0.036400091, -0.017707961])
+    xvals = np.array([1, 2, 3])
+    a = 1.3
+    b = 2.2
+    c = 0.5
+    test_vals = income.arctan_deriv_func(xvals, a, b, c)
+
+    assert np.allclose(test_vals, expected_vals)
