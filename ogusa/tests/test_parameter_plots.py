5a68cfec35b441be322a936a66c00a25dcac5280
jdebacker <jason.debacker@gmail.com>
2020-01-31 11:38:33 -0500

    test saved figures for parameter_plots

--- ogusa/tests/test_parameter_plots.py
+++ ogusa/tests/test_parameter_plots.py
@@ -6,6 +6,7 @@ import pytest
 import os
 import numpy as np
 import scipy.interpolate as si
+import matplotlib.image as mpimg
 from ogusa import utils, parameter_plots, income
 
 
@@ -21,10 +22,26 @@ def test_plot_imm_rates():
     assert fig
 
 
+def test_plot_imm_rates_save_fig(tmpdir):
+    parameter_plots.plot_imm_rates(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'imm_rates_orig.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 def test_plot_mort_rates():
     fig = parameter_plots.plot_mort_rates(
         base_params, include_title=True)
     assert fig
+
+
+def test_plot_mort_rates_save_fig(tmpdir):
+    parameter_plots.plot_mort_rates(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'mortality_rates.png'))
+
+    assert isinstance(img, np.ndarray)
 
 
 def test_plot_pop_growth():
@@ -33,10 +50,26 @@ def test_plot_pop_growth():
     assert fig
 
 
+def test_plot_pop_growth_rates_save_fig(tmpdir):
+    parameter_plots.plot_pop_growth(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'pop_growth_rates.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 def test_plot_ability_profiles():
     fig = parameter_plots.plot_ability_profiles(
         base_params, include_title=True)
     assert fig
+
+
+def test_plot_ability_profiles_save_fig(tmpdir):
+    parameter_plots.plot_ability_profiles(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'ability_profiles.png'))
+
+    assert isinstance(img, np.ndarray)
 
 
 def test_plot_elliptical_u():
@@ -48,10 +81,26 @@ def test_plot_elliptical_u():
     assert fig2
 
 
+def test_plot_elliptical_u_save_fig(tmpdir):
+    parameter_plots.plot_elliptical_u(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'ellipse_v_CFE.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 def test_plot_chi_n():
     fig = parameter_plots.plot_chi_n(
         base_params, include_title=True)
     assert fig
+
+
+def test_plot_chi_n_save_fig(tmpdir):
+    parameter_plots.plot_chi_n(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'chi_n_values.png'))
+
+    assert isinstance(img, np.ndarray)
 
 
 @pytest.mark.parametrize(
@@ -61,6 +110,14 @@ def test_plot_population(years_to_plot):
     fig = parameter_plots.plot_population(
         base_params, years_to_plot=years_to_plot, include_title=True)
     assert fig
+
+
+def test_plot_population_save_fig(tmpdir):
+    parameter_plots.plot_population(
+            base_params, path=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'pop_distribution.png'))
+
+    assert isinstance(img, np.ndarray)
 
 
 def test_plot_fert_rates():
@@ -79,6 +136,24 @@ def test_plot_fert_rates():
     assert fig
 
 
+def test_plot_fert_rates_save_fig(tmpdir):
+    totpers = base_params.S
+    min_yr = 20
+    max_yr = 100
+    fert_data = (np.array([0.0, 0.0, 0.3, 12.3, 47.1, 80.7, 105.5, 98.0,
+                           49.3, 10.4, 0.8, 0.0, 0.0]) / 2000)
+    age_midp = np.array([9, 10, 12, 16, 18.5, 22, 27, 32, 37, 42, 47,
+                         55, 56])
+    fert_func = si.interp1d(age_midp, fert_data, kind='cubic')
+    fert_rates = np.random.uniform(size=totpers)
+    parameter_plots.plot_fert_rates(
+        fert_func, age_midp, totpers, min_yr, max_yr, fert_data,
+        fert_rates, output_dir=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'fert_rates.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 def test_plot_mort_rates_data():
     totpers = base_params.S - 1
     min_yr = 21
@@ -93,6 +168,22 @@ def test_plot_mort_rates_data():
     assert fig
 
 
+def test_plot_mort_rates_data_save_fig(tmpdir):
+    totpers = base_params.S - 1
+    min_yr = 21
+    max_yr = 100
+    age_year_all = np.arange(min_yr, max_yr)
+    mort_rates = base_params.rho[1:]
+    mort_rates_all = base_params.rho[1:]
+    infmort_rate = base_params.rho[0]
+    parameter_plots.plot_mort_rates_data(
+        totpers, min_yr, max_yr, age_year_all, mort_rates_all,
+        infmort_rate, mort_rates, output_dir=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'mort_rates.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 def test_plot_omega_fixed():
     E = 0
     S = base_params.S
@@ -104,6 +195,19 @@ def test_plot_omega_fixed():
     assert fig
 
 
+def test_plot_omega_fixed_save_fig(tmpdir):
+    E = 0
+    S = base_params.S
+    age_per_EpS = np.arange(21, S + 21)
+    omega_SS_orig = base_params.omega_SS
+    omega_SSfx = base_params.omega_SS
+    parameter_plots.plot_omega_fixed(
+        age_per_EpS, omega_SS_orig, omega_SSfx, E, S, output_dir=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'OrigVsFixSSpop.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 def test_plot_imm_fixed():
     E = 0
     S = base_params.S
@@ -113,6 +217,20 @@ def test_plot_imm_fixed():
     fig = parameter_plots.plot_imm_fixed(
         age_per_EpS, imm_rates_orig, imm_rates_adj, E, S)
     assert fig
+
+
+def test_plot_imm_fixed_save_fig(tmpdir):
+    E = 0
+    S = base_params.S
+    age_per_EpS = np.arange(21, S + 21)
+    imm_rates_orig = base_params.imm_rates[0, :]
+    imm_rates_adj = base_params.imm_rates[-1, :]
+    parameter_plots.plot_imm_fixed(
+        age_per_EpS, imm_rates_orig, imm_rates_adj, E, S,
+        output_dir=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'OrigVsAdjImm.png'))
+
+    assert isinstance(img, np.ndarray)
 
 
 def test_plot_population_path():
@@ -129,9 +247,25 @@ def test_plot_population_path():
     assert fig
 
 
+def test_plot_population_path_save_fig(tmpdir):
+    E = 0
+    S = base_params.S
+    age_per_EpS = np.arange(21, S + 21)
+    pop_2013_pct = base_params.omega[0, :]
+    omega_path_lev = base_params.omega.T
+    omega_SSfx = base_params.omega_SS
+    curr_year = base_params.start_year
+    parameter_plots.plot_population_path(
+        age_per_EpS, pop_2013_pct, omega_path_lev, omega_SSfx,
+        curr_year, E, S, output_dir=tmpdir)
+    img = mpimg.imread(os.path.join(tmpdir, 'PopDistPath.png'))
+
+    assert isinstance(img, np.ndarray)
+
+
 # TODO:
-# gen_3Dscatters_hist
-# txfunc_graph
+# gen_3Dscatters_hist -- requires microdata df
+# txfunc_graph - require micro data df
 # txfunc_sse_plot
 
 
@@ -145,3 +279,20 @@ def test_plot_income_data():
         ages, abil_midp, abil_pcts, emat)
 
     assert fig
+
+
+def test_plot_income_data_save_fig(tmpdir):
+    ages = np.linspace(20 + 0.5, 100 - 0.5, 80)
+    abil_midp = np.array([0.125, 0.375, 0.6, 0.75, 0.85, 0.945, 0.995])
+    abil_pcts = np.array([0.25, 0.25, 0.2, 0.1, 0.1, 0.09, 0.01])
+    age_wgts = np.ones(80) * 1 / 80
+    emat = income.get_e_orig(age_wgts, abil_pcts)
+    parameter_plots.plot_income_data(
+        ages, abil_midp, abil_pcts, emat, output_dir=tmpdir)
+    img1 = mpimg.imread(os.path.join(tmpdir, 'ability_3D_lev.png'))
+    img2 = mpimg.imread(os.path.join(tmpdir, 'ability_3D_log.png'))
+    img3 = mpimg.imread(os.path.join(tmpdir, 'ability_2D_log.png'))
+
+    assert isinstance(img1, np.ndarray)
+    assert isinstance(img2, np.ndarray)
+    assert isinstance(img3, np.ndarray)
