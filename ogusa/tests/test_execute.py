707570fc13e52cc03dd1aa554f8b68c37e927d2e
jdebacker <jason.debacker@gmail.com>
2020-04-07 20:14:44 -0400

    add tax func paths to test_execute

--- ogusa/tests/test_execute.py
+++ ogusa/tests/test_execute.py
@@ -1,14 +1,19 @@
 import pytest
 from ogusa import SS, TPI
 from ogusa.execute import runner
+import os
+
+CUR_PATH = os.path.abspath(os.path.dirname(__file__))
+BASELINE_DIR = os.path.join(CUR_PATH, 'OUTPUT_BASELINE')
+REFORM_DIR = os.path.join(CUR_PATH, 'OUTPUT_REFORM')
+
+# Monkey patch enforcement flag since small data won't pass checks
+SS.ENFORCE_SOLUTION_CHECKS = False
+TPI.ENFORCE_SOLUTION_CHECKS = False
 
 
 @pytest.mark.full_run
 def test_runner_baseline():
-    # Monkey patch enforcement flag since small data won't pass checks
-    SS.ENFORCE_SOLUTION_CHECKS = False
-    TPI.ENFORCE_SOLUTION_CHECKS = False
-    BASELINE_DIR = './OUTPUT_BASELINE_'
     runner(output_base=BASELINE_DIR, baseline_dir=BASELINE_DIR,
            test=True, time_path=True, baseline=True, run_micro=False,
            data='cps')
@@ -16,10 +21,6 @@ def test_runner_baseline():
 
 @pytest.mark.full_run
 def test_runner_reform():
-    # Monkey patch enforcement flag since small data won't pass checks
-    SS.ENFORCE_SOLUTION_CHECKS = False
-    TPI.ENFORCE_SOLUTION_CHECKS = False
-    BASELINE_DIR = './OUTPUT_BASELINE_'
-    runner(output_base=BASELINE_DIR, baseline_dir=BASELINE_DIR,
+    runner(output_base=REFORM_DIR, baseline_dir=BASELINE_DIR,
            test=True, time_path=False, baseline=False, run_micro=False,
            data='cps')
