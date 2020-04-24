b79209f33c280ac67a96962d5b19f0a51eaa1dd7
jdebacker <jason.debacker@gmail.com>
2020-04-24 16:56:09 -0400

    add test of constant demographics with small open parameterization

--- ogusa/tests/test_basic.py
+++ ogusa/tests/test_basic.py
@@ -65,3 +65,41 @@ def test_constant_demographics_TPI():
     tpi_output = TPI.run_TPI(spec, None)
     assert(np.allclose(tpi_output['bmat_splus1'][:spec.T, :, :],
                        ss_outputs['bssmat_splus1']))
+
+
+@pytest.mark.full_run
+def test_constant_demographics_TPI_small_open():
+    '''
+    This tests solves the model under the assumption of constant
+    demographics, a balanced budget, and tax functions that do not vary
+    over time, as well as with a small open economy assumption.
+    '''
+    # Create output directory structure
+    spec = Specifications(run_micro=False, output_base=OUTPUT_DIR,
+                          baseline_dir=OUTPUT_DIR, test=False,
+                          time_path=True, baseline=True, iit_reform={},
+                          guid='')
+    og_spec = {'constant_demographics': True, 'budget_balance': True,
+               'zero_taxes': True, 'maxiter': 2,
+               'r_gov_shift': 0.0, 'zeta_D': [0.0, 0.0],
+               'zeta_K': [1.0], 'debt_ratio_ss': 1.0,
+               'initial_foreign_debt_ratio': 0.0,
+               'start_year': 2019, 'cit_rate': [0.0],
+               'PIA_rate_bkt_1': 0.0, 'PIA_rate_bkt_2': 0.0,
+               'PIA_rate_bkt_3': 0.0,
+               'eta': (spec.omega_SS.reshape(spec.S, 1) *
+                       spec.lambdas.reshape(1, spec.J))}
+    spec.update_specifications(og_spec)
+    spec.get_tax_function_parameters(None, False,
+                                     tax_func_path=TAX_FUNC_PATH)
+    # Run SS
+    ss_outputs = SS.run_SS(spec, None)
+    # save SS results
+    utils.mkdirs(os.path.join(OUTPUT_DIR, "SS"))
+    ss_dir = os.path.join(OUTPUT_DIR, "SS", "SS_vars.pkl")
+    with open(ss_dir, "wb") as f:
+        pickle.dump(ss_outputs, f)
+    # Run TPI
+    tpi_output = TPI.run_TPI(spec, None)
+    assert(np.allclose(tpi_output['bmat_splus1'][:spec.T, :, :],
+                       ss_outputs['bssmat_splus1']))
