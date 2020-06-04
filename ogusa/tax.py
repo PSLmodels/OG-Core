9411111012c8478d61e5e6abd2e6b0631cf07a98
jdebacker <jason.debacker@gmail.com>
2020-06-04 08:35:33 -0400

    create income and payroll tax liab and pension amount functions

--- ogusa/tax.py
+++ ogusa/tax.py
@@ -442,3 +442,144 @@ def total_taxes(r, w, b, n, bq, factor, tr, theta, t, j, shift, method,
     total_tax = T_I + T_P + T_BQ + T_W - tr
 
     return total_tax
+
+
+def income_tax_liab(r, w, b, n, factor, t, j, method, e, etr_params, p):
+    '''
+    Calculate income and payroll tax liability for each household
+
+    Args:
+        r (array_like): real interest rate
+        w (array_like): real wage rate
+        b (Numpy array): savings
+        n (Numpy array): labor supply
+        factor (scalar): scaling factor converting model units to
+            dollars
+        t (int): time period
+        j (int): index of lifetime income group
+        method (str): adjusts calculation dimensions based on 'SS' or
+            'TPI'
+        e (Numpy array): effective labor units
+        etr_params (Numpy array): effective tax rate function parameters
+        p (OG-USA Specifications object): model parameters
+
+    Returns:
+        T_I (Numpy array): total income and payroll taxes paid for each
+            household
+
+    '''
+    if j is not None:
+        if method == 'TPI':
+            if b.ndim == 2:
+                r = r.reshape(r.shape[0], 1)
+                w = w.reshape(w.shape[0], 1)
+    else:
+        if method == 'TPI':
+            r = utils.to_timepath_shape(r)
+            w = utils.to_timepath_shape(w)
+
+    income = r * b + w * e * n
+    labor_income = w * e * n
+    T_I = ETR_income(r, w, b, n, factor, e, etr_params, p) * income
+    if method == 'SS':
+        T_P = p.tau_payroll[-1] * labor_income
+    elif method == 'TPI':
+        length = w.shape[0]
+        if len(b.shape) == 1:
+            T_P = p.tau_payroll[t: t + length] * labor_income
+        elif len(b.shape) == 2:
+            T_P = (p.tau_payroll[t: t + length].reshape(length, 1) *
+                   labor_income)
+        else:
+            T_P = (p.tau_payroll[t:t + length].reshape(length, 1, 1) *
+                   labor_income)
+    elif method == 'TPI_scalar':
+        T_P = p.tau_payroll[0] * labor_income
+
+    income_payroll_tax_liab = T_I + T_P
+
+    return income_payroll_tax_liab
+
+
+def pension_amount(r, w, b, n, bq, factor, tr, theta, t, j, shift, method,
+                   e, etr_params, p):
+    '''
+    Calculate public pension benefit amounts for each household.
+
+    Args:
+        r (array_like): real interest rate
+        w (array_like): real wage rate
+        b (Numpy array): savings
+        n (Numpy array): labor supply
+        bq (Numpy array): bequests received
+        factor (scalar): scaling factor converting model units to
+            dollars
+        tr (Numpy array): government transfers to the household
+        theta (Numpy array): social security replacement rate value for
+            lifetime income group j
+        t (int): time period
+        j (int): index of lifetime income group
+        shift (bool): whether computing for periods 0--s or 1--(s+1),
+            =True for 1--(s+1)
+        method (str): adjusts calculation dimensions based on 'SS' or
+            'TPI'
+        e (Numpy array): effective labor units
+        etr_params (Numpy array): effective tax rate function parameters
+        p (OG-USA Specifications object): model parameters
+
+    Returns:
+        total_taxes (Numpy array): net taxes paid for each household
+
+    '''
+    if j is not None:
+        if method == 'TPI':
+            if b.ndim == 2:
+                r = r.reshape(r.shape[0], 1)
+                w = w.reshape(w.shape[0], 1)
+    else:
+        if method == 'TPI':
+            r = utils.to_timepath_shape(r)
+            w = utils.to_timepath_shape(w)
+
+    pension = np.zeros_like(n)
+    if method == 'SS':
+        # Depending on if we are looking at b_s or b_s+1, the
+        # entry for retirement will change (it shifts back one).
+        # The shift boolean makes sure we start replacement rates
+        # at the correct age.
+        if shift is False:
+            pension[p.retire[-1]:] = theta * w
+        else:
+            pension[p.retire[-1] - 1:] = theta * w
+    elif method == 'TPI':
+        length = w.shape[0]
+        if not shift:
+            # retireTPIis different from retire, because in TPincomewe are
+            # counting backwards with different length lists.  This will
+            # always be the correct location of retirement, depending
+            # on the shape of the lists.
+            retireTPI = (p.retire[t: t + length] - p.S)
+        else:
+            retireTPI = (p.retire[t: t + length] - 1 - p.S)
+        if len(b.shape) == 1:
+            if not shift:
+                retireTPI = p.retire[t] - p.S
+            else:
+                retireTPI = p.retire[t] - 1 - p.S
+            pension[retireTPI:] = (
+                theta[j] * p.replacement_rate_adjust[t] * w[retireTPI:])
+        elif len(b.shape) == 2:
+            for tt in range(pension.shape[0]):
+                pension[tt, retireTPI[tt]:] = (
+                    theta * p.replacement_rate_adjust[t + tt] * w[tt])
+        else:
+            for tt in range(pension.shape[0]):
+                pension[tt, retireTPI[tt]:, :] = (
+                    theta.reshape(1, p.J) *
+                    p.replacement_rate_adjust[t + tt] * w[tt])
+    elif method == 'TPI_scalar':
+        # The above methods won't work if scalars are used.  This option
+        # is only called by the SS_TPI_firstdoughnutring function in TPI.
+        pension = theta * p.replacement_rate_adjust[0] * w
+
+    return pension
