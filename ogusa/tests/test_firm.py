1594a5c22528aacdfb55d364e31f432aeaf3a192
jdebacker <jason.debacker@gmail.com>
2020-06-18 18:33:17 -0400

    update param values, expected NPV

--- ogusa/tests/test_firm.py
+++ ogusa/tests/test_firm.py
@@ -95,156 +95,6 @@ p1.update_specifications(new_param_values1)
 # assign values for Y and K variables
 Y1 = np.array([2.0])
 K1 = np.array([1.0])
-expected1 = np.array([0.890898718])
-p2 = Specifications()
-new_param_values2 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'tau_b': [0.5],
-    'delta_tau_annual': [0.35],
-    'epsilon': 0.5,
-    'delta_annual': [0.5]
-}
-# update parameters instance with new values for test
-p2.update_specifications(new_param_values2)
-expected2 = np.array([2.0])
-p3 = Specifications()
-new_param_values3 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'tau_b': [0.5],
-    'delta_tau_annual': [0.35],
-    'epsilon': 1.0,
-    'delta_annual': [0.5]
-}
-# update parameters instance with new values for test
-p3.update_specifications(new_param_values3)
-expected3 = np.array([1.0])
-p4 = Specifications()
-new_param_values4 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'tau_b': [0.5],
-    'delta_tau_annual': [0.35],
-    'epsilon': 1.2,
-    'delta_annual': [0.5],
-    'T': 3,
-    'S': 3
-}
-# update parameters instance with new values for test
-p4.update_specifications(new_param_values4)
-Y4 = np.array([3.0, 3.2, 3.8])
-K4 = np.array([1.8, 1.2, 1.0])
-expected4 = np.array([0.765321585, 1.13225422, 1.520978242])
-
-p5 = Specifications()
-new_param_values5 = {
-    'Z': [1.5, 2.5, 0.6],
-    'gamma': 0.5,
-    'tau_b': [0.2, 0.0, 0.5],
-    'delta_tau_annual': [0.35, 0.2, 0.1],
-    'epsilon': 1.2,
-    'delta_annual': [0.5],
-    'T': 3,
-    'S': 3
-}
-# update parameters instance with new values for test
-p5.update_specifications(new_param_values5)
-expected5 = np.array([0.919102974, 1.480604103, 1.567905472])
-
-
-@pytest.mark.parametrize('Y,K,p,method,expected',
-                         [(Y1, K1, p1, 'SS', expected1),
-                          (Y1, K1, p2, 'SS', expected2),
-                          (Y1, K1, p3, 'SS', expected3),
-                          (Y4, K4, p4, 'TPI', expected4),
-                          (Y4, K4, p5, 'TPI', expected5)],
-                         ids=['epsilon=1.2,SS', 'epsilon=0.5,SS',
-                              'epsilon=1.0,SS', 'epsilon=1.2,TP',
-                              'epsilon=1.2,TP,varyParams'])
-def test_get_MPK(Y, K, p, method, expected):
-    """
-        choose values that simplify the calculations and are similar to
-        observed values
-    """
-    MPK = firm.get_MPK(Y, K, p, method)
-    assert (np.allclose(MPK, expected))
-
-
-p1 = Specifications()
-new_param_values1 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'epsilon': 0.2
-}
-# update parameters instance with new values for test
-p1.update_specifications(new_param_values1)
-Y1 = np.array([2.0])
-L1 = np.array([1.0])
-expected1 = np.array([16.])
-p2 = Specifications()
-new_param_values2 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'epsilon': 1.5
-}
-# update parameters instance with new values for test
-p2.update_specifications(new_param_values2)
-expected2 = np.array([0.793700526])
-p3 = Specifications()
-new_param_values3 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'epsilon': 1.0
-}
-# update parameters instance with new values for test
-p3.update_specifications(new_param_values3)
-expected3 = np.array([1.0])
-p4 = Specifications()
-new_param_values4 = {
-    'Z': [0.5, 0.47],
-    'gamma': 0.5,
-    'epsilon': 1.2,
-    'T': 3,
-    'S': 3
-}
-# update parameters instance with new values for test
-p4.update_specifications(new_param_values4)
-Y4 = np.array([2.0, 2.0, 2.0])
-L4 = np.array([1.0, 1.0, 1.0])
-expected4 = np.array([0.890898718, 0.881758476, 0.881758476])
-
-
-@pytest.mark.parametrize('Y,L,p,method,expected',
-                         [(Y1, L1, p1, 'SS', expected1),
-                          (Y1, L1, p2, 'SS', expected2),
-                          (Y1, L1, p3, 'SS', expected3),
-                          (Y4, L4, p4, 'TPI', expected4)],
-                         ids=['epsilon=0.2,SS', 'epsilon=1.5,SS',
-                              'epsilon=1.0,SS', 'epsilon=1.2,TP'])
-def test_get_MPL(Y, L, p, method, expected):
-    """
-        choose values that simplify the calculations and are similar to
-        observed values
-    """
-    MPL = firm.get_MPL(Y, L, p, method)
-    assert (np.allclose(MPL, expected, atol=1e-6))
-
-
-p1 = Specifications()
-new_param_values1 = {
-    'Z': [0.5],
-    'gamma': 0.5,
-    'delta_annual': [0.25],
-    'tau_b': [0.5],
-    'delta_tau_annual': [0.35],
-    'epsilon': 1.2
-}
-# update parameters instance with new values for test
-p1.update_specifications(new_param_values1)
-# assign values for Y and K variables
-Y1 = np.array([2.0])
-K1 = np.array([1.0])
 expected1 = np.array([0.59492233])
 p2 = Specifications()
 new_param_values2 = {
@@ -591,9 +441,10 @@ expected4 = np.array([3.39707089, 2.85348453, 2.85348453])
                          ids=['epsilon=1.2,SS', 'epsilon=1.0,SS',
                               'epsilon=0.4,SS', 'epsilon=0.4,TP'])
 def test_get_K(L, r, p, method, expected):
-    '''
-    Test of the fucntion that returns K from the firm's FOC
-    '''
+    """
+        choose values that simplify the calculations and are similar to
+        observed values
+    """
     K = firm.get_K(L, r, p, method)
     assert (np.allclose(K, expected, atol=1e-6))
 
@@ -681,11 +532,13 @@ def test_get_NPV_depr():
     '''
     Test of firm.get_NPV_depr() function.
     '''
-    expected_val = 0.044871795
+    expected_val = 0.194444444
     r = 0.04
     tau_b = 0.35
-    delta_tau - 0.05
+    delta_tau = 0.05
     T = None
     S = None
     method = 'SS'
     test_val = firm.get_NPV_depr(r, tau_b, delta_tau, T, S, method)
+
+    assert np.allclose(test_val, expected_val)
