b4deb30c1d5b1567143f252b11f55598ada16d8a
jdebacker <jason.debacker@gmail.com>
2020-07-26 17:26:06 -0400

    change placement of legend '

--- ogusa/parameter_plots.py
+++ ogusa/parameter_plots.py
@@ -222,7 +222,7 @@ def plot_elliptical_u(p, plot_MU=True, include_title=False, path=None):
         plt.ylabel(r'Marginal Utility')
     else:
         plt.ylabel(r'Utility')
-    plt.legend(loc='center right')
+    plt.legend(loc='top left')
     if path is None:
         return fig
     else:
