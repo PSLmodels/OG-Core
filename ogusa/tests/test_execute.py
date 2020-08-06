37dcf49ab0a09b72394cac41ebaa31a312c08bf7
jdebacker <jason.debacker@gmail.com>
2020-08-06 16:19:53 -0400

    updates to use checked in data, fewer processors

--- ogusa/tests/test_execute.py
+++ ogusa/tests/test_execute.py
@@ -4,10 +4,12 @@ import pytest
 from ogusa import SS, TPI
 from ogusa.execute import runner
 import os
-NUM_WORKERS = min(multiprocessing.cpu_count(), 7)
+NUM_WORKERS = min(multiprocessing.cpu_count(), 2)
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 BASELINE_DIR = os.path.join(CUR_PATH, 'OUTPUT_BASELINE')
 REFORM_DIR = os.path.join(CUR_PATH, 'OUTPUT_REFORM')
+BASE_TAX = os.path.join(CUR_PATH, 'TxFuncEst_baseline.pkl')
+REFORM_TAX = os.path.join(CUR_PATH, 'TxFuncEst_policy.pkl')
 
 # Monkey patch enforcement flag since small data won't pass checks
 SS.ENFORCE_SOLUTION_CHECKS = False
@@ -27,12 +29,16 @@ def dask_client():
 @pytest.mark.full_run
 def test_runner_baseline(dask_client):
     runner(output_base=BASELINE_DIR, baseline_dir=BASELINE_DIR,
-           test=True, time_path=True, baseline=True, run_micro=False,
-           data='cps', client=dask_client, num_workers=NUM_WORKERS)
+           test=True, time_path=True, baseline=True,
+           og_spec={'start_year': 2018}, run_micro=False,
+           tax_func_path=BASE_TAX, data='cps', client=dask_client,
+           num_workers=NUM_WORKERS)
 
 
 @pytest.mark.full_run
 def test_runner_reform(dask_client):
     runner(output_base=REFORM_DIR, baseline_dir=BASELINE_DIR,
-           test=True, time_path=False, baseline=False, run_micro=False,
-           data='cps', client=dask_client, num_workers=NUM_WORKERS)
+           test=True, time_path=False, baseline=False,
+           og_spec={'start_year': 2018}, run_micro=False,
+           tax_func_path=REFORM_TAX, data='cps', client=dask_client,
+           num_workers=NUM_WORKERS)
