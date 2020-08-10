1fba23ba726cfbd2b4138c6223c1c04316da1f3f
Richard Evans <rickecon@gmail.com>
2020-08-10 15:48:00 -0600

    Added psutil in environment.yml and updated memory usage in test_execute.py

--- ogusa/tests/test_execute.py
+++ ogusa/tests/test_execute.py
@@ -1,10 +1,29 @@
 import multiprocessing
 from distributed import Client, LocalCluster
+import numpy as np
+import psutil
 import pytest
 from ogusa import SS, TPI
 from ogusa.execute import runner
 import os
-NUM_WORKERS = min(multiprocessing.cpu_count(), 7)
+
+# Get named tuple for RAM stats and compute total available RAM in GB and set
+# number of workers
+num_workers_max_sys = multiprocessing.cpu_count()
+RAM_stats = psutil.virtual_memory()
+print(RAM_stats)
+RAM_total_bytes = RAM_stats.total
+RAM_total_GB = RAM_total_bytes /  1073741824
+print(RAM_total_GB)
+mem_per_wkr_tot = RAM_total_GB / num_workers_max_sys
+num_workers_max = min(num_workers_max_sys, 7)
+# NUM_WORKERS = min(multiprocessing.cpu_count(), 7)
+NUM_WORKERS = np.minimum(num_workers_max,
+                         int(np.floor((0.6 * RAM_total_GB) /
+                                      mem_per_wkr_tot)))
+print('Max num worders=', num_workers_max, ', and NUM_WORKERS=', NUM_WORKERS)
+
+# Set paths
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 BASELINE_DIR = os.path.join(CUR_PATH, 'OUTPUT_BASELINE')
 REFORM_DIR = os.path.join(CUR_PATH, 'OUTPUT_REFORM')
