15013bae25957576f87fd521e13814bcb967ef41
jdebacker <jason.debacker@gmail.com>
2020-09-01 22:31:32 -0400

    update functions to test vars

--- cs-config/cs_config/tests/test_functions.py
+++ cs-config/cs_config/tests/test_functions.py
@@ -23,27 +23,6 @@ class TestFunctions1(CoreTestFunctions):
 #     assert df.loc[0, 'Change from Baseline (pp)'] != 0
 
 
-@pytest.mark.full_run
-def test_convert_adj():
-    adj = {
-        "STD": [
-            {"MARS": "single", "year": "2019", "value": 0},
-            {"MARS": "mjoint", "year": 2019, "value": 1}
-        ],
-        "EITC_c": [{"EIC": "0kids", "year": "2019", "value": 1000.0}],
-        "BEN_ssi_repeal": [
-            {"year": 2019, "value": True}
-        ]
-    }
-    res = helpers.convert_adj(adj, 2019)
-    assert res == {
-        "STD": {
-            2019: [0, 1, 12200.0, 18350.0, 24400.0]
-        },
-        "EITC_c": {
-            2019: [1000.0, 3526.0, 5828.0, 6557.0]
-        },
-        "BEN_ssi_repeal": {
-            2019: True
-        }
-    }
+def test_convert_defaults():
+    # TODO
+    return
