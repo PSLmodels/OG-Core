9d98145eef450681146fa9705a2b058c5a1606ba
Richard Evans <rickecon@gmail.com>
2020-09-08 23:52:51 -0600

    Got rid of extra imports in test_run_example.py

--- ogusa/tests/test_run_example.py
+++ ogusa/tests/test_run_example.py
@@ -1,20 +1,11 @@
 #### import modules (same as run_ogusa_example)
 import multiprocessing
-from distributed import Client
 import time
-import numpy as np
 import os, sys
-import taxcalc
-from taxcalc import Calculator
-from ogusa import output_tables as ot
-from ogusa import output_plots as op
-from ogusa.execute import runner
-from ogusa.constants import REFORM_DIR, BASELINE_DIR
-from ogusa.utils import safe_read_pickle
 
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
+# currentdir = os.path.dirname(os.path.realpath(__file__))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.append(parentdir)
 from run_examples import run_ogusa_example
 
 
@@ -26,18 +17,17 @@ def test_run_ogusa_example(f = call_run_ogusa_example):
     '''
     test that run_ogusa_example runs for at least 5 minutes
     '''
-    p = multiprocessing.Process(target = f, 
+    p = multiprocessing.Process(target = f,
                                 name="run_ogusa_example", args=())
     p.start()
     time.sleep(300)
     if p.is_alive():
-        p.terminate() 
+        p.terminate()
         p.join()
         timetest = True
     else:
         print("run_ogusa_example did not run for minimum time")
         timetest = False
     print('timetest ==', timetest)
-    
-    assert timetest == True
 
+    assert timetest == True
