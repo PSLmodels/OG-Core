14286e88fd06ad04488c15873da33513d46f4e27
jdebacker <jason.debacker@gmail.com>
2020-09-28 21:03:52 -0400

    fix typo

--- ogusa/TPI.py
+++ ogusa/TPI.py
@@ -28,7 +28,7 @@ ENFORCE_SOLUTION_CHECKS = True
 def get_initial_SS_values(p):
     '''
     Get values of variables for the initial period and the steady state
-    equlibrium values.
+    equilibrium values.
 
     Args:
         p (OG-USA Specifications object): model parameters
@@ -376,6 +376,8 @@ def inner_loop(guesses, outer_loop_vars, initial_values, j, ind, p):
         n_mat[t + ind, ind] = n_vec
 
     print('Type ', j, ' max euler error = ', euler_errors.max())
+    print('Type ', j, ' max euler error labor = ', euler_errors[:, :p.S].max())
+    print('Type ', j, ' max euler error savings = ', euler_errors[:, p.S:].max())
 
     return euler_errors, b_mat, n_mat
 
