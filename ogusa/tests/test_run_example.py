d05b787e904ce1c967ce6c6efb4930bba94185ba
jdebacker <jason.debacker@gmail.com>
2020-10-01 16:04:31 -0400

    update pycroft latest

--- ogusa/tests/test_run_example.py
+++ ogusa/tests/test_run_example.py
@@ -1,43 +1,127 @@
 '''
-This test tests whether starting a `run_ogusa_example.py` run of the model does
-not break down (is still running) after 5 minutes or 300 seconds.
+Example script for setting policy and running OG-USA.
 '''
 
+# import modules
 import multiprocessing
+from distributed import Client
 import time
+import numpy as np
 import os, sys
-import importlib.util
-from pathlib import Path
+import taxcalc
+from taxcalc import Calculator
+from ogusa import output_tables as ot
+from ogusa import output_plots as op
+from ogusa.execute import runner
+from ogusa.constants import REFORM_DIR, BASELINE_DIR
+from ogusa.utils import safe_read_pickle
 
 
-def call_run_ogusa_example():
-    cur_path = os.path.split(os.path.abspath(__file__))[0]
-    path = Path(cur_path)
-    roe_fldr = os.path.join(path.parent.parent, "run_examples")
-    roe_file_path = os.path.join(roe_fldr, "run_ogusa_example.py")
-    spec = importlib.util.spec_from_file_location('run_ogusa_example.py',
-                                                   roe_file_path)
-    roe_module = importlib.util.module_from_spec(spec)
-    sys.modules['run_ogusa_example.py'] = roe_module
-    spec.loader.exec_module(roe_module)
-    roe_module.main()
+def main():
+    # Define parameters to use for multiprocessing
+    client = Client()
+    num_workers = min(multiprocessing.cpu_count(), 2)
+    print('Number of workers = ', num_workers)
+    run_start_time = time.time()
 
+    # Grab a reform JSON file already in Tax-Calculator
+    # In this example the 'reform' is a change to 2017 law (the
+    # baseline policy is tax law in 2018)
+    reform_url = ('https://raw.githubusercontent.com/'
+                  'PSLmodels/Tax-Calculator/' + taxcalc.__version__ +
+                  '/taxcalc/reforms/2017_law.json')
+    ref = Calculator.read_json_param_objects(reform_url, None)
+    iit_reform = ref['policy']
 
-def test_run_ogusa_example(f = call_run_ogusa_example):
+    # Directories to save data
+    CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+    base_dir = os.path.join(CUR_DIR, BASELINE_DIR)
+    reform_dir = os.path.join(CUR_DIR, REFORM_DIR)
+
+    # Set some OG model parameters
+    # See default_parameters.json for more description of these parameters
+    alpha_T = np.zeros(50)  # Adjusting the path of transfer spending
+    alpha_T[0:2] = 0.09
+    alpha_T[2:10] = 0.09 + 0.01
+    alpha_T[10:40] = 0.09 - 0.01
+    alpha_T[40:] = 0.09
+    alpha_G = np.zeros(7)  # Adjusting the path of non-transfer spending
+    alpha_G[0:3] = 0.05 - 0.01
+    alpha_G[3:6] = 0.05 - 0.005
+    alpha_G[6:] = 0.05
+    # Set start year for baseline and reform.
+    START_YEAR = 2021
+    # Also adjust the Frisch elasticity, the start year, the
+    # effective corporate income tax rate, and the SS debt-to-GDP ratio
+    og_spec = {'frisch': 0.41, 'start_year': START_YEAR, 'cit_rate': [0.21],
+               'debt_ratio_ss': 1.0, 'alpha_T': alpha_T.tolist(),
+               'alpha_G': alpha_G.tolist()}
+
     '''
-    test that run_ogusa_example runs for at least 5 minutes
+    ------------------------------------------------------------------------
+    Run baseline policy first
+    ------------------------------------------------------------------------
     '''
-    p = multiprocessing.Process(target = f,
-                                name="run_ogusa_example", args=())
-    p.start()
-    time.sleep(300)
-    if p.is_alive():
-        p.terminate()
-        p.join()
-        timetest = True
-    else:
-        print("run_ogusa_example did not run for minimum time")
-        timetest = False
-    print('timetest ==', timetest)
+    tax_func_path = os.path.join(
+        CUR_DIR, '..', 'ogusa', 'data', 'tax_functions',
+        'TxFuncEst_baseline_CPS.pkl')  # use cached baseline estimates
+    kwargs = {'output_base': base_dir, 'baseline_dir': base_dir,
+              'test': False, 'time_path': False, 'baseline': True,
+              'og_spec': og_spec, 'guid': '_example',
+              'run_micro': False, 'tax_func_path': tax_func_path,
+              'data': 'cps', 'client': client,
+              'num_workers': num_workers}
 
-    assert timetest == True
+    start_time = time.time()
+    runner(**kwargs)
+    print('run time = ', time.time()-start_time)
+
+    '''
+    ------------------------------------------------------------------------
+    Run reform policy
+    ------------------------------------------------------------------------
+    '''
+    # update the effective corporate income tax rate
+    og_spec = {'frisch': 0.41, 'start_year': START_YEAR,
+               'cit_rate': [0.35], 'debt_ratio_ss': 1.0,
+               'alpha_T': alpha_T.tolist(),
+               'alpha_G': alpha_G.tolist()}
+    kwargs = {'output_base': reform_dir, 'baseline_dir': base_dir,
+              'test': False, 'time_path': False, 'baseline': False,
+              'og_spec': og_spec, 'guid': '_example',
+              'iit_reform': iit_reform, 'run_micro': True, 'data': 'cps',
+              'client': client, 'num_workers': num_workers}
+
+    start_time = time.time()
+    runner(**kwargs)
+    print('run time = ', time.time()-start_time)
+
+    # return ans - the percentage changes in macro aggregates and prices
+    # due to policy changes from the baseline to the reform
+    base_tpi = safe_read_pickle(
+        os.path.join(base_dir, 'TPI', 'TPI_vars.pkl'))
+    base_params = safe_read_pickle(
+        os.path.join(base_dir, 'model_params.pkl'))
+    reform_tpi = safe_read_pickle(
+        os.path.join(reform_dir, 'TPI', 'TPI_vars.pkl'))
+    reform_params = safe_read_pickle(
+        os.path.join(reform_dir, 'model_params.pkl'))
+    ans = ot.macro_table(
+        base_tpi, base_params, reform_tpi=reform_tpi,
+        reform_params=reform_params,
+        var_list=['Y', 'C', 'K', 'L', 'r', 'w'], output_type='pct_diff',
+        num_years=10, start_year=og_spec['start_year'])
+
+    # create plots of output
+    op.plot_all(base_dir, reform_dir,
+                os.path.join(CUR_DIR, 'run_example_plots'))
+
+    print("total time was ", (time.time() - run_start_time))
+    print('Percentage changes in aggregates:', ans)
+    # save percentage change output to csv file
+    ans.to_csv('ogusa_example_output.csv')
+    client.close()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
