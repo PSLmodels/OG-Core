cac6f437a8abea648133af542735099ddd008107
jdebacker <jason.debacker@gmail.com>
2020-10-07 22:03:58 -0400

    remove wavg func that not used any more

--- ogusa/utils.py
+++ ogusa/utils.py
@@ -814,22 +814,3 @@ def not_connected(url='http://www.google.com/', timeout=5):
         return False
     except requests.ConnectionError:
         return True
-
-
-def wavg(group, avg_name, weight_name):
-    '''
-    Computes a weighted average.
-    Args:
-        group (Pandas DataFrame): data for the particular grouping
-        avg_name (string): name of variable to compute wgt avg with
-        weight_name (string): name of weighting variables
-    Returns:
-        d (scalar): weighted avg for the group
-    '''
-    warnings.filterwarnings('error')
-    d = group[avg_name]
-    w = group[weight_name]
-    try:
-        return (d * w).sum() / w.sum()
-    except Warning:
-        return d.mean()
