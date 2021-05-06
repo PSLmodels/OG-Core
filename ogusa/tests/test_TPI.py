3bf5bb93040d602f60135458d1c1e519142abf27
jdebacker <jason.debacker@gmail.com>
2021-05-05 22:20:14 -0400

    tpi full runs passing

--- ogusa/tests/test_TPI.py
+++ ogusa/tests/test_TPI.py
@@ -4,11 +4,14 @@ import pytest
 import pickle
 import numpy as np
 import os
+import json
 from ogusa import SS, TPI, utils, firm
 import ogusa.aggregates as aggr
 from ogusa.parameters import Specifications
 NUM_WORKERS = min(multiprocessing.cpu_count(), 7)
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
+
+TEST_PARAM_DICT = json.load(open(os.path.join(CUR_PATH, 'testing_params.json')))
 
 
 @pytest.fixture(scope="module")
@@ -273,14 +276,17 @@ def test_inner_loop(dask_client):
 
 
 g_n_ss = 0.0012907765315350872
-imm_rates = np.load(os.path.join(CUR_PATH, 'old_imm_rates.npy'))
-e = np.load(os.path.join(CUR_PATH, 'old_e.npy'))
-omega_SS = np.load(os.path.join(CUR_PATH, 'old_omega_SS.npy'))
-omega_S_preTP = np.load(os.path.join(CUR_PATH, 'old_omega_S_preTP.npy'))
-omega = np.load(os.path.join(CUR_PATH, 'old_omega.npy'))
-g_n = np.load(os.path.join(CUR_PATH, 'old_g_n.npy'))
+imm_rates = np.load(os.path.join(CUR_PATH, 'imm_rates_tpi.npy'))
+e = np.load(os.path.join(CUR_PATH, 'e_tpi.npy'))
+omega_SS = np.load(os.path.join(CUR_PATH, 'omega_SS_tpi.npy'))
+omega_S_preTP = np.load(os.path.join(CUR_PATH, 'omega_S_preTP_tpi.npy'))
+omega = np.load(os.path.join(CUR_PATH, 'omega_tpi.npy'))
+g_n = np.load(os.path.join(CUR_PATH, 'g_n_tpi.npy'))
 rho = np.load(os.path.join(CUR_PATH, 'old_rho.npy'))
-param_updates1 = {}
+param_updates1 = {'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename1 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_baseline.pkl')
 param_updates2 = {'budget_balance': True, 'alpha_G': [0.0],
@@ -290,42 +296,55 @@ param_updates2 = {'budget_balance': True, 'alpha_G': [0.0],
                   'g_n': g_n, 'rho': rho}
 filename2 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_baseline_balanced_budget.pkl')
-param_updates3 = {}
+param_updates3 = {'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename3 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_reform.pkl')
-param_updates4 = {'baseline_spending': True}
+param_updates4 = {'baseline_spending': True, 'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename4 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_reform_baseline_spend.pkl')
-param_updates5 = {'zeta_K': [1.0]}
+param_updates5 = {'zeta_K': [1.0], 'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename5 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_baseline_small_open.pkl')
-param_updates6 = {'zeta_K': [0.2, 0.2, 0.2, 1.0, 1.0, 1.0, 0.2]}
+param_updates6 = {'zeta_K': [0.2, 0.2, 0.2, 1.0, 1.0, 1.0, 0.2],
+                  'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename6 = os.path.join(
     CUR_PATH, 'test_io_data',
     'run_TPI_outputs_baseline_small_open_some_periods.pkl')
-param_updates7 = {'delta_tau_annual': [0.0]}
+param_updates7 = {'delta_tau_annual': [0.0], 'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename7 = os.path.join(
     CUR_PATH, 'test_io_data',
     'run_TPI_outputs_baseline_delta_tau0.pkl')
 
 
 @pytest.mark.local
-# @pytest.mark.parametrize('baseline,param_updates,filename',
-#                          [(True, param_updates2, filename2),
-#                           (True, param_updates1, filename1),
-#                           (False, param_updates3, filename3),
-#                           (False, param_updates4, filename4),
-#                           (True, param_updates5, filename5),
-#                           (True, param_updates6, filename6),
-#                           (True, param_updates7, filename7)],
-#                          ids=['Baseline, balanced budget', 'Baseline',
-#                               'Reform', 'Reform, baseline spending',
-#                               'Baseline, small open',
-#                               'Baseline, small open some periods',
-#                               'Baseline, delta_tau = 0'])
 @pytest.mark.parametrize('baseline,param_updates,filename',
-                         [(True, param_updates2, filename2)],
-                         ids=['Baseline, balanced budget'])
+                         [(True, param_updates2, filename2),
+                          (True, param_updates1, filename1),
+                          (False, param_updates3, filename3),
+                          (False, param_updates4, filename4),
+                          (True, param_updates5, filename5),
+                          (True, param_updates6, filename6),
+                          (True, param_updates7, filename7)],
+                         ids=['Baseline, balanced budget', 'Baseline',
+                              'Reform', 'Reform, baseline spending',
+                              'Baseline, small open',
+                              'Baseline, small open some periods',
+                              'Baseline, delta_tau = 0'])
 def test_run_TPI_full_run(baseline, param_updates, filename, tmp_path,
                           dask_client):
     '''
@@ -341,11 +360,6 @@ def test_run_TPI_full_run(baseline, param_updates, filename, tmp_path,
                        output_base=output_base, client=dask_client,
                        num_workers=NUM_WORKERS)
     p.update_specifications(param_updates)
-    import cloudpickle
-    param_dir = "new_params_tpi.pkl"
-    with open(param_dir, "wb") as f:
-        cloudpickle.dump((p), f)
-    assert False
 
     # Need to run SS first to get results
     SS.ENFORCE_SOLUTION_CHECKS = False
@@ -373,16 +387,29 @@ def test_run_TPI_full_run(baseline, param_updates, filename, tmp_path,
                                atol=1e-04))
 
 
-param_updates1 = {}
+param_updates1 = {'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename1 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_baseline_2.pkl')
-param_updates2 = {'budget_balance': True, 'alpha_G': [0.0]}
+param_updates2 = {'budget_balance': True, 'alpha_G': [0.0],
+                  'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename2 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_baseline_balanced_budget_2.pkl')
-param_updates3 = {}
+param_updates3 = {'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename3 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_reform_2.pkl')
-param_updates4 = {'baseline_spending': True}
+param_updates4 = {'baseline_spending': True, 'omega_SS': omega_SS,
+                  'g_n_ss': g_n_ss, 'imm_rates': imm_rates, 'e': e,
+                  'omega': omega, 'omega_S_preTP': omega_S_preTP,
+                  'g_n': g_n, 'rho': rho}
 filename4 = os.path.join(CUR_PATH, 'test_io_data',
                          'run_TPI_outputs_reform_baseline_spend_2.pkl')
 
@@ -479,15 +506,42 @@ def test_run_TPI_extra(baseline, param_updates, filename, tmp_path,
     else:
         output_base = os.path.join(CUR_PATH, 'reform')
     p = Specifications(baseline=baseline, baseline_dir=baseline_dir,
-                       output_base=output_base, test=True,
+                       output_base=output_base,
                        client=dask_client, num_workers=NUM_WORKERS)
-    p.update_specifications(param_updates)
-    p.maxiter = 2  # this test runs through just two iterations
-    p.get_tax_function_parameters(
-        None, run_micro=False,
-        tax_func_path=os.path.join(
-            CUR_PATH, '..', 'data', 'tax_functions',
-            'TxFuncEst_baseline_CPS.pkl'))
+    TEST_PARAM_DICT.update(param_updates)
+    p.update_specifications(TEST_PARAM_DICT)
+    p.maxiter = 2  # t`his test runs through just two iterations
+    p.BW = 10
+    dict_params = utils.safe_read_pickle(os.path.join(
+        CUR_PATH, 'TxFuncEst_baseline.pkl'))
+    num_etr_params = dict_params['tfunc_etr_params_S'].shape[2]
+    num_mtrx_params = dict_params['tfunc_mtrx_params_S'].shape[2]
+    num_mtry_params = dict_params['tfunc_mtry_params_S'].shape[2]
+    p.mean_income_data = dict_params['tfunc_avginc'][0]
+    p.etr_params = np.empty((p.T + p.S, p.S, num_etr_params))
+    p.etr_params[:p.BW, :, :] =\
+        np.transpose(
+            dict_params['tfunc_etr_params_S'][:p.S, :p.BW, :],
+            axes=[1, 0, 2])
+    p.etr_params[p.BW:, :, :] = np.tile(np.transpose(
+        dict_params['tfunc_etr_params_S'][:p.S, -1, :].reshape(
+            p.S, 1, num_etr_params), axes=[1, 0, 2]), (p.T - p.BW, 1, 1))
+    p.mtrx_params = np.empty((p.T + p.S, p.S, num_mtrx_params))
+    p.mtrx_params[:p.BW, :, :] =\
+        np.transpose(
+            dict_params['tfunc_mtrx_params_S'][:p.S, :p.BW, :],
+            axes=[1, 0, 2])
+    p.mtrx_params[p.BW:, :, :] = np.tile(np.transpose(
+        dict_params['tfunc_mtrx_params_S'][:p.S, -1, :].reshape(
+            p.S, 1, num_mtrx_params), axes=[1, 0, 2]), (p.T - p.BW, 1, 1))
+    p.mtry_params = np.empty((p.T + p.S, p.S, num_mtry_params))
+    p.mtry_params[:p.BW, :, :] =\
+        np.transpose(
+            dict_params['tfunc_mtry_params_S'][:p.S, :p.BW, :],
+            axes=[1, 0, 2])
+    p.mtry_params[p.BW:, :, :] = np.tile(np.transpose(
+        dict_params['tfunc_mtry_params_S'][:p.S, -1, :].reshape(
+            p.S, 1, num_mtry_params), axes=[1, 0, 2]), (p.T - p.BW, 1, 1))
 
     # Need to run SS first to get results
     SS.ENFORCE_SOLUTION_CHECKS = False
