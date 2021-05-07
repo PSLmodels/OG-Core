d50c05a0d9278fcfc824afe4b10ac30b1f343b5e
jdebacker <jason.debacker@gmail.com>
2021-05-06 22:39:13 -0400

    update run example for new api

--- run_examples/run_ogusa_example.py
+++ run_examples/run_ogusa_example.py
@@ -24,15 +24,6 @@ def main():
     print('Number of workers = ', num_workers)
     run_start_time = time.time()
 
-    # Grab a reform JSON file already in Tax-Calculator
-    # In this example the 'reform' is a change to 2017 law (the
-    # baseline policy is tax law in 2018)
-    reform_url = ('https://raw.githubusercontent.com/'
-                  'PSLmodels/Tax-Calculator/' + taxcalc.__version__ +
-                  '/taxcalc/reforms/2017_law.json')
-    ref = Calculator.read_json_param_objects(reform_url, None)
-    iit_reform = ref['policy']
-
     # Directories to save data
     CUR_DIR = os.path.dirname(os.path.realpath(__file__))
     base_dir = os.path.join(CUR_DIR, BASELINE_DIR)
@@ -66,11 +57,9 @@ def main():
         CUR_DIR, '..', 'ogusa', 'data', 'tax_functions',
         'TxFuncEst_baseline_CPS.pkl')  # use cached baseline estimates
     kwargs = {'output_base': base_dir, 'baseline_dir': base_dir,
-              'test': False, 'time_path': True, 'baseline': True,
+              'time_path': True, 'baseline': True,
               'og_spec': og_spec, 'guid': '_example',
-              'run_micro': False, 'tax_func_path': tax_func_path,
-              'data': 'cps', 'client': client,
-              'num_workers': num_workers}
+              'client': client, 'num_workers': num_workers}
 
     start_time = time.time()
     runner(**kwargs)
@@ -82,14 +71,10 @@ def main():
     ------------------------------------------------------------------------
     '''
     # update the effective corporate income tax rate
-    og_spec = {'frisch': 0.41, 'start_year': START_YEAR,
-               'cit_rate': [0.35], 'debt_ratio_ss': 1.0,
-               'alpha_T': alpha_T.tolist(),
-               'alpha_G': alpha_G.tolist()}
+    og_spec = og_spec.update({'cit_rate': [0.35]})
     kwargs = {'output_base': reform_dir, 'baseline_dir': base_dir,
-              'test': False, 'time_path': True, 'baseline': False,
+              'time_path': True, 'baseline': False,
               'og_spec': og_spec, 'guid': '_example',
-              'iit_reform': iit_reform, 'run_micro': True, 'data': 'cps',
               'client': client, 'num_workers': num_workers}
 
     start_time = time.time()
