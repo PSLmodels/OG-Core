d9d5ee7b35469bc14f8cbeaf977726f029304af7
Richard Evans <rickecon@gmail.com>
2021-07-26 16:12:21 -0600

    Updated OGUSAplots.mplstyle to OGcorePlots.mplstyle and corresponding references

--- ogcore/parameter_plots.py
+++ ogcore/parameter_plots.py
@@ -7,7 +7,7 @@ from ogcore.constants import GROUP_LABELS
 from ogcore import utils, txfunc
 from ogcore.constants import DEFAULT_START_YEAR, VAR_LABELS
 CUR_PATH = os.path.split(os.path.abspath(__file__))[0]
-style_file = os.path.join(CUR_PATH, 'OGUSAplots.mplstyle')
+style_file = os.path.join(CUR_PATH, 'OGcorePlots.mplstyle')
 plt.style.use(style_file)
 
 
