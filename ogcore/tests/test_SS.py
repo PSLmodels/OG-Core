9766fe72996427c871fc6a8363b8f36bcacca6c1
jdebacker <jason.debacker@gmail.com>
2021-08-07 07:52:05 -0400

    update run SS test and results

--- ogcore/tests/test_SS.py
+++ ogcore/tests/test_SS.py
@@ -404,11 +404,10 @@ def test_euler_equation_solver(input_tuple, ubi_j, p, expected):
     assert(np.allclose(np.array(test_list), np.array(expected)))
 
 
-imm_rates_deltatau0 = np.load(os.path.join(CUR_PATH, 'old_imm_rates_deltatau0.npy'))
 param_updates1 = {}
-# param_updates1 = {'start_year': 2020}
 filename1 = 'run_SS_baseline_outputs.pkl'
-param_updates2 = {'use_zeta': True}
+param_updates2 = {'use_zeta': True, 'initial_guess_r_SS': 0.10,
+                  'initial_guess_TR_SS': 0.02}
 filename2 = 'run_SS_baseline_use_zeta.pkl'
 param_updates3 = {'zeta_K': [1.0]}
 filename3 = 'run_SS_baseline_small_open.pkl'
@@ -420,7 +419,8 @@ param_updates6 = {'use_zeta': True}
 filename6 = 'run_SS_reform_use_zeta.pkl'
 param_updates7 = {'zeta_K': [1.0]}
 filename7 = 'run_SS_reform_small_open.pkl'
-param_updates8 = {'zeta_K': [1.0], 'use_zeta': True}
+param_updates8 = {'zeta_K': [1.0], 'use_zeta': True,
+                  'initial_guess_TR_SS': 0.02}
 filename8 = 'run_SS_reform_small_open_use_zeta.pkl'
 param_updates9 = {'baseline_spending': True}
 filename9 = 'run_SS_reform_baseline_spend.pkl'
@@ -435,10 +435,11 @@ filename11 = 'run_SS_baseline_delta_tau0.pkl'
 # failures for the baseline spending=True tests which depend on the
 # output of the baseline run just prior
 @pytest.mark.parametrize('baseline,param_updates,filename',
-                         [(True, param_updates1, filename1),
+                         [
+                          (True, param_updates1, filename1),
                           (False, param_updates9, filename9),
                           (True, param_updates2, filename2),
-                        #   (False, param_updates10, filename10),
+                          (False, param_updates10, filename10),
                           (True, param_updates3, filename3),
                           (True, param_updates4, filename4),
                           (False, param_updates5, filename5),
@@ -447,9 +448,10 @@ filename11 = 'run_SS_baseline_delta_tau0.pkl'
                           (False, param_updates8, filename8),
                           (False, param_updates11, filename11)
                           ],
-                         ids=['Baseline', 'Reform, baseline spending',
+                         ids=[
+                              'Baseline', 'Reform, baseline spending',
                               'Baseline, use zeta',
-                            #   'Reform, baseline spending, use zeta',
+                              'Reform, baseline spending, use zeta',
                               'Baseline, small open',
                               'Baseline, small open use zeta',
                               'Reform', 'Reform, use zeta',
@@ -457,157 +459,38 @@ filename11 = 'run_SS_baseline_delta_tau0.pkl'
                               'Reform, small open use zeta',
                               'Baseline, delta_tau=0'
                               ])
+
 @pytest.mark.local
 def test_run_SS(baseline, param_updates, filename, dask_client):
     # Test SS.run_SS function.  Provide inputs to function and
     # ensure that output returned matches what it has been before.
-    SS.ENFORCE_SOLUTION_CHECKS = False
+    SS.ENFORCE_SOLUTION_CHECKS = True#False
+    # if running reform, then need to solve baseline first to get values
     if baseline is False:
         p_base = Specifications(
             output_base=constants.BASELINE_DIR,
             baseline_dir=constants.BASELINE_DIR,
-            time_path=False, baseline=True,
+            baseline=True,
             num_workers=NUM_WORKERS)
         p_base.update_specifications(param_updates)
-        p_base.BW = 10
-        p_base.frac_tax_payroll = np.zeros(p_base.frac_tax_payroll.shape)
-        dict_params = utils.safe_read_pickle(os.path.join(
-            CUR_PATH, 'TxFuncEst_baseline.pkl'))
-        num_etr_params = dict_params['tfunc_etr_params_S'].shape[2]
-        num_mtrx_params = dict_params['tfunc_mtrx_params_S'].shape[2]
-        num_mtry_params = dict_params['tfunc_mtry_params_S'].shape[2]
-        p_base.mean_income_data = dict_params['tfunc_avginc'][0]
-        p_base.etr_params = np.empty((p_base.T, p_base.S, num_etr_params))
-        p_base.etr_params[:p_base.BW, :, :] =\
-            np.transpose(
-                dict_params['tfunc_etr_params_S'][:p_base.S, :p_base.BW, :],
-                axes=[1, 0, 2])
-        p_base.etr_params[p_base.BW:, :, :] = np.tile(np.transpose(
-            dict_params['tfunc_etr_params_S'][:p_base.S, -1, :].reshape(
-                p_base.S, 1, num_etr_params), axes=[1, 0, 2]), (p_base.T - p_base.BW, 1, 1))
-        p_base.mtrx_params = np.empty((p_base.T, p_base.S, num_mtrx_params))
-        p_base.mtrx_params[:p_base.BW, :, :] =\
-            np.transpose(
-                dict_params['tfunc_mtrx_params_S'][:p_base.S, :p_base.BW, :],
-                axes=[1, 0, 2])
-        p_base.mtrx_params[p_base.BW:, :, :] = np.tile(np.transpose(
-            dict_params['tfunc_mtrx_params_S'][:p_base.S, -1, :].reshape(
-                p_base.S, 1, num_mtrx_params), axes=[1, 0, 2]), (p_base.T - p_base.BW, 1, 1))
-        p_base.mtry_params = np.empty((p_base.T, p_base.S, num_mtry_params))
-        p_base.mtry_params[:p_base.BW, :, :] =\
-            np.transpose(
-                dict_params['tfunc_mtry_params_S'][:p_base.S, :p_base.BW, :],
-                axes=[1, 0, 2])
-        p_base.mtry_params[p_base.BW:, :, :] = np.tile(np.transpose(
-            dict_params['tfunc_mtry_params_S'][:p_base.S, -1, :].reshape(
-                p_base.S, 1, num_mtry_params), axes=[1, 0, 2]), (p_base.T - p_base.BW, 1, 1))
-        etr_params_old = p_base.etr_params
-        mtrx_params_old = p_base.mtrx_params
-        mtry_params_old = p_base.mtry_params
-        p_base.etr_params = etr_params_old.copy()
-        p_base.etr_params[:, :, 5] = etr_params_old[:, :, 6]
-        p_base.etr_params[:, :, 6] = etr_params_old[:, :, 11]
-        p_base.etr_params[:, :, 7] = etr_params_old[:, :, 5]
-        p_base.etr_params[:, :, 8] = etr_params_old[:, :, 7]
-        p_base.etr_params[:, :, 9] = etr_params_old[:, :, 8]
-        p_base.etr_params[:, :, 10] = etr_params_old[:, :, 9]
-        p_base.etr_params[:, :, 11] = etr_params_old[:, :, 10]
-        p_base.mtrx_params = mtrx_params_old.copy()
-        p_base.mtrx_params[:, :, 5] = mtrx_params_old[:, :, 6]
-        p_base.mtrx_params[:, :, 6] = mtrx_params_old[:, :, 11]
-        p_base.mtrx_params[:, :, 7] = mtrx_params_old[:, :, 5]
-        p_base.mtrx_params[:, :, 8] = mtrx_params_old[:, :, 7]
-        p_base.mtrx_params[:, :, 9] = mtrx_params_old[:, :, 8]
-        p_base.mtrx_params[:, :, 10] = mtrx_params_old[:, :, 9]
-        p_base.mtrx_params[:, :, 11] = mtrx_params_old[:, :, 10]
-        p_base.mtry_params = mtry_params_old.copy()
-        p_base.mtry_params[:, :, 5] = mtry_params_old[:, :, 6]
-        p_base.mtry_params[:, :, 6] = mtry_params_old[:, :, 11]
-        p_base.mtry_params[:, :, 7] = mtry_params_old[:, :, 5]
-        p_base.mtry_params[:, :, 8] = mtry_params_old[:, :, 7]
-        p_base.mtry_params[:, :, 9] = mtry_params_old[:, :, 8]
-        p_base.mtry_params[:, :, 10] = mtry_params_old[:, :, 9]
-        p_base.mtry_params[:, :, 11] = mtry_params_old[:, :, 10]
-        base_ss_outputs = SS.run_SS(p_base, dask_client)
+        if p_base.use_zeta:
+            p_base.update_specifications({
+                'initial_guess_r_SS': 0.10, 'initial_guess_TR_SS': 0.02})
+        p_base.baseline_spending = False
+        base_ss_outputs = SS.run_SS(p_base, client=dask_client)
         utils.mkdirs(os.path.join(
             constants.BASELINE_DIR, "SS"))
         ss_dir = os.path.join(
             constants.BASELINE_DIR, "SS", "SS_vars.pkl")
         with open(ss_dir, "wb") as f:
             pickle.dump(base_ss_outputs, f)
-    if baseline:
-        dict_params = utils.safe_read_pickle(os.path.join(
-            CUR_PATH, 'TxFuncEst_baseline.pkl'))
-    else:
-        dict_params = utils.safe_read_pickle(os.path.join(
-            CUR_PATH, 'TxFuncEst_policy.pkl'))
+    # now run specification for test
     p = Specifications(baseline=baseline, num_workers=NUM_WORKERS)
     p.update_specifications(param_updates)
-    p.BW = 10
-    p.frac_tax_payroll = np.zeros(p.frac_tax_payroll.shape)
-    num_etr_params = dict_params['tfunc_etr_params_S'].shape[2]
-    num_mtrx_params = dict_params['tfunc_mtrx_params_S'].shape[2]
-    num_mtry_params = dict_params['tfunc_mtry_params_S'].shape[2]
-    p.mean_income_data = dict_params['tfunc_avginc'][0]
-    p.etr_params = np.empty((p.T, p.S, num_etr_params))
-    p.etr_params[:p.BW, :, :] =\
-        np.transpose(
-            dict_params['tfunc_etr_params_S'][:p.S, :p.BW, :],
-            axes=[1, 0, 2])
-    p.etr_params[p.BW:, :, :] = np.tile(np.transpose(
-        dict_params['tfunc_etr_params_S'][:p.S, -1, :].reshape(
-            p.S, 1, num_etr_params), axes=[1, 0, 2]), (p.T - p.BW, 1, 1))
-    p.mtrx_params = np.empty((p.T, p.S, num_mtrx_params))
-    p.mtrx_params[:p.BW, :, :] =\
-        np.transpose(
-            dict_params['tfunc_mtrx_params_S'][:p.S, :p.BW, :],
-            axes=[1, 0, 2])
-    p.mtrx_params[p.BW:, :, :] = np.tile(np.transpose(
-        dict_params['tfunc_mtrx_params_S'][:p.S, -1, :].reshape(
-            p.S, 1, num_mtrx_params), axes=[1, 0, 2]), (p.T - p.BW, 1, 1))
-    p.mtry_params = np.empty((p.T, p.S, num_mtry_params))
-    p.mtry_params[:p.BW, :, :] =\
-        np.transpose(
-            dict_params['tfunc_mtry_params_S'][:p.S, :p.BW, :],
-            axes=[1, 0, 2])
-    p.mtry_params[p.BW:, :, :] = np.tile(np.transpose(
-        dict_params['tfunc_mtry_params_S'][:p.S, -1, :].reshape(
-            p.S, 1, num_mtry_params), axes=[1, 0, 2]), (p.T - p.BW, 1, 1))
-    # p.get_tax_function_parameters(None, run_micro=False,
-    #                               tax_func_path=tax_func_path)
-    etr_params_old = p.etr_params
-    mtrx_params_old = p.mtrx_params
-    mtry_params_old = p.mtry_params
-    p.etr_params = etr_params_old.copy()
-    p.etr_params[:, :, 5] = etr_params_old[:, :, 6]
-    p.etr_params[:, :, 6] = etr_params_old[:, :, 11]
-    p.etr_params[:, :, 7] = etr_params_old[:, :, 5]
-    p.etr_params[:, :, 8] = etr_params_old[:, :, 7]
-    p.etr_params[:, :, 9] = etr_params_old[:, :, 8]
-    p.etr_params[:, :, 10] = etr_params_old[:, :, 9]
-    p.etr_params[:, :, 11] = etr_params_old[:, :, 10]
-    p.mtrx_params = mtrx_params_old.copy()
-    p.mtrx_params[:, :, 5] = mtrx_params_old[:, :, 6]
-    p.mtrx_params[:, :, 6] = mtrx_params_old[:, :, 11]
-    p.mtrx_params[:, :, 7] = mtrx_params_old[:, :, 5]
-    p.mtrx_params[:, :, 8] = mtrx_params_old[:, :, 7]
-    p.mtrx_params[:, :, 9] = mtrx_params_old[:, :, 8]
-    p.mtrx_params[:, :, 10] = mtrx_params_old[:, :, 9]
-    p.mtrx_params[:, :, 11] = mtrx_params_old[:, :, 10]
-    p.mtry_params = mtry_params_old.copy()
-    p.mtry_params[:, :, 5] = mtry_params_old[:, :, 6]
-    p.mtry_params[:, :, 6] = mtry_params_old[:, :, 11]
-    p.mtry_params[:, :, 7] = mtry_params_old[:, :, 5]
-    p.mtry_params[:, :, 8] = mtry_params_old[:, :, 7]
-    p.mtry_params[:, :, 9] = mtry_params_old[:, :, 8]
-    p.mtry_params[:, :, 10] = mtry_params_old[:, :, 9]
-    p.mtry_params[:, :, 11] = mtry_params_old[:, :, 10]
-
-    test_dict = SS.run_SS(p, client=None)
-    pickle.dump(test_dict, open(os.path.join(CUR_PATH, 'test_io_data', filename), 'wb'))
+    test_dict = SS.run_SS(p, client=dask_client)
     expected_dict = utils.safe_read_pickle(
         os.path.join(CUR_PATH, 'test_io_data', filename))
 
     for k, v in expected_dict.items():
-        # print('Checking item = ', k)
-        assert(np.allclose(test_dict[k], v, atol=1e-04))
+        print('Checking item = ', k)
+        assert(np.allclose(test_dict[k], v, atol=1e-06))
