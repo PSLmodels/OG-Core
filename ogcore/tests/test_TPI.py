a7d69f64eb27e6ddf3cdc211de2054755228e9c1
jdebacker <jason.debacker@gmail.com>
2021-08-10 17:32:04 -0400

    update donut tests and output

--- ogcore/tests/test_TPI.py
+++ ogcore/tests/test_TPI.py
@@ -28,21 +28,18 @@ def dask_client():
 filename1 = 'intial_SS_values_baseline.pkl'
 filename2 = 'intial_SS_values_reform.pkl'
 filename3 = 'intial_SS_values_reform_base_spend.pkl'
-param_updates1 = {'start_year': 2020}
-param_updates2 = {'start_year': 2020}
-param_updates3 = {'start_year': 2020, 'baseline_spending': True}
 
 
 @pytest.mark.parametrize('baseline,param_updates,filename',
-                         [(True, param_updates1, filename1),
-                          (False, param_updates2, filename2),
-                          (False, param_updates3, filename3)],
+                         [(True, {}, filename1),
+                          (False, {}, filename2),
+                          (False, {'baseline_spending': True}, filename3)],
                          ids=['Baseline', 'Reform',
                               'Reform, baseline_spending'])
 def test_get_initial_SS_values(baseline, param_updates, filename,
                                dask_client):
-    p = Specifications(baseline=baseline, client=dask_client,
-                       num_workers=NUM_WORKERS)
+    p = Specifications(
+        baseline=baseline, num_workers=NUM_WORKERS)
     p.update_specifications(param_updates)
     p.baseline_dir = os.path.join(CUR_PATH, 'test_io_data', 'OUTPUT')
     p.output_base = os.path.join(CUR_PATH, 'test_io_data', 'OUTPUT')
@@ -63,8 +60,8 @@ def test_get_initial_SS_values(baseline, param_updates, filename,
     b_sinit = np.array(list(np.zeros(p.J).reshape(1, p.J)) +
                        list(initial_b[:-1]))
     b_splus1init = initial_b
-    exp_initial_values = (B0, b_sinit, b_splus1init, factor, initial_b,
-                          initial_n)
+    exp_initial_values = (
+        B0, b_sinit, b_splus1init, factor, initial_b, initial_n)
 
     for i, v in enumerate(exp_initial_values):
         assert(np.allclose(test_initial_values[i], v, equal_nan=True))
@@ -79,69 +76,13 @@ def test_get_initial_SS_values(baseline, param_updates, filename,
         assert(np.allclose(test_ss_vars[k], v, equal_nan=True))
 
 
-def test_firstdoughnutring(dask_client):
+def test_firstdoughnutring():
     # Test TPI.firstdoughnutring function.  Provide inputs to function and
     # ensure that output returned matches what it has been before.
     input_tuple = utils.safe_read_pickle(
         os.path.join(CUR_PATH, 'test_io_data', 'firstdoughnutring_inputs.pkl'))
-    guesses, r, w, b, BQ, TR, j, params = input_tuple
-    income_tax_params, tpi_params, initial_b = params
-    tpi_params = tpi_params + [True]
-    p = Specifications(client=dask_client, num_workers=NUM_WORKERS)
-    (p.J, p.S, p.T, p.BW, beta, p.sigma, p.alpha, p.gamma, p.epsilon,
-     Z, p.delta, p.ltilde, p.nu, p.g_y, p.g_n, tau_b, delta_tau,
-     tau_payroll, tau_bq, p.rho, p.omega, N_tilde, lambdas,
-     p.imm_rates, p.e, retire, p.mean_income_data, factor, h_wealth,
-     p_wealth, m_wealth, p.b_ellipse, p.upsilon, p.chi_b, p.chi_n,
-     theta, p.baseline) = tpi_params
-    p.beta = np.ones(p.J) * beta
-    p.Z = np.ones(p.T + p.S) * Z
-    p.tau_bq = np.ones(p.T + p.S) * 0.0
-    p.tau_payroll = np.ones(p.T + p.S) * tau_payroll
-    p.tau_b = np.ones(p.T + p.S) * tau_b
-    p.delta_tau = np.ones(p.T + p.S) * delta_tau
-    p.h_wealth = np.ones(p.T + p.S) * h_wealth
-    p.p_wealth = np.ones(p.T + p.S) * p_wealth
-    p.m_wealth = np.ones(p.T + p.S) * m_wealth
-    p.retire = (np.ones(p.T + p.S) * retire).astype(int)
-    p.tax_func_type = 'DEP'
-    p.analytical_mtrs, etr_params, mtrx_params, mtry_params =\
-        income_tax_params
-    p.etr_params = np.transpose(etr_params, (1, 0, 2))
-    p.mtrx_params = np.transpose(mtrx_params, (1, 0, 2))
-    p.mtry_params = np.transpose(mtry_params, (1, 0, 2))
-    etr_params_old = p.etr_params
-    mtrx_params_old = p.mtrx_params
-    mtry_params_old = p.mtry_params
-    p.etr_params = etr_params_old.copy()
-    p.etr_params[:, :, 5] = etr_params_old[:, :, 6]
-    p.etr_params[:, :, 6] = etr_params_old[:, :, 11]
-    p.etr_params[:, :, 7] = etr_params_old[:, :, 5]
-    p.etr_params[:, :, 8] = etr_params_old[:, :, 7]
-    p.etr_params[:, :, 9] = etr_params_old[:, :, 8]
-    p.etr_params[:, :, 10] = etr_params_old[:, :, 9]
-    p.etr_params[:, :, 11] = etr_params_old[:, :, 10]
-    p.mtrx_params = mtrx_params_old.copy()
-    p.mtrx_params[:, :, 5] = mtrx_params_old[:, :, 6]
-    p.mtrx_params[:, :, 6] = mtrx_params_old[:, :, 11]
-    p.mtrx_params[:, :, 7] = mtrx_params_old[:, :, 5]
-    p.mtrx_params[:, :, 8] = mtrx_params_old[:, :, 7]
-    p.mtrx_params[:, :, 9] = mtrx_params_old[:, :, 8]
-    p.mtrx_params[:, :, 10] = mtrx_params_old[:, :, 9]
-    p.mtrx_params[:, :, 11] = mtrx_params_old[:, :, 10]
-    p.mtry_params = mtry_params_old.copy()
-    p.mtry_params[:, :, 5] = mtry_params_old[:, :, 6]
-    p.mtry_params[:, :, 6] = mtry_params_old[:, :, 11]
-    p.mtry_params[:, :, 7] = mtry_params_old[:, :, 5]
-    p.mtry_params[:, :, 8] = mtry_params_old[:, :, 7]
-    p.mtry_params[:, :, 9] = mtry_params_old[:, :, 8]
-    p.mtry_params[:, :, 10] = mtry_params_old[:, :, 9]
-    p.mtry_params[:, :, 11] = mtry_params_old[:, :, 10]
-    p.lambdas = lambdas.reshape(p.J, 1)
-    p.num_workers = 1
-    bq = BQ / p.lambdas[j]
-    tr = TR
-    ubi = 0.0
+    guesses, r, w, bq, tr, theta, factor, ubi, j, initial_b = input_tuple
+    p = Specifications()
     test_list = TPI.firstdoughnutring(guesses, r, w, bq, tr, theta,
                                       factor, ubi, j, initial_b, p)
 
@@ -171,11 +112,12 @@ def test_twist_doughnut(file_inputs, file_outputs):
     ensure that output returned matches what it has been before.
     '''
     input_tuple = utils.safe_read_pickle(file_inputs)
-    (guesses, r, w, bq, tr, theta, factor, j, s, t, tau_c, etr_params,
-     mtrx_params, mtry_params, initial_b, p) = input_tuple
-    ubi = np.zeros(int(len(guesses) / 2))
-    input_tuple = (guesses, r, w, bq, tr, theta, factor, ubi, j, s, t, tau_c,
-                   etr_params, mtrx_params, mtry_params, initial_b, p)
+    (guesses, r, w, bq, tr, theta, factor, ubi, j, s, t, tau_c,
+     etr_params, mtrx_params, mtry_params, initial_b) = input_tuple
+    p = Specifications()
+    input_tuple = (
+        guesses, r, w, bq, tr, theta, factor, ubi, j, s, t, tau_c,
+        etr_params, mtrx_params, mtry_params, initial_b, p)
     test_list = TPI.twist_doughnut(*input_tuple)
     expected_list = utils.safe_read_pickle(file_outputs)
     assert(np.allclose(np.array(test_list), np.array(expected_list)))
