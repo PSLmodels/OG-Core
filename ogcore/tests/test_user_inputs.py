44fa1983deea8384bb50d6839ba4da9dd619b5f7
Richard Evans <rickecon@gmail.com>
2022-01-19 11:07:28 -0700

    Update test_user_inputs.py to save to temp files

--- ogcore/tests/test_user_inputs.py
+++ ogcore/tests/test_user_inputs.py
@@ -22,11 +22,11 @@ def dask_client():
 @pytest.mark.local
 @pytest.mark.parametrize('frisch', [0.32, 0.4, 0.62],
                          ids=['Frisch 0.32', 'Frisch 0.4', 'Frisch 0.6'])
-def test_frisch(frisch, dask_client):
+def test_frisch(tmpdir, frisch, dask_client):
     og_spec = {'frisch': frisch, 'debt_ratio_ss': 1.0}
     p = Specifications(
-        baseline=True, num_workers=NUM_WORKERS, baseline_dir=OUTPUT_DIR,
-        output_base=OUTPUT_DIR)
+        baseline=True, num_workers=NUM_WORKERS, baseline_dir=tmpdir,
+        output_base=tmpdir)
     p.update_specifications(og_spec)
     runner(p, time_path=False, client=dask_client)
 
@@ -34,12 +34,12 @@ def test_frisch(frisch, dask_client):
 @pytest.mark.local
 @pytest.mark.parametrize('g_y_annual', [0.0, 0.04],
                          ids=['0.0', '0.04'])
-def test_gy(g_y_annual, dask_client):
+def test_gy(tmpdir, g_y_annual, dask_client):
     og_spec = {'frisch': 0.41, 'debt_ratio_ss': 1.0,
                'g_y_annual': g_y_annual}
     p = Specifications(
-        baseline=True, num_workers=NUM_WORKERS, baseline_dir=OUTPUT_DIR,
-        output_base=OUTPUT_DIR)
+        baseline=True, num_workers=NUM_WORKERS, baseline_dir=tmpdir,
+        output_base=tmpdir)
     p.update_specifications(og_spec)
     runner(p, time_path=False, client=dask_client)
 
@@ -48,10 +48,10 @@ def test_gy(g_y_annual, dask_client):
 @pytest.mark.parametrize('sigma', [1.3, 1.5, 1.7, 1.9],
                          ids=['sigma=1.3', 'sigma=1.5', 'sigma=1.7',
                               'sigma=1.9'])
-def test_sigma(sigma, dask_client):
+def test_sigma(tmpdir, sigma, dask_client):
     og_spec = {'frisch': 0.41, 'debt_ratio_ss': 1.0, 'sigma': sigma}
     p = Specifications(
-        baseline=True, num_workers=NUM_WORKERS, baseline_dir=OUTPUT_DIR,
-        output_base=OUTPUT_DIR)
+        baseline=True, num_workers=NUM_WORKERS, baseline_dir=tmpdir,
+        output_base=tmpdir)
     p.update_specifications(og_spec)
     runner(p, time_path=False, client=dask_client)
