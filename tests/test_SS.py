393b5f672c751523c67f23c5d01a38fe249fb6c3
Richard Evans <rickecon@gmail.com>
2022-02-17 17:39:55 -0700

    Updated test_SS.py::test_run_SS

--- tests/test_SS.py
+++ tests/test_SS.py
@@ -514,8 +514,7 @@ def test_run_SS(tmpdir, baseline, param_updates, filename, dask_client):
             baseline=True,
             num_workers=NUM_WORKERS)
         param_updates_base = param_updates.copy()
-        if param_updates['baseline_spending'] == True:
-            param_updates_base['baseline_spending'] = False
+        param_updates_base['baseline_spending'] = False
         p_base.update_specifications(param_updates_base)
         base_ss_outputs = SS.run_SS(p_base, client=dask_client)
         utils.mkdirs(os.path.join(baseline_dir, "SS"))
