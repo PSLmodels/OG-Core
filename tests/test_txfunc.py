2fb6041d0af20cae51599dcfc19471d3dc8e811e
Richard Evans <rickecon@gmail.com>
2022-09-18 11:08:01 -0600

    Updated test_tax_func_estimate() func in test_txfunc.py

--- tests/test_txfunc.py
+++ tests/test_txfunc.py
@@ -693,10 +693,10 @@ def test_tax_func_estimate(tmpdir, dask_client):
     for k, v in expected_dict.items():
         if isinstance(v, str):  # for testing tax_func_type object
             assert test_dict[k] == v
-        elif isinstance(expected_dict[i], list):
-            test_dict_obj = np.array(test_dict[i])
-            exp_dict_obj = np.array(expected_dict[i])
-            print("For element", i, ", diff =",
+        elif isinstance(expected_dict[k], list):
+            test_dict_obj = np.array(test_dict[k])
+            exp_dict_obj = np.array(expected_dict[k])
+            print("For element", k, ", diff =",
                   np.absolute(test_dict_obj - exp_dict_obj).max())
         else:  # for testing all other objects
             print("Max diff for ", k, " = ",
