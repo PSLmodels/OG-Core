79e27fdab4e752b4bd2183b5c85d31b48559a5b5
jdebacker <jason.debacker@gmail.com>
2023-03-28 20:34:13 -0400

    change loops to list comprehensions in txfunc

--- ogcore/txfunc.py
+++ ogcore/txfunc.py
@@ -257,28 +257,30 @@ def get_tax_rates(
             mono_interp = params[0]
             txrates = mono_interp(income)
         else:
-            txrates = np.zeros_like(income)
             if np.isscalar(income):
                 txrates = params[0](income)
             elif (
                 income.ndim == 1
             ):  # I think only calls here are for loops over S (or just a single age)
-                for s in range(income.shape[0]):
-                    txrates[s] = params[s][0](income[s])
+                # for s in range(income.shape[0]):
+                #     txrates[s] = params[s][0](income[s])
+                txrates = [params[s][0](income[s]) for s in range(income.shape[0])]
             elif (
                 income.ndim == 2
             ):  # I think only calls here are for loops over S and J
-                for s in range(income.shape[0]):
-                    for j in range(income.shape[1]):
-                        txrates[s, j] = params[s][j][0](income[s, j])
+                # for s in range(income.shape[0]):
+                #     for j in range(income.shape[1]):
+                #         txrates[s, j] = params[s][j][0](income[s, j])
+                txrates = [[params[s][j][0](income[s, j]) for j in range(income.shape[1])] for s in range(income.shape[0])]
             else:  # to catch 3D arrays, looping over T, S, J
-                for t in range(income.shape[0]):
-                    for s in range(income.shape[1]):
-                        for j in range(income.shape[2]):
-                            txrates[t, s, j] = params[t][s][j][0](
-                                income[t, s, j]
-                            )
-
+                # for t in range(income.shape[0]):
+                #     for s in range(income.shape[1]):
+                #         for j in range(income.shape[2]):
+                #             txrates[t, s, j] = params[t][s][j][0](
+                #                 income[t, s, j]
+                #             )
+                txrates = [[[params[t][s][j][0](income[t, s, j]) for j in range(income.shape[2])] for s in range(income.shape[1])] for t in range(income.shape[0])]
+        np.array(txrates)
     return txrates
 
 
