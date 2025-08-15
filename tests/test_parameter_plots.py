6e709886d90f2b5b28ecc6cdb416ad014533a8d3
Jason DeBacker <jason.debacker@gmail.com>
2025-08-15 13:24:54 -0400

    read in new params files

--- tests/test_parameter_plots.py
+++ tests/test_parameter_plots.py
@@ -14,22 +14,28 @@ from ogcore import utils, parameter_plots, Specifications
 
 # Load in test results and parameters
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
-if sys.version_info[1] < 11:
-    base_params = utils.safe_read_pickle(
-        os.path.join(CUR_PATH, "test_io_data", "model_params_baseline.pkl")
-    )
-elif sys.version_info[1] == 11:
+if sys.version_info[1] == 11:
     base_params = utils.safe_read_pickle(
         os.path.join(
             CUR_PATH, "test_io_data", "model_params_baseline_v311.pkl"
         )
     )
-else:
+elif sys.version_info[1] == 12:
     base_params = utils.safe_read_pickle(
         os.path.join(
             CUR_PATH, "test_io_data", "model_params_baseline_v312.pkl"
         )
     )
+elif sys.version_info[1] == 13:
+    base_params = utils.safe_read_pickle(
+        os.path.join(
+            CUR_PATH, "test_io_data", "model_params_baseline.pkl"
+        )
+    )
+else:
+    # Raise assertion error
+    assert False, "Unsupported Python version"
+
 base_taxfunctions = utils.safe_read_pickle(
     os.path.join(CUR_PATH, "test_io_data", "TxFuncEst_baseline.pkl")
 )
