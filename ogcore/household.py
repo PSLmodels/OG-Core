165c321d9008725c820a4ddc101bcf56b229e0c5
Jason DeBacker <jason.debacker@gmail.com>
2025-09-08 12:26:25 -0400

    update log messages

--- ogcore/household.py
+++ ogcore/household.py
@@ -769,28 +769,27 @@ def constraint_checker_SS(bssmat, nssmat, cssmat, ltilde):
     logging.info("Checking constraints on capital, labor, and consumption.")
 
     if (bssmat < 0).any():
-        logging.info("\tWARNING: There is negative capital stock")
+        logging.info("WARNING: There is negative capital stock")
     flag2 = False
     if (nssmat < 0).any():
         logging.info(
-            "\tWARNING: Labor supply violates nonnegativity ", "constraints."
+            "WARNING: Labor supply violates nonnegativity constraints."
         )
         flag2 = True
     if (nssmat > ltilde).any():
-        logging.info("\tWARNING: Labor supply violates the ltilde constraint.")
+        logging.info("WARNING: Labor supply violates the ltilde constraint.")
         flag2 = True
     if flag2 is False:
         logging.info(
-            "\tThere were no violations of the constraints on labor",
-            " supply.",
+            "There were no violations of the constraints on labor supply.",
         )
     if (cssmat < 0).any():
         logging.info(
-            "\tWARNING: Consumption violates nonnegativity", " constraints."
+            "\tWARNING: Consumption violates nonnegativity constraints."
         )
     else:
         logging.info(
-            "\tThere were no violations of the constraints on", " consumption."
+            "There were no violations of the constraints on consumption."
         )
 
 
