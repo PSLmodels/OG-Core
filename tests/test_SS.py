5a826821db531a2a8537ce1f01ae635a5352ddc3
Jason DeBacker <jason.debacker@gmail.com>
2025-09-10 16:59:51 -0400

    update var namemapping for new change

--- tests/test_SS.py
+++ tests/test_SS.py
@@ -70,7 +70,7 @@ VAR_NAME_MAPPING = {
     "trssmat": "tr",
     "ubissmat": "ubi",
     "yss_before_tax_mat": "before_tax_income",
-    "total_taxes_ss": "hh_taxes",
+    "total_taxes_ss": "hh_net_taxes",
     "etr_ss": "etr",
     "mtrx_ss": "mtrx",
     "mtry_ss": "mtry",
