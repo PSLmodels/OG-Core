8587e6e5ce9492fa30a3ac6325d87a70ebee7019
Jason DeBacker <jason.debacker@gmail.com>
2025-11-18 00:55:13 -0500

    use c_i in cons tax liab calcs

--- ogcore/SS.py
+++ ogcore/SS.py
@@ -978,7 +978,15 @@ def SS_solver(
         np.squeeze(p.e[-1, :, :]),
         p,
     )
-    sales_tax_ss = tax.cons_tax_liab(cssmat, p_i_ss, p, "SS")
+    c_i = household.get_ci(
+            cssmat,
+            p_i_ss,
+            p_tilde_ss,
+            p.tau_c[-1, :],
+            p.alpha_c,
+            "SS",
+        )
+    sales_tax_ss = tax.cons_tax_liab(c_i, p_i_ss, p, "SS")
     yss_before_tax_mat = household.get_y(
         r_p_ss, wss, bssmat_s, nssmat, p, "SS"
     )
